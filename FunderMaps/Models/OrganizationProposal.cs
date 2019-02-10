7654ef7b1e43563e25c2c391bdd2c64f290be156
Yorick de Wid <yorick17@outlook.com>
2019-02-10 15:03:56 +0100

    Organization proposal model
    - Remove cookie partial

---
+++ FunderMaps/Models/OrganizationProposal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FunderMaps.Models
+{
+    public class OrganizationProposal
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public Guid Token { get; set; } = Guid.NewGuid();
+
+        public OrganizationProposal(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
