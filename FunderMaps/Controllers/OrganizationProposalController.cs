be1b2044abebeb0ffd4b4092f7d60297da5047b5
Yorick de Wid <yorick17@outlook.com>
2019-02-10 15:42:04 +0100

    Explicit return object
    - Generate new token for security

--- FunderMaps/Controllers/OrganizationProposalController.cs
+++ FunderMaps/Controllers/OrganizationProposalController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using FunderMaps.Data;
 using FunderMaps.Models;
 
@@ -18,6 +19,7 @@ namespace FunderMaps.Controllers
 
         // POST: api/organizationproposal
         [HttpPost]
+        [ProducesResponseType(typeof(OrganizationProposal), StatusCodes.Status200OK)]
         public async Task<IActionResult> Post([FromBody] OrganizationProposal proposal)
         {
             // Organization proposals must be unique.
@@ -25,6 +27,10 @@ namespace FunderMaps.Controllers
             {
                 return Conflict(proposal.Name);
             }
+
+            // Always generate a new token so that the user cannot
+            // control the token input.
+            proposal.GenerateToken();
 
             await _context.OrganizationProposals.AddAsync(proposal);
             await _context.SaveChangesAsync();
