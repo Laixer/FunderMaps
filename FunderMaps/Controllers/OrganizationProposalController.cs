4e11608bf3d80854d0a65bca0f515530d4ceaab3
Yorick de Wid <yorick17@outlook.com>
2019-02-10 15:02:50 +0100

    Create organization proprosal via API

---
+++ FunderMaps/Controllers/OrganizationProposalController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FunderMaps.Data;
+using FunderMaps.Models;
+
+namespace FunderMaps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationProposalController : ControllerBase
+    {
+        private readonly FunderMapsDbContext _context;
+
+        public OrganizationProposalController(FunderMapsDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/organizationproposal
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] OrganizationProposal proposal)
+        {
+            // Organization proposals must be unique.
+            if (await _context.OrganizationProposals.FindAsync(proposal.Name) != null)
+            {
+                return Conflict(proposal.Name);
+            }
+
+            await _context.OrganizationProposals.AddAsync(proposal);
+            await _context.SaveChangesAsync();
+
+            return Ok(proposal);
+        }
+    }
+}
