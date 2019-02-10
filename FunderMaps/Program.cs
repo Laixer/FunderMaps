b338102cd76c719bf08edb428a46aafdab2b6d7c
Yorick de Wid <yorick17@outlook.com>
2019-02-10 23:27:58 +0100

    Run webhost in async

--- FunderMaps/Program.cs
+++ FunderMaps/Program.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace FunderMaps
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            await WebHost
+                .CreateDefaultBuilder<Startup>(args)
+                .Build()
+                .RunAsync();
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
     }
 }
