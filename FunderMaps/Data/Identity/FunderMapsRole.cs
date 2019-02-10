1e0d1d0cfa4dee5ddcf838cc5bb7351c20828568
Yorick de Wid <yorick17@outlook.com>
2019-02-10 14:58:44 +0100

    Identity objects

---
+++ FunderMaps/Data/Identity/FunderMapsRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace FunderMaps.Data.Identity
+{
+    public class FunderMapsRole : IdentityRole<Guid>
+    {
+        public FunderMapsRole()
+        {
+        }
+
+        public FunderMapsRole(string roleName)
+            : base(roleName)
+        {
+        }
+    }
+}
---
+++ FunderMaps/Data/Identity/FunderMapsUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace FunderMaps.Data.Identity
+{
+    public class FunderMapsUser : IdentityUser<Guid>
+    {
+    }
+}
