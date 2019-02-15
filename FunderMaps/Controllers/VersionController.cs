fe4b7393f1d1a83f7378d643b5ae50d6bc9675c1
Yorick de Wid <yorick17@outlook.com>
2019-02-15 12:30:02 +0100

    Return application version information

---
+++ FunderMaps/Controllers/VersionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FunderMaps.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        public sealed class VersionOutputModel
+        {
+            public string Name { get; set; }
+            public Version ApplicationVersion { get; set; }
+            public string ApplicationVersionString { get; set; }
+        }
+
+        // GET: api/version
+        [HttpGet]
+        public VersionOutputModel Get()
+        {
+            AssemblyName assembly = Assembly.GetEntryAssembly().GetName();
+
+            return new VersionOutputModel
+            {
+                Name = assembly.Name,
+                ApplicationVersion = assembly.Version,
+                ApplicationVersionString = assembly.Version.ToString(),
+            };
+        }
+    }
+}
