cfed7bda20fb4d0a444b86aac3c6141575cca872
Yorick de Wid <yorick17@outlook.com>
2019-02-15 21:49:46 +0100

    File storage provider

---
+++ FunderMaps/Interfaces/IFileStorageService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunderMaps.Interfaces
+{
+    public interface IFileStorageService
+    {
+        /// <summary>
+        /// Store the file in the data store.
+        /// </summary>
+        /// <param name="name">File name.</param>
+        /// <param name="content">Content array.</param>
+        Task StoreFileAsync(string name, byte[] content);
+
+        /// <summary>
+        /// Store the file in the data store.
+        /// </summary>
+        /// <param name="name">File name.</param>
+        /// <param name="stream">Content stream.</param>
+        Task StoreFileAsync(string name, Stream stream);
+    }
+}
---
+++ FunderMaps/Services/AzureBlobStorageService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using FunderMaps.Interfaces;
+using System.IO;
+
+namespace FunderMaps.Services
+{
+    public class AzureBlobStorageService : IFileStorageService
+    {
+        private readonly CloudStorageAccount _storageAccount;
+        protected readonly CloudBlobClient _blobClient;
+
+        public AzureBlobStorageService(IConfiguration config)
+        {
+            _storageAccount = CloudStorageAccount.Parse(config.GetConnectionString("AzureStorageConnectionString"));
+            _blobClient = _storageAccount.CreateCloudBlobClient();
+        }
+
+        protected CloudBlobContainer SetContainer(string name) =>
+            _blobClient.GetContainerReference(name);
+
+        protected CloudBlockBlob PrepareBlob(string filename, BlobProperties properties)
+        {
+            CloudBlockBlob cloudBlockBlob = SetContainer("report").GetBlockBlobReference(filename);
+
+            cloudBlockBlob.Properties.CacheControl = properties.CacheControl;
+            cloudBlockBlob.Properties.ContentMD5 = properties.ContentMD5;
+            cloudBlockBlob.Properties.ContentEncoding = properties.ContentEncoding;
+            cloudBlockBlob.Properties.ContentLanguage = properties.ContentLanguage;
+            cloudBlockBlob.Properties.ContentDisposition = properties.ContentDisposition;
+
+            return cloudBlockBlob;
+        }
+
+        protected CloudBlockBlob PrepareBlob(string filename)
+        {
+            return PrepareBlob(filename, new BlobProperties());
+        }
+
+        public async Task StoreFileAsync(string name, byte[] content)
+        {
+            await PrepareBlob(name).UploadFromByteArrayAsync(content, 0, 0);
+        }
+
+        public async Task StoreFileAsync(string name, Stream stream)
+        {
+            await PrepareBlob(name).UploadFromStreamAsync(stream);
+        }
+    }
+}
