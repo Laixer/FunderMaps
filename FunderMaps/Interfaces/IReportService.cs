c0bb1d5d9968d57b25a07f22acf2ecd9efbfa71e
Yorick de Wid <yorick17@outlook.com>
2019-02-15 22:20:32 +0100

    Prepare Report and Sample services

---
+++ FunderMaps/Interfaces/IReportService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunderMaps.Interfaces
+{
+    public interface IReportService
+    {
+        Task CreateAsync();
+        Task DeleteAsync();
+        Task ValidateAsync();
+    }
+}
