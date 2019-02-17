424a01db3e57110c191d2a3b9c81242b3175754b
Yorick de Wid <yorick17@outlook.com>
2019-02-17 17:39:21 +0100

    Get configuration value from any location

--- FunderMaps/Extensions/ConfigurationExtensions.cs
+++ FunderMaps/Extensions/ConfigurationExtensions.cs
@@ -7,13 +7,42 @@ namespace FunderMaps.Extensions
     public static class ConfigurationExtensions
     {
         /// <summary>
+        /// Retrieve the configuration option from one of the configuration providers.
+        /// </summary>
+        /// <param name="configuration">Configuration interface.</param>
+        /// <param name="section">Settting section.</param>
+        /// <param name="key">Setting key.</param>
+        /// <returns>Value or null</returns>
+        private static string GetConfigSection(IConfiguration configuration, string section, string key)
+        {
+            var value = string.Empty;
+
+            // Get option from default configuration
+            value = configuration?.GetSection(section)?[key];
+            if (value != null)
+            {
+                return value;
+            }
+
+            // Get option from Azure settings
+            value = configuration?.GetSection($"APPSETTING_{section}")?[key];
+            if (value != null)
+            {
+                return value;
+            }
+
+            // FUTURE: Add any other methodes here
+
+            return value;
+        }
+
+        /// <summary>
         /// Get signature key from configuration and convert into security key.
         /// </summary>
         /// <param name="configuration">The configuration.</param>
         public static SymmetricSecurityKey GetJwtSignKey(this IConfiguration configuration)
         {
-            var configKey = configuration?.GetSection("Jwt")?["SignatureKey"];
-
+            var configKey = GetConfigSection(configuration, "Jwt", "SignatureKey");
             return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configKey));
         }
 
@@ -32,7 +61,7 @@ namespace FunderMaps.Extensions
         /// <param name="configuration">The configuration.</param>
         public static string GetJwtAudience(this IConfiguration configuration)
         {
-            return configuration?.GetSection("Jwt")?["Audience"];
+            return GetConfigSection(configuration, "Jwt", "Audience");
         }
 
         /// <summary>
@@ -41,7 +70,7 @@ namespace FunderMaps.Extensions
         /// <param name="configuration">The configuration.</param>
         public static int GetJwtTokenExpirationInMinutes(this IConfiguration configuration)
         {
-            return int.Parse(configuration?.GetSection("Jwt")?["TokenValidity"]);
+            return int.Parse(GetConfigSection(configuration, "Jwt", "TokenValidity"));
         }
     }
 }
