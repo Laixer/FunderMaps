8c27a991a64dd8c88a0f29cc00735fe45c81b7c4
Yorick de Wid <yorick17@outlook.com>
2019-02-17 15:35:45 +0100

    Get Jwt settings from config
    - Create Jwt Token from identity user

---
+++ FunderMaps/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FunderMaps.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Get signature key from configuration and convert into security key.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public static SymmetricSecurityKey GetJwtSignKey(this IConfiguration configuration)
+        {
+            var configKey = configuration?.GetSection("Jwt")?["SignatureKey"];
+
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configKey));
+        }
+
+        /// <summary>
+        /// Get the issuer from the configuration.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public static string GetJwtIssuer(this IConfiguration configuration)
+        {
+            return configuration?.GetSection("Jwt")?["Issuer"];
+        }
+
+        /// <summary>
+        /// Get the audience from the configuration.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public static string GetJwtAudience(this IConfiguration configuration)
+        {
+            return configuration?.GetSection("Jwt")?["Audience"];
+        }
+
+        /// <summary>
+        /// Get the token expiration time in minutes from the configuration.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public static int GetJwtTokenExpirationInMinutes(this IConfiguration configuration)
+        {
+            return int.Parse(configuration?.GetSection("Jwt")?["TokenValidity"]);
+        }
+    }
+}
---
+++ FunderMaps/Identity/JwtTokenIdentityUser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FunderMaps.Identity
+{
+    public class JwtTokenIdentityUser<TUser, TKey>
+        where TUser : IdentityUser<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        private readonly JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+        public List<Claim> Claims { get; }
+        public SigningCredentials SigningCredentials { get; }
+
+        public string SignatureAlgorithm { get; set; } = SecurityAlgorithms.HmacSha256;
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int TokenValid { get; set; } = 30;
+
+        public JwtTokenIdentityUser(TUser user, SymmetricSecurityKey key)
+        {
+            Claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+            SigningCredentials = GetSigningKey(key);
+        }
+
+        protected SigningCredentials GetSigningKey(SymmetricSecurityKey key)
+        {
+            return new SigningCredentials(key, SignatureAlgorithm);
+        }
+
+        protected JwtSecurityToken ConstructJwtToken(List<Claim> claims)
+        {
+            var tokenNotValidBefore = DateTime.Now;
+            var tokenNotValidAfter = DateTime.Now.AddMinutes(TokenValid);
+
+            return new JwtSecurityToken(
+                issuer: Issuer,
+                audience: Audience,
+                claims: claims,
+                notBefore: tokenNotValidBefore,
+                expires: tokenNotValidAfter,
+                signingCredentials: SigningCredentials);
+        }
+
+        public string WriteToken()
+        {
+            return handler.WriteToken(ConstructJwtToken(Claims));
+        }
+    }
+}
