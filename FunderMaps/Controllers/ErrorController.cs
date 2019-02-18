641c59675681f916b7c76779a415a3fa06443dc0
Yorick de Wid <yorick17@outlook.com>
2019-02-18 13:19:38 +0100

    Remove most of the default views
    - Redirect errors to error view

---
+++ FunderMaps/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FunderMaps.Models;
+
+namespace FunderMaps.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
