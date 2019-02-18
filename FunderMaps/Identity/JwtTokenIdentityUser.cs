9fb11ed549772d428f2b50715caf79430fd49a31
Yorick de Wid <yorick17@outlook.com>
2019-02-18 11:44:23 +0100

    Add user roles as jwt claims
    - Documentation

--- FunderMaps/Identity/JwtTokenIdentityUser.cs
+++ FunderMaps/Identity/JwtTokenIdentityUser.cs
@@ -21,22 +21,52 @@ namespace FunderMaps.Identity
         public string Audience { get; set; }
         public int TokenValid { get; set; } = 30;
 
+        /// <summary>
+        /// Create a new instance of the JWT token identity user.
+        /// </summary>
+        /// <param name="user">Identity user.</param>
+        /// <param name="key">Symmetric key used for signatures.</param>
         public JwtTokenIdentityUser(TUser user, SymmetricSecurityKey key)
         {
             Claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
             };
             SigningCredentials = GetSigningKey(key);
         }
 
+        /// <summary>
+        /// Add principal roles as authorization claims.
+        /// </summary>
+        /// <param name="roles">List of user roles.</param>
+        public void AddRoleClaims(IList<string> roles)
+        {
+            foreach (var role in roles)
+            {
+                Claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+        }
+
+        // FUTURE: AddExternalClaims
+
+        /// <summary>
+        /// Use symmetric key as token signature credentials.
+        /// </summary>
+        /// <param name="key">Symmetric key.</param>
+        /// <returns>Signature credentials.</returns>
         protected SigningCredentials GetSigningKey(SymmetricSecurityKey key)
         {
             return new SigningCredentials(key, SignatureAlgorithm);
         }
 
+        /// <summary>
+        /// Create the JWT token.
+        /// </summary>
+        /// <param name="claims">List of authorization claims</param>
+        /// <returns>Jwt token.</returns>
         protected JwtSecurityToken ConstructJwtToken(List<Claim> claims)
         {
             var tokenNotValidBefore = DateTime.Now;
@@ -51,6 +81,10 @@ namespace FunderMaps.Identity
                 signingCredentials: SigningCredentials);
         }
 
+        /// <summary>
+        /// Serialize jwt token into string.
+        /// </summary>
+        /// <returns>Token as string.</returns>
         public string WriteToken()
         {
             return handler.WriteToken(ConstructJwtToken(Claims));
