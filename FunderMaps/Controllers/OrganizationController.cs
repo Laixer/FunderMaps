2f83d53366f8732449c0ba39dc7f0bd6003a89ee
Yorick de Wid <yorick17@outlook.com>
2019-02-19 14:26:03 +0100

    Split organization registrations

--- FunderMaps/Controllers/OrganizationController.cs
+++ FunderMaps/Controllers/OrganizationController.cs
@@ -33,7 +33,6 @@ namespace FunderMaps.Controllers
             _authorizationService = authorizationService;
         }
 
-        // FUTURE: Only organization users or admins
         // GET: api/organization/{id}
         [HttpGet("{id:guid}")]
         public async Task<IActionResult> GetAsync(Guid id)
@@ -54,151 +53,76 @@ namespace FunderMaps.Controllers
             return Forbid();
         }
 
-        public sealed class OrganizationInitiationInputModel
+        public sealed class UserInputModel
         {
-            public sealed class UserModel
-            {
-                [Required]
-                [EmailAddress]
-                [DataType(DataType.EmailAddress)]
-                public string Email { get; set; }
-
-                [Required]
-                [DataType(DataType.Password)]
-                public string Password { get; set; }
-            }
-
-            public sealed class OrganizationModel
-            {
-                [Required]
-                public string Address { get; set; }
-
-                [Required]
-                public int AddressNumber { get; set; }
-            }
+            [Required]
+            [EmailAddress]
+            [DataType(DataType.EmailAddress)]
+            public string Email { get; set; }
 
             [Required]
-            public UserModel User { get; set; }
-
-            [Required]
-            public OrganizationModel Organization { get; set; }
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
         }
 
-        // POST: api/organization/proposal/{token}
-        /// <summary>
-        /// Create a new organization from an organization proposal. At the same time a superuser
-        /// for the organization is created.
-        /// </summary>
-        /// <param name="token">Proposal token.</param>
-        /// <param name="input">Organization and superuser information.</param>
-        [AllowAnonymous]
-        [HttpPost("proposal/{token:guid}")]
-        public async Task<IActionResult> FromProposalAsync([FromRoute] Guid token, [FromBody] OrganizationInitiationInputModel input)
-        {
-            var proposal = await _context.OrganizationProposals
-                .Where(s => s.Token == token)
-                .AsNoTracking()
-                .SingleOrDefaultAsync();
-
-            if (proposal == null)
-            {
-                return NotFound();
-            }
-
-            // Create everything at once, or nothing at all if an error occurs.
-            using (var transaction = await _context.Database.BeginTransactionAsync())
-            {
-                var role = await _context.OrganizationRoles
-                    .Where(s => s.Name == Constants.SuperuserRole)
-                    .SingleAsync();
-
-                // Prepare user account
-                var user = new FunderMapsUser(input.User.Email);
-                var result = await _userManager.CreateAsync(user, input.User.Password);
-                if (!result.Succeeded)
-                {
-                    transaction.Rollback();
-                    return Conflict();
-                }
-
-                // Create organization address
-                var address = new Address
-                {
-                    Street = input.Organization.Address,
-                    AddressNumber = input.Organization.AddressNumber,
-                };
-                await _context.Addresses.AddAsync(address);
-                await _context.SaveChangesAsync();
-
-                // Create organization
-                var organization = new Organization
-                {
-                    Name = proposal.Name,
-                    NormalizedName = proposal.NormalizedName,
-                    Email = proposal.Email,
-                    HomeAddress = address,
-                    PostalAddres = address,
-                };
-                await _context.Organizations.AddAsync(organization);
-                await _context.SaveChangesAsync();
-
-                // Attach user to organization
-                await _context.OrganizationUsers.AddAsync(new OrganizationUser
-                {
-                    User = user,
-                    Organization = organization,
-                    OrganizationRole = role,
-                });
-                await _context.SaveChangesAsync();
-
-                // Remove proposal
-                _context.OrganizationProposals.Remove(proposal);
-                await _context.SaveChangesAsync();
-
-                transaction.Commit();
-
-                return Ok(organization);
-            }
-        }
-
+        // FUTURE: Only administrators or superusers
         // POST: api/organization/{id}/new_superuser
         [HttpPost("{id:guid}/new_superuser")]
-        public async Task<IActionResult> NewSuperUserAsync(Guid id, [FromBody] FunderMapsUser user)
+        public async Task<IActionResult> NewSuperUserAsync(Guid id, [FromBody] UserInputModel input)
         {
             var organization = await _context.Organizations.FindAsync(id);
 
-            // Prepare user account
-            var result = await _userManager.CreateAsync(user);
-            if (!result.Succeeded)
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, organization, "OrganizationRolePolicy");
+            if (authorizationResult.Succeeded)
             {
-                return Conflict();
+                // Create everything at once, or nothing at all if an error occurs.
+                using (var transaction = await _context.Database.BeginTransactionAsync())
+                {
+                    var role = await _context.OrganizationRoles
+                        .Where(s => s.Name == Constants.SuperuserRole)
+                        .SingleAsync();
+
+                    // Prepare user account
+                    var user = new FunderMapsUser(input.Email);
+                    var result = await _userManager.CreateAsync(user, input.Password);
+                    if (!result.Succeeded)
+                    {
+                        transaction.Rollback();
+                        return Conflict();
+                    }
+
+                    // Attach user to organization
+                    _context.OrganizationUsers.Add(new OrganizationUser
+                    {
+                        User = user,
+                        Organization = organization,
+                        OrganizationRole = role,
+                    });
+                    await _context.SaveChangesAsync();
+
+                    transaction.Commit();
+
+                    return Ok();
+                }
             }
 
-            // Attach user to organization
-            _context.OrganizationUsers.Add(new OrganizationUser
-            {
-                User = user,
-                Organization = organization,
-            });
-            await _context.SaveChangesAsync();
-
-            return Ok();
+            return Forbid();
         }
 
         // PUT: api/organization
         [HttpPut]
         public async Task<IActionResult> PutAsync([FromBody] Organization organization)
         {
-            //var authorizationResult = await _authorizationService.AuthorizeAsync(User, organization, "");
-            //if (authorizationResult.Succeeded)
-            //{
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, organization, "OrganizationMemberPolicy");
+            if (authorizationResult.Succeeded)
+            {
                 _context.Organizations.Update(organization);
                 await _context.SaveChangesAsync();
 
                 return Ok(organization);
-            //}
+            }
 
-            //return new ForbidResult();
+            return Forbid();
         }
     }
 }
---
+++ FunderMaps/Controllers/OrganizationRegistrationController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FunderMaps.Data;
+using FunderMaps.Helpers;
+using FunderMaps.Models;
+using FunderMaps.Models.Identity;
+
+namespace FunderMaps.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationRegistrationController : ControllerBase
+    {
+        private readonly FunderMapsDbContext _context;
+        private readonly UserManager<FunderMapsUser> _userManager;
+
+        public OrganizationRegistrationController(FunderMapsDbContext context, UserManager<FunderMapsUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public sealed class OrganizationInitiationInputModel
+        {
+            public sealed class UserModel
+            {
+                [Required]
+                [EmailAddress]
+                [DataType(DataType.EmailAddress)]
+                public string Email { get; set; }
+
+                [Required]
+                [DataType(DataType.Password)]
+                public string Password { get; set; }
+            }
+
+            public sealed class OrganizationModel
+            {
+                [Required]
+                public string Address { get; set; }
+
+                [Required]
+                public int AddressNumber { get; set; }
+            }
+
+            [Required]
+            public UserModel User { get; set; }
+
+            [Required]
+            public OrganizationModel Organization { get; set; }
+        }
+
+        // POST: api/organization/proposal/{token}
+        /// <summary>
+        /// Create a new organization from an organization proposal. At the same time a superuser
+        /// for the organization is created.
+        /// </summary>
+        /// <param name="token">Proposal token.</param>
+        /// <param name="input">Organization and superuser information.</param>
+        [HttpPost("proposal/{token:guid}")]
+        public async Task<IActionResult> FromProposalAsync([FromRoute] Guid token, [FromBody] OrganizationInitiationInputModel input)
+        {
+            var proposal = await _context.OrganizationProposals
+                .Where(s => s.Token == token)
+                .AsNoTracking()
+                .SingleOrDefaultAsync();
+
+            if (proposal == null)
+            {
+                return NotFound();
+            }
+
+            // Create everything at once, or nothing at all if an error occurs.
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                var role = await _context.OrganizationRoles
+                    .Where(s => s.Name == Constants.SuperuserRole)
+                    .SingleAsync();
+
+                // Prepare user account
+                var user = new FunderMapsUser(input.User.Email);
+                var result = await _userManager.CreateAsync(user, input.User.Password);
+                if (!result.Succeeded)
+                {
+                    transaction.Rollback();
+                    return Conflict();
+                }
+
+                // Create organization address
+                var address = new Address
+                {
+                    Street = input.Organization.Address,
+                    AddressNumber = input.Organization.AddressNumber,
+                };
+                await _context.Addresses.AddAsync(address);
+                await _context.SaveChangesAsync();
+
+                // Create organization
+                var organization = new Organization
+                {
+                    Name = proposal.Name,
+                    NormalizedName = proposal.NormalizedName,
+                    Email = proposal.Email,
+                    HomeAddress = address,
+                    PostalAddres = address,
+                };
+                await _context.Organizations.AddAsync(organization);
+                await _context.SaveChangesAsync();
+
+                // Attach user to organization
+                await _context.OrganizationUsers.AddAsync(new OrganizationUser
+                {
+                    User = user,
+                    Organization = organization,
+                    OrganizationRole = role,
+                });
+                await _context.SaveChangesAsync();
+
+                // Remove proposal
+                _context.OrganizationProposals.Remove(proposal);
+                await _context.SaveChangesAsync();
+
+                transaction.Commit();
+
+                return Ok(organization);
+            }
+        }
+    }
+}
