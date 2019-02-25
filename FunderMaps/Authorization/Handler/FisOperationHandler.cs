86ac9c4a68f5e57320e021d03f68dfb47977b2c5
Yorick de Wid <yorick17@outlook.com>
2019-02-25 17:22:10 +0100

    Use authorization handler for FIS database access

---
+++ FunderMaps/Authorization/Handler/FisOperationHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using FunderMaps.Helpers;
+using FunderMaps.Authorization.Requirement;
+using FunderMaps.Models.Fis;
+using FunderMaps.Data.Authorization;
+
+namespace FunderMaps.Authorization.Handler
+{
+    public class FisOperationHandler : AuthorizationHandler<OperationAuthorizationRequirement, Report>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+            OperationAuthorizationRequirement requirement,
+            Report report)
+        {
+            // Administrator role can operate on anything
+            if (context.User.IsInRole(Constants.AdministratorRole))
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+
+            // User must have organization claim on this resource
+            if (!context.User.HasClaim(FisClaimTypes.OrganizationAttestationIdentifier, report.Owner.ToString()))
+            {
+                return Task.CompletedTask;
+            }
+
+            // User must have certain organization role for operation
+            var organizationClaim = context.User.Claims.Where(s => s.Type == FisClaimTypes.OrganizationUserRole).First();
+            switch (organizationClaim.Value)
+            {
+                case Constants.SuperuserRole:
+                    if (requirement.Name == OperationsRequirement.Read.Name
+                        || requirement.Name == OperationsRequirement.Create.Name
+                        || requirement.Name == OperationsRequirement.Update.Name
+                        || requirement.Name == OperationsRequirement.Delete.Name
+                        || requirement.Name == OperationsRequirement.Validate.Name)
+                    {
+                        context.Succeed(requirement);
+                    }
+                    break;
+                case Constants.WriterRole:
+                    if (requirement.Name == OperationsRequirement.Read.Name
+                        || requirement.Name == OperationsRequirement.Create.Name
+                        || requirement.Name == OperationsRequirement.Update.Name)
+                    {
+                        context.Succeed(requirement);
+                    }
+                    break;
+                case Constants.ReaderRole:
+                    if (requirement.Name == OperationsRequirement.Read.Name)
+                    {
+                        context.Succeed(requirement);
+                    }
+                    break;
+                case Constants.VerifierRole:
+                    if (requirement.Name == OperationsRequirement.Read.Name
+                        || requirement.Name == OperationsRequirement.Create.Name
+                        || requirement.Name == OperationsRequirement.Update.Name
+                        || requirement.Name == OperationsRequirement.Validate.Name)
+                    {
+                        context.Succeed(requirement);
+                    }
+                    break;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
