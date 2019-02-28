4b247c79cc651ff69a942c511a14f27400906b33
Yorick de Wid <yorick17@outlook.com>
2019-02-28 13:51:22 +0100

    Return serverside error as JSON

--- FunderMaps/Controllers/ErrorController.cs
+++ FunderMaps/Controllers/ErrorController.cs
@@ -1,18 +1,27 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using FunderMaps.Models;
 
 namespace FunderMaps.Controllers
 {
     public class ErrorController : Controller
     {
-        // GET: Error
-        public ActionResult Error()
+        public sealed class ErrorOutoutModel
         {
-            return View(new ErrorViewModel
+            public string Title { get; set; }
+            public int Status { get; set; }
+            public string TraceId { get; set; }
+        }
+
+        // GET: oops
+        [Produces("application/json")]
+        public IActionResult Error()
+        {
+            return StatusCode(500, new ErrorOutoutModel
             {
-                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                Title = "An error has occured on the remote side",
+                Status = 500,
+                TraceId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
             });
         }
     }
