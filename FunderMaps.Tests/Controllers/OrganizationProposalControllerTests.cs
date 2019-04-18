de64a9d6ed89a844ba4db15647134a40d04fa9c6
Yorick de Wid <yorick17@outlook.com>
2019-04-18 16:56:12 +0200

    Additional testcases for organization proposal controller

--- FunderMaps.Tests/Controllers/OrganizationProposalControllerTests.cs
+++ FunderMaps.Tests/Controllers/OrganizationProposalControllerTests.cs
@@ -77,5 +77,95 @@ namespace FunderMaps.Tests.Controllers
                 Assert.Equal("Resource not found", returnValue.Errors[0].Message);
             }
         }
+
+        [Fact]
+        public async Task Post_ReturnsNewOrganizationProposal()
+        {
+            // Arrange
+            var options = DatabaseWrapper.GenerateDatabase<FunderMapsDbContext>(nameof(Post_ReturnsNewOrganizationProposal));
+
+            Guid token = Guid.NewGuid();
+            OrganizationProposal proposal = new OrganizationProposal("testcomp", "info@example.org")
+            {
+                Token = token,
+            };
+
+            using (var context = new FunderMapsDbContext(options))
+            {
+                var controller = new OrganizationProposalController(context, new LookupNormalizer());
+
+                // Act
+                var result = await controller.PostAsync(proposal);
+
+                // Assert
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                var returnValue = Assert.IsType<OrganizationProposal>(okResult.Value);
+                Assert.Equal(proposal.Name, returnValue.Name);
+                Assert.Equal(proposal.Email, returnValue.Email);
+            }
+        }
+
+        [Fact]
+        public async Task Post_ReturnsConflict_GivenExistingName()
+        {
+            // Arrange
+            var normalizer = new LookupNormalizer();
+            var options = DatabaseWrapper.GenerateDatabase<FunderMapsDbContext>(nameof(Post_ReturnsConflict_GivenExistingName));
+
+            Guid token = Guid.NewGuid();
+            OrganizationProposal proposal = new OrganizationProposal("testcomp", "info@example.org")
+            {
+                Token = token,
+                NormalizedName = normalizer.Normalize("testcomp"),
+            };
+
+            using (var context = new FunderMapsDbContext(options))
+            {
+                await context.OrganizationProposals.AddAsync(proposal);
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new FunderMapsDbContext(options))
+            {
+                var controller = new OrganizationProposalController(context, new LookupNormalizer());
+
+                // Act
+                var result = await controller.PostAsync(proposal);
+
+                // Assert
+                var okResult = Assert.IsType<ConflictObjectResult>(result);
+                Assert.Equal(proposal.Name, okResult.Value);
+            }
+        }
+
+        [Fact]
+        public async Task Delete_ReturnsNothing()
+        {
+            // Arrange
+            var options = DatabaseWrapper.GenerateDatabase<FunderMapsDbContext>(nameof(Delete_ReturnsNothing));
+
+            Guid token = Guid.NewGuid();
+            OrganizationProposal proposal = new OrganizationProposal("testcomp", "info@example.org")
+            {
+                Token = token,
+            };
+
+            using (var context = new FunderMapsDbContext(options))
+            {
+                await context.OrganizationProposals.AddAsync(proposal);
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new FunderMapsDbContext(options))
+            {
+                var controller = new OrganizationProposalController(context, new LookupNormalizer());
+
+                // Act
+                var result = await controller.DeleteAsync(token);
+
+                // Assert
+                var okResult = Assert.IsType<NoContentResult>(result);
+            }
+        }
     }
 }
