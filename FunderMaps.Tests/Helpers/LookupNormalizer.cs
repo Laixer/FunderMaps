1c588c40ac2c7d7ae2d34a46c4324d9712cfa796
Yorick de Wid <yorick17@outlook.com>
2019-04-18 15:41:20 +0200

    Lookup helpers for testcasses

---
+++ FunderMaps.Tests/Helpers/LookupNormalizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace FunderMaps.Tests.Helpers
+{
+    public class LookupNormalizer : ILookupNormalizer
+    {
+        public string Normalize(string key)
+        {
+            return key.Normalize().ToLowerInvariant();
+        }
+    }
+}
