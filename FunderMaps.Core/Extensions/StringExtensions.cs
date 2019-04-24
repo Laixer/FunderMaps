4269f69b9837221b8f4746d09db70e5a938a9a5a
Yorick de Wid <yorick17@outlook.com>
2019-04-24 23:23:31 +0200

    Convert snake case string to lower underscope

---
+++ FunderMaps.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FunderMaps.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToUnderscore(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentException("message", nameof(str));
+            }
+
+            return Regex.Replace(str, "(?<=.)([A-Z])", "_$0", RegexOptions.Compiled).ToLower();
+        }
+    }
+}
