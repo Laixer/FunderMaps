ecbc0f076eee38c698142e4108847fe1db902389
Yorick de Wid <yorick17@outlook.com>
2019-04-25 00:26:05 +0200

    Generic enum to string converter with snake case convention

--- FunderMaps.Core/Entities/Fis/FoundationQuality.cs
+++ FunderMaps.Core/Entities/Fis/FoundationQuality.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 namespace FunderMaps.Core.Entities.Fis
 {
---
+++ FunderMaps/Data/Converters/EnumSnakeCaseConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using FunderMaps.Core.Extensions;
+
+namespace FunderMaps.Data.Converters
+{
+    /// <summary>
+    /// Convert string to enum and back with snake case string conversion.
+    /// </summary>
+    public class EnumSnakeCaseConverter<TEnum> : ValueConverter<TEnum, string>
+        where TEnum : struct, IConvertible
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumSnakeCaseConverter{TEnum}" /> class.
+        /// </summary>
+        public EnumSnakeCaseConverter(ConverterMappingHints mappingHints = null)
+            : base(v => v.ToString().ToUnderscore(),
+                  v => (TEnum)Enum.Parse(typeof(TEnum), v, true),
+                  mappingHints)
+        {
+        }
+
+        /// <summary>
+        /// A <see cref="ValueConverterInfo" /> for the default use of this converter.
+        /// </summary>
+        public static ValueConverterInfo DefaultInfo { get; }
+            = new ValueConverterInfo(typeof(TEnum), typeof(string), i => new EnumSnakeCaseConverter<TEnum>(i.MappingHints));
+    }
+}
