be8bce9ec6e8d45db2c2c827893cc0bd421225d9
Bobby <b.poeran@live.nl>
2019-08-15 15:44:20 +0200

    Year is not nullable

--- src/FunderMaps.Core/Entities/Fis/FoundationRecovery.cs
+++ src/FunderMaps.Core/Entities/Fis/FoundationRecovery.cs
@@ -24,7 +24,7 @@ namespace FunderMaps.Core.Entities.Fis
         public FoundationRecoveryType Type { get; set; }
 
         [Required]
-        public short? Year { get; set; }
+        public short Year { get; set; }
 
         [Required]
         public Guid Address { get; set; }
