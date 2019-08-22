87a70470a7d82a4bbc9a25ab7fe9b60c6a707b81
Yorick de Wid <yorick17@outlook.com>
2019-08-22 10:04:26 +0200

    Confiure dapper datastore

---
+++ contrib/Laixer.Identity.Dapper/Extensions/NpgsqIdentityDapperOptionsExtensions.cs
@@ -0,0 +1,58 @@
+﻿namespace Laixer.Identity.Dapper.Extensions
+{
+    public static class NpgsqIdentityDapperOptionsExtensions
+    {
+        /// <summary>
+        /// Adds Npgsql as database driver.
+        /// </summary>
+        /// <param name="options">The <see cref="IdentityDapperOptions"/> instance this method extends.</param>
+        /// <param name="connectionString">Database connection string.</param>
+        /// <returns>The <see cref="IdentityDapperOptions"/> instance this method extends.</returns>
+        public static IdentityDapperOptions UseNpgsql(this IdentityDapperOptions options, string connectionString)
+        {
+            options.ConnectionString = connectionString;
+
+            options.Database = new Database.Npgsql(options);
+
+            return options;
+        }
+
+        /// <summary>
+        /// Adds Npgsql as database driver.
+        /// </summary>
+        /// <param name="options">The <see cref="IdentityDapperOptions"/> instance this method extends.</param>
+        /// <param name="connectionString">Database connection string.</param>
+        /// <param name="schema">Database schema.</param>
+        /// <returns>The <see cref="IdentityDapperOptions"/> instance this method extends.</returns>
+        public static IdentityDapperOptions UseNpgsql(this IdentityDapperOptions options, string connectionString, string schema)
+        {
+            options.ConnectionString = connectionString;
+            options.Schema = schema;
+
+            options.Database = new Database.Npgsql(options);
+
+            return options;
+        }
+
+        /// <summary>
+        /// Adds Npgsql as database driver.
+        /// </summary>
+        /// <param name="options">The <see cref="IdentityDapperOptions"/> instance this method extends.</param>
+        /// <param name="connectionString">Database connection string.</param>
+        /// <param name="schema">Database schema.</param>
+        /// <param name="userTable">Databse user store table.</param>
+        /// <param name="roleTable">Databse role store table.</param>
+        /// <returns>The <see cref="IdentityDapperOptions"/> instance this method extends.</returns>
+        public static IdentityDapperOptions UseNpgsql(this IdentityDapperOptions options, string connectionString, string schema, string userTable, string roleTable)
+        {
+            options.ConnectionString = connectionString;
+            options.Schema = schema;
+            options.UserTable = userTable;
+            options.RoleTable = roleTable;
+
+            options.Database = new Database.Npgsql(options);
+
+            return options;
+        }
+    }
+}
---
+++ contrib/Laixer.Identity.Dapper/IdentityDapperOptions.cs
@@ -0,0 +1,35 @@
+﻿using Laixer.Identity.Dapper.Database;
+
+namespace Laixer.Identity.Dapper
+{
+    /// <summary>
+    /// Identity Dapper database configuration.
+    /// </summary>
+    public class IdentityDapperOptions
+    {
+        /// <summary>
+        /// Database connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Database schema.
+        /// </summary>
+        public string Schema { get; set; }
+
+        /// <summary>
+        /// User store table.
+        /// </summary>
+        public string UserTable { get; set; } = "AspNetUsers";
+
+        /// <summary>
+        /// Role store table.
+        /// </summary>
+        public string RoleTable { get; set; } = "AspNetUserRoles";
+
+        /// <summary>
+        /// Database driver for the connection.
+        /// </summary>
+        public IDatabaseDriver Database { get; set; }
+    }
+}
