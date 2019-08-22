d729a1dbd8892d1a40187552d7df123e0581ebad
Yorick de Wid <yorick17@outlook.com>
2019-08-22 11:03:15 +0200

    Npgsql driver

---
+++ contrib/Laixer.Identity.Dapper/Database/Npgsql.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Laixer.Identity.Dapper.Database
+{
+    /// <summary>
+    /// Driver for PostgreSQL driver.
+    /// </summary>
+    internal class Npgsql : IDatabaseDriver
+    {
+        private readonly IdentityDapperOptions _options;
+
+        /// <summary>
+        /// Create new instance.
+        /// </summary>
+        /// <param name="options">The <see cref="IdentityDapperOptions"/> instance.</param>
+        public Npgsql(IdentityDapperOptions options)
+        {
+            _options = options;
+
+            // Set the default schema for PostgreSQL
+            if (string.IsNullOrEmpty(_options.Schema))
+            {
+                _options.Schema = "public";
+            }
+        }
+
+        /// <summary>
+        /// Get the database connection.
+        /// </summary>
+        /// <returns>Instance of <see cref="NpgsqlConnection"/>.</returns>
+        public IDbConnection GetDbConnection() => new NpgsqlConnection(_options.ConnectionString);
+    }
+}
