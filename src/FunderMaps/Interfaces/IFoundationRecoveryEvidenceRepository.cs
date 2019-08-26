2282cf03c8712ceabcdcb2703fd22d107f4c986a
Bobby <b.poeran@live.nl>
2019-08-26 15:22:04 +0200

    Created recovery evidence controller + interface + repo
    Also simplefied put and post of the recovery controller by passing the input after check to the repository

---
+++ src/FunderMaps/Interfaces/IFoundationRecoveryEvidenceRepository.cs
@@ -0,0 +1,29 @@
+﻿using FunderMaps.Core.Entities.Fis;
+using FunderMaps.Core.Interfaces;
+using FunderMaps.Core.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FunderMaps.Interfaces
+{
+    /// <summary>
+    /// Interface for the foundation recovery evidence repository
+    /// </summary>
+    public interface IFoundationRecoveryEvidenceRepository : IAsyncRepository<FoundationRecoveryEvidence, string>
+    {
+        /// <summary>
+        /// Generate a list of evidence items based on the organization id
+        /// </summary>
+        /// <param name="org_id"></param>
+        /// <param name="navigation"></param>
+        /// <returns></returns>
+        Task<IReadOnlyList<FoundationRecoveryEvidence>> ListAllAsync(int org_id, Navigation navigation);
+
+        /// <summary>
+        /// Return the amount of reports counts based on the organization id
+        /// </summary>
+        /// <param name="org_id"></param>
+        /// <returns></returns>
+        Task<uint> CountAsync(int org_id);
+    }
+}
