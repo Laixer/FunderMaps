8e6d7e0f1e0267ba2135e1e9ec8c98f35bc95239
Yorick de Wid <yorick17@outlook.com>
2019-08-30 15:06:42 +0200

    Project settings

--- src/FunderMaps/Startup.cs
+++ src/FunderMaps/Startup.cs
@@ -21,7 +21,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
