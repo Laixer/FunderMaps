c0bf4fc073ed75113a79bc86eea735f88e141c38
Yorick de Wid <yorick17@outlook.com>
2019-08-30 15:31:32 +0200

    Documentation

--- src/FunderMaps.Core/Entities/BaseEntity.cs
+++ src/FunderMaps.Core/Entities/BaseEntity.cs
@@ -4,10 +4,8 @@
     /// Base for all entities.
     /// </summary>
     /// <remarks>
-    /// This can easily be modified to be BaseEntity<T> and public T Id to support different key types.
+    /// This can easily be modified to be BaseEntity and public T Id to support different key types.
     /// Using non-generic integer types for simplicity and to ease caching logic.
     /// </remarks>
-    public class BaseEntity
-    {
-    }
+    public class BaseEntity { }
 }
---
+++ src/FunderMaps.Core/Entities/BaseLevel.cs
@@ -0,0 +1,23 @@
+﻿namespace FunderMaps.Core.Entities
+{
+    /// <summary>
+    /// Base measurement level.
+    /// </summary>
+    public enum BaseLevel
+    {
+        /// <summary>
+        /// Normaal Amsterdams Peil.
+        /// </summary>
+        NAP,
+
+        /// <summary>
+        /// Tweede Algemene Waterpassing.
+        /// </summary>
+        TAW,
+
+        /// <summary>
+        /// Normalnull.
+        /// </summary>
+        NN,
+    }
+}
