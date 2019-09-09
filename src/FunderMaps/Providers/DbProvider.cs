afed288292fb36f5493b2250a8d385f7d289d2dc
Yorick de Wid <yorick17@outlook.com>
2019-09-09 16:25:54 +0200

    Fetch connection string from Env
    This should have been done by Microsoft.Extensions.Configuration.EnvironmentVariables, but due to a bug in ASP.NET Core there is no support for PostgreSQL. See attached pull request

--- src/FunderMaps/Providers/DbProvider.cs
+++ src/FunderMaps/Providers/DbProvider.cs
@@ -19,6 +19,9 @@ namespace FunderMaps.Providers
         /// </summary>
         public IConfiguration Configuration { get; }
 
+        /// <summary>
+        /// Static initializer.
+        /// </summary>
         static DbProvider()
         {
             DefaultTypeMap.MatchNamesWithUnderscores = true;
@@ -34,6 +37,25 @@ namespace FunderMaps.Providers
             Configuration = configuration;
             _options = options?.Value;
             connectionString = Configuration.GetConnectionString(_options.ConnectionStringName);
+
+            // FUTURE: There is no support for PostgreSQL in the configuration as of yet.
+            //         Solution is at Microsoft.Extensions.Configuration.EnvironmentVariables.EnvironmentVariablesConfigurationProvider.cs
+            //         Proposal: https://github.com/aspnet/Extensions/pull/1695
+
+            if (connectionString != null) { return; }
+            connectionString = Configuration.GetValue<string>($"POSTGRESQLCONNSTR_{_options.ConnectionStringName}");
+
+            if (connectionString != null) { return; }
+            connectionString = Configuration.GetValue<string>($"MYSQLCONNSTR_{_options.ConnectionStringName}");
+
+            if (connectionString != null) { return; }
+            connectionString = Configuration.GetValue<string>($"SQLAZURECONNSTR_{_options.ConnectionStringName}");
+
+            if (connectionString != null) { return; }
+            connectionString = Configuration.GetValue<string>($"SQLCONNSTR_{_options.ConnectionStringName}");
+
+            if (connectionString != null) { return; }
+            connectionString = Configuration.GetValue<string>($"CUSTOMCONNSTR_{_options.ConnectionStringName}");
         }
 
         /// <summary>
