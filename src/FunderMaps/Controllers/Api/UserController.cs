8f3ecffbb7bbea8fac56013d3ed42f54bb2877f3
Yorick de Wid <yorick17@outlook.com>
2019-11-07 20:49:33 +0100

    Semantics

--- src/FunderMaps/Controllers/Api/UserController.cs
+++ src/FunderMaps/Controllers/Api/UserController.cs
@@ -22,7 +22,6 @@ namespace FunderMaps.Controllers.Api
         /// Create new instance.
         /// </summary>
         /// <param name="userManager">See <see cref="UserManager{TUser}"/>.</param>
-        /// <param name="eventBus">Event bus.</param>
         public UserController(UserManager<FunderMapsUser> userManager)
         {
             _userManager = userManager;
