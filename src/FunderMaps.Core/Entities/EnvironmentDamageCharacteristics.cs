218db7d042ecbf6e71b11534f3cfdd501d1b4bf8
Yorick de Wid <yorick17@outlook.com>
2020-01-17 14:22:10 +0100

    Types for incidents

---
+++ src/FunderMaps.Core/Entities/EnvironmentDamageCharacteristics.cs
@@ -0,0 +1,63 @@
+﻿namespace FunderMaps.Core.Entities
+{
+    /// <summary>
+    /// Environment damage characteristics.
+    /// </summary>
+    public enum EnvironmentDamageCharacteristics
+    {
+        /// <summary>
+        /// Subsidence.
+        /// </summary>
+        Subsidence,
+
+        /// <summary>
+        /// Sagging cewer connections.
+        /// </summary>
+        SaggingSewerConnection,
+
+        /// <summary>
+        /// Sagging cables and pipes.
+        /// </summary>
+        SaggingCablesPipes,
+
+        /// <summary>
+        /// Flooding.
+        /// </summary>
+        Flooding,
+
+        /// <summary>
+        /// Foundation damage nearby.
+        /// </summary>
+        FoundationDamageNearby,
+
+        /// <summary>
+        /// Elavation.
+        /// </summary>
+        Elavation,
+
+        /// <summary>
+        /// Increasing traffic.
+        /// </summary>
+        IncreasingTraffic,
+
+        /// <summary>
+        /// Construction nearby.
+        /// </summary>
+        ConstructionNearby,
+
+        /// <summary>
+        /// Vegetation nearby.
+        /// </summary>
+        VegetationNearby,
+
+        /// <summary>
+        /// Sewage leakage.
+        /// </summary>
+        SewageLeakage,
+
+        /// <summary>
+        /// Low ground water.
+        /// </summary>
+        LowGroundWater,
+    }
+}
