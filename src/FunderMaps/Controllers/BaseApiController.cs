ce1102b2fe0c3ddc72e5faf8f79cedcebe5d13c2
Yorick de Wid <yorickdewid@users.noreply.github.com>
2020-02-11 21:30:42 +0100

    Some refactoring

--- src/FunderMaps/Controllers/BaseApiController.cs
+++ src/FunderMaps/Controllers/BaseApiController.cs
@@ -15,7 +15,8 @@ namespace FunderMaps.Controllers
         /// <param name="code">Error code.</param>
         /// <param name="message">Error message.</param>
         /// <returns>ActionResult.</returns>
-        protected ActionResult Unauthorized(int code, string message) => StatusCode(401, new ErrorOutputModel(code, message));
+        protected ActionResult Unauthorized(int code, string message) =>
+            StatusCode(401, new ErrorOutputModel(code, message));
 
         /// <summary>
         /// Return bad request with an error model.
@@ -23,14 +24,16 @@ namespace FunderMaps.Controllers
         /// <param name="code">Error code.</param>
         /// <param name="message">Error message.</param>
         /// <returns>ActionResult.</returns>
-        protected ActionResult BadRequest(int code, string message) => StatusCode(400, new ErrorOutputModel(code, message));
+        protected ActionResult BadRequest(int code, string message) =>
+            StatusCode(400, new ErrorOutputModel(code, message));
 
         /// <summary>
         /// Return bad request with an existing error model.
         /// </summary>
         /// <param name="model">Error model.</param>
         /// <returns>ActionResult.</returns>
-        protected ActionResult BadRequest(ErrorOutputModel model) => StatusCode(400, model);
+        protected ActionResult BadRequest(ErrorOutputModel model) =>
+            StatusCode(400, model);
 
         /// <summary>
         /// Return not found with an error model.
@@ -38,13 +41,15 @@ namespace FunderMaps.Controllers
         /// <param name="code">Error code.</param>
         /// <param name="message">Error message.</param>
         /// <returns>ActionResult.</returns>
-        protected ActionResult NotFound(int code, string message) => StatusCode(404, new ErrorOutputModel(code, message));
+        protected ActionResult NotFound(int code, string message) =>
+            StatusCode(404, new ErrorOutputModel(code, message));
 
         /// <summary>
         /// Return default error model with resource not found.
         /// </summary>
         /// <returns>ActionResult.</returns>
-        protected ActionResult ResourceNotFound() => NotFound(0, "Resource not found");
+        protected ActionResult ResourceNotFound() =>
+            NotFound(0, "Resource not found");
 
         /// <summary>
         /// Return conflict with an error model.
@@ -52,13 +57,15 @@ namespace FunderMaps.Controllers
         /// <param name="code">Error code.</param>
         /// <param name="message">Error message.</param>
         /// <returns>ActionResult.</returns>
-        protected ActionResult Conflict(int code, string message) => StatusCode(409, new ErrorOutputModel(code, message));
+        protected ActionResult Conflict(int code, string message) =>
+            StatusCode(409, new ErrorOutputModel(code, message));
 
         /// <summary>
         /// Return default error model with resource already exists.
         /// </summary>
         /// <returns>ActionResult.</returns>
-        protected ActionResult ResourceExists() => Conflict(0, "Resource already exists");
+        protected ActionResult ResourceExists() =>
+            Conflict(0, "Resource already exists");
 
         /// <summary>
         /// Return forbidden with an error model.
@@ -66,13 +73,15 @@ namespace FunderMaps.Controllers
         /// <param name="code">Error code.</param>
         /// <param name="message">Error message.</param>
         /// <returns>ActionResult.</returns>
-        protected ActionResult Forbid(int code, string message) => StatusCode(403, new ErrorOutputModel(code, message));
+        protected ActionResult Forbid(int code, string message) =>
+            StatusCode(403, new ErrorOutputModel(code, message));
 
         /// <summary>
         /// Return default error model with resource already exists.
         /// </summary>
         /// <returns>ActionResult.</returns>
-        protected ActionResult ResourceForbid() => Forbid(0, "Resource access forbidden with current principal");
+        protected ActionResult ResourceForbid() =>
+            Forbid(0, "Resource access forbidden with current principal");
 
         /// <summary>
         /// Return application error with an error model.
@@ -80,7 +89,7 @@ namespace FunderMaps.Controllers
         /// <param name="code">Error code.</param>
         /// <param name="message">Error message.</param>
         /// <returns>ActionResult.</returns>
-        protected ActionResult ApplicationError(int code = 0, string message = "Something went wrong during request processing")
-            => StatusCode(500, new ErrorOutputModel(code, message));
+        protected ActionResult ApplicationError(int code = 0, string message = "Something went wrong during request processing") =>
+            StatusCode(500, new ErrorOutputModel(code, message));
     }
 }
