cb74a98d0d5c5bee2de6fc02580eec712acb958a
Yorick de Wid <yorick17@outlook.com>
2020-02-11 17:01:05 +0100

    Correct typo

--- src/FunderMaps.Core/Entities/FoundationDamageCause.cs
+++ src/FunderMaps.Core/Entities/FoundationDamageCause.cs
@@ -58,7 +58,7 @@
         /// <summary>
         /// Foundation flaw.
         /// </summary>
-        FoundationFaw,
+        FoundationFlaw,
 
         /// <summary>
         /// Construnction heave.
