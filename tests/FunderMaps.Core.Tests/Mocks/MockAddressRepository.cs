80a0346723624856721181d64f9d5ee5fa241aa8
Yorick de Wid <yorick17@outlook.com>
2020-07-17 13:10:14 +0200

    Create MockAddressRepository.cs

---
+++ tests/FunderMaps.Core.Tests/Mocks/MockAddressRepository.cs
@@ -0,0 +1,16 @@
+﻿using FunderMaps.Core.Interfaces.Repositories;
+using Moq;
+
+namespace FunderMaps.Core.Tests.Mocks
+{
+    internal class MockAddressRepository : Mock<IAddressRepository>
+    {
+        //public MockIncidentRepository MockGetByIdAsync(Incident result)
+        //{
+        //    Setup(s => s.GetByIdAsync(It.IsAny<string>()))
+        //        .Returns(new ValueTask<Incident>(result));
+
+        //    return this;
+        //}
+    }
+}
