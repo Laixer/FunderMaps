e3e9e129d56e354b103325022e018fb1db2bd422
Yorick de Wid <yorick17@outlook.com>
2020-07-18 13:12:13 +0200

    Testcases

---
+++ tests/FunderMaps.Core.Tests/DataAnnotations/AddressAttributeTests.cs
@@ -0,0 +1,38 @@
+﻿using FunderMaps.Core.DataAnnotations;
+using Xunit;
+
+namespace FunderMaps.Core.Tests.DataAnnotations
+{
+    public class AddressAttributeTests
+    {
+        [Fact]
+        public void IsValidOnInput()
+        {
+            // Arrange
+            var attr = new AddressAttribute();
+
+            // Act
+            bool valid = attr.IsValid("gfm-12345");
+
+            // Assert
+            Assert.True(valid);
+        }
+
+        [Fact]
+        public void IsInvalidOnInput()
+        {
+            // Arrange
+            var attr = new AddressAttribute();
+
+            // Act
+            bool valid = attr.IsValid("ggfm-12345");
+            bool valid2 = attr.IsValid("gfm12345");
+            bool valid3 = attr.IsValid("GFM-12345");
+
+            // Assert
+            Assert.False(valid);
+            Assert.False(valid2);
+            Assert.False(valid3);
+        }
+    }
+}
---
+++ tests/FunderMaps.Core.Tests/DataAnnotations/IncidentAttributeTests.cs
@@ -0,0 +1,38 @@
+﻿using FunderMaps.Core.DataAnnotations;
+using Xunit;
+
+namespace FunderMaps.Core.Tests.DataAnnotations
+{
+    public class IncidentAttributeTests
+    {
+        [Fact]
+        public void IsValidOnInput()
+        {
+            // Arrange
+            var attr = new IncidentAttribute();
+
+            // Act
+            bool valid = attr.IsValid("FIR-12345");
+
+            // Assert
+            Assert.True(valid);
+        }
+
+        [Fact]
+        public void IsInvalidOnInput()
+        {
+            // Arrange
+            var attr = new AddressAttribute();
+
+            // Act
+            bool valid = attr.IsValid("FI-12345");
+            bool valid2 = attr.IsValid("FIR12345");
+            bool valid3 = attr.IsValid("fir-12345");
+
+            // Assert
+            Assert.False(valid);
+            Assert.False(valid2);
+            Assert.False(valid3);
+        }
+    }
+}
