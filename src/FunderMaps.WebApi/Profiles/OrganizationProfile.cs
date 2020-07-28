e5f68cec903d5858158b81920a80dd7a429725f5
Yorick de Wid <yorick17@outlook.com>
2020-07-28 14:06:03 +0200

    Organization controller

---
+++ src/FunderMaps.WebApi/Profiles/OrganizationProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FunderMaps.Core.Entities;
+using FunderMaps.WebApi.DataTransferObjects;
+
+namespace FunderMaps.WebApi.Profiles
+{
+    /// <summary>
+    ///     Mapping profile.
+    /// </summary>
+    public class OrganizationProfile : Profile
+    {
+        /// <summary>
+        ///     Create new instance.
+        /// </summary>
+        public OrganizationProfile()
+            => CreateMap<Organization, OrganizationDto>().ReverseMap();
+    }
+}
