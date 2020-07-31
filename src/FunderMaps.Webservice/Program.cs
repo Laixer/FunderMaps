899f4cbdeb053fa95a91651da7d9bcd09510d98b
Yorick de Wid <yorick17@outlook.com>
2020-08-01 01:29:18 +0200

    Mirror project config

--- src/FunderMaps.Webservice/Program.cs
+++ src/FunderMaps.Webservice/Program.cs
@@ -1,21 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace FunderMaps.Webservice
 {
-    public class Program
+    /// <summary>
+    ///     Application entry.
+    /// </summary>
+    public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        /// <summary>
+        ///     Application entry point.
+        /// </summary>
+        /// <param name="args">Commandline arguments.</param>
+        public static Task Main(string[] args)
+            => CreateHostBuilder(args).Build().RunAsync();
 
+        /// <summary>
+        ///     Build a host and run the application.
+        /// </summary>
+        /// <remarks>
+        ///     The signature of this method should not be changed.
+        ///     External tooling expects this function be present.
+        /// </remarks>
+        /// <param name="args">Commandline arguments.</param>
+        /// <returns>See <see cref="IHostBuilder"/>.</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
--- src/FunderMaps.Webservice/Startup.cs
+++ src/FunderMaps.Webservice/Startup.cs
@@ -1,39 +1,86 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO.Compression;
 
 namespace FunderMaps.Webservice
 {
+    /// <summary>
+    ///     Application configuration.
+    /// </summary>
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary>
+        ///     Configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        ///     Create a new instance.
+        /// </summary>
+        /// <param name="configuration">See <see cref="IConfiguration"/>.</param>
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <remarks>
+        ///     Order is undetermined when configuring services.
+        /// </remarks>
+        /// <param name="services">See <see cref="IServiceCollection"/>.</param>
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddApplicationInsightsTelemetry();
+
+            services.AddControllers(); // TODO: REVIEW: Should we IgnoreNullValues ?
+
+            services.AddResponseCompression(options =>
+            {
+                // NOTE: Compression is disabled by default when serving data
+                // over HTTPS because of BREACH exploit.
+                options.EnableForHttps = true;
+            });
+
+            // Configure compression providers.
+            services
+                .Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Fastest)
+                .Configure<BrotliCompressionProviderOptions>(options => options.Level = CompressionLevel.Fastest);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        ///     This method gets called by the runtime. Use this  method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <remarks>
+        ///     The order in which the pipeline handles request is of importance.
+        /// </remarks>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                //app.UseExceptionHandler("/oops");
+                app.UseHsts();
+            }
+
+            app.UseResponseCompression();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!").ConfigureAwait(false);
-                });
+                // TODO: Set /api endpoint as default
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
