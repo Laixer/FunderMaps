0b266654c65c3a4330db6096159ef53608afe2e4
Yorick de Wid <yorick17@outlook.com>
2020-08-01 11:43:18 +0200

    Test invalid HTTP method

--- tests/FunderMaps.IntegrationTests/VersionsTests.cs
+++ tests/FunderMaps.IntegrationTests/VersionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -21,12 +22,25 @@ namespace FunderMaps.IntegrationTests
             var client = _factory.CreateClient();
 
             // Act
-            var response = await client.GetAsync("/api/version").ConfigureAwait(false);
+            var response = await client.GetAsync(new Uri($"{client.BaseAddress}api/version")).ConfigureAwait(false);
 
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.True(response.Content.Headers.ContentType.ToString().Contains("json", StringComparison.InvariantCultureIgnoreCase));
             Assert.True(response.Content.Headers.ContentType.ToString().Contains("utf-8", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        [Fact]
+        public async Task GetEndpointsReturnMethodNotAllowed()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync(new Uri($"{client.BaseAddress}api/version"), null).ConfigureAwait(false);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.MethodNotAllowed, response.StatusCode);
         }
     }
 }
