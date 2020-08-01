43dfd4be8c198949abdfed4c63ab0d0d645c4041
Yorick de Wid <yorick17@outlook.com>
2020-08-01 17:20:55 +0200

    Update version tests

--- tests/FunderMaps.IntegrationTests/VersionsTests.cs
+++ tests/FunderMaps.IntegrationTests/VersionsTests.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using System;
+﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace FunderMaps.IntegrationTests
 {
-    public class VersionsTests : IClassFixture<WebApplicationFactory<Startup>>
+    public class VersionsTests : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
-        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly CustomWebApplicationFactory<Startup> _factory;
 
-        public VersionsTests(WebApplicationFactory<Startup> factory)
+        public VersionsTests(CustomWebApplicationFactory<Startup> factory)
         {
             _factory = factory;
         }
@@ -21,12 +21,15 @@ namespace FunderMaps.IntegrationTests
             var client = _factory.CreateClient();
 
             // Act
-            var response = await client.GetAsync(new Uri($"{client.BaseAddress}api/version")).ConfigureAwait(false);
+            var response = await client.GetAsync("api/version").ConfigureAwait(false);
 
             // Assert
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.True(response.Content.Headers.ContentType.ToString().Contains("json", StringComparison.InvariantCultureIgnoreCase));
-            Assert.True(response.Content.Headers.ContentType.ToString().Contains("utf-8", StringComparison.InvariantCultureIgnoreCase));
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("json", response.Content.Headers.ContentType.ToString(), StringComparison.InvariantCultureIgnoreCase);
+            Assert.Contains("utf-8", response.Content.Headers.ContentType.ToString(), StringComparison.InvariantCultureIgnoreCase);
+            Assert.True(response.Headers.CacheControl.Public);
+            Assert.NotNull(response.Headers.CacheControl.MaxAge);
         }
 
         [Fact]
@@ -36,10 +39,10 @@ namespace FunderMaps.IntegrationTests
             var client = _factory.CreateClient();
 
             // Act
-            var response = await client.PostAsync(new Uri($"{client.BaseAddress}api/version"), null).ConfigureAwait(false);
+            var response = await client.PostAsync("api/version", null).ConfigureAwait(false);
 
             // Assert
-            Assert.Equal(System.Net.HttpStatusCode.MethodNotAllowed, response.StatusCode);
+            Assert.Equal(HttpStatusCode.MethodNotAllowed, response.StatusCode);
         }
     }
 }
