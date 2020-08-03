01aa993407a6fcf9c687e76c11857303a128d2ca
Yorick de Wid <yorick17@outlook.com>
2020-08-03 13:14:21 +0200

    Not Found integration tests

---
+++ tests/FunderMaps.IntegrationTests/Application/ErrorTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunderMaps.IntegrationTests.Application
+{
+    public class ErrorTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public ErrorTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/api")]
+        [InlineData("/api/does-not-exist")]
+        public async Task GetEndpointsReturnNotFound(string uri)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(uri).ConfigureAwait(false);
+
+            // Assert
+            Assert.False(response.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
