7e32d949ec0f1c0c4779a54dc289a2a3bea4f401
Thomas Beckers <tabeckers95@gmail.com>
2020-08-05 13:47:04 +0200

    Update ProductTypeMapper.cs

--- src/FunderMaps.Webservice/Mapping/ProductTypeMapper.cs
+++ src/FunderMaps.Webservice/Mapping/ProductTypeMapper.cs
@@ -1,48 +1,85 @@
-﻿using FunderMaps.Webservice.Core.Enums;
-using FunderMaps.Webservice.Enums;
-using FunderMaps.Webservice.Exceptions;
+﻿using FunderMaps.Core.Exceptions;
+using FunderMaps.Core.Types.Products;
+using FunderMaps.Webservice.ResponseModels;
+using FunderMaps.Webservice.ResponseModels.Analysis;
 using FunderMaps.Webservice.Utility;
+using System;
 
 namespace FunderMaps.Webservice.Mapping
 {
-
     /// <summary>
     /// Contains mapping functionality between <see cref="ProductType"/>s 
     /// and their respective names.
+    /// TODO How to do this elegantly? Also see <see cref="Translation.ProductTypeTranslator"/>.
     /// </summary>
     internal static class ProductTypeMapper
     {
         /// <summary>
         /// Maps a given <paramref name="name"/> to the corresponding <see cref="ProductType"/>.
-        /// TODO Niet hardcoden.
-        /// TODO Hoe doen we dit elegant?
         /// </summary>
         /// <remarks>
         /// This changes the <paramref name="name"/> to lower case before comparing.
         /// </remarks>
         /// <param name="name"><see cref="Product"/> name</param>
         /// <returns><see cref="ProductType"/></returns>
-        public static ProductType Map(string name)
+        public static AnalysisProductType MapAnalysisFromString(string name)
         {
             name.ThrowIfNullOrEmpty();
-            name = name.ToUpperInvariant();
-            return name switch
+            return name.ToUpperInvariant() switch
             {
-                "HERSTEL" => ProductType.StatisticsBuildingsRestored,
-                "MELDINGEN" => ProductType.StatisticsIncidents,
-                "ONDERZOEKEN" => ProductType.StatisticsReports,
-                "BOUWJAREN" => ProductType.StatisticsConstructionYears,
-                "VASTGESTELDE_GEGEVENS" => ProductType.StatisticsDataCollected,
-                "FUNDERING_VERHOUDING" => ProductType.StatisticsFoundationRatio,
-                "FUNDERING_RISICO" => ProductType.StatisticsFoundationRisk,
-                "PAND" => ProductType.Building,
-                "KOSTEN" => ProductType.Costs,
-                "BESCHRIJVING" => ProductType.Description,
-                "FUNDERING" => ProductType.Foundation,
-                "FUNDERING_PLUS" => ProductType.FoundationPlus,
-                "VOLLEDIG" => ProductType.Complete,
-                "RISICO" => ProductType.Risk,
+                "BUILDINGDATA" => AnalysisProductType.BuildingData,
+                "COSTS" => AnalysisProductType.Costs,
+                "DESCRIPTION" => AnalysisProductType.BuildingDescription,
+                "FOUNDATION" => AnalysisProductType.Foundation,
+                "FOUNDATIONPLUS" => AnalysisProductType.FoundationPlus,
+                "COMPLETE" => AnalysisProductType.Complete,
+                "RISK" => AnalysisProductType.Risk,
                 _ => throw new ProductNotFoundException(nameof(name)),
+            };
+        }
+
+        /// <summary>
+        /// Maps a given <paramref name="name"/> to the corresponding <see cref="ProductType"/>.
+        /// </summary>
+        /// <remarks>
+        /// This changes the <paramref name="name"/> to lower case before comparing.
+        /// </remarks>
+        /// <param name="name"><see cref="Product"/> name</param>
+        /// <returns><see cref="StatisticsProductType"/></returns>
+        public static StatisticsProductType MapStatisticsFromString(string name)
+        {
+            name.ThrowIfNullOrEmpty();
+            return name.ToUpperInvariant() switch
+            {
+                "RESTORATION" => StatisticsProductType.BuildingsRestored,
+                "INCIDENTS" => StatisticsProductType.Incidents,
+                "REPORTS" => StatisticsProductType.Reports,
+                "CONSTRUCTIONYEARS" => StatisticsProductType.ConstructionYears,
+                "DATACOLLECTED" => StatisticsProductType.DataCollected,
+                "FOUNDATIONRATIO" => StatisticsProductType.FoundationRatio,
+                "FOUNDATIONRISK" => StatisticsProductType.FoundationRisk,
+                _ => throw new ProductNotFoundException(nameof(name)),
+            };
+        }
+
+        /// <summary>
+        /// Maps an <see cref="AnalysisProductType"/> to the corresponding
+        /// <see cref="AnalysisResponseModelBase"/> implementation.
+        /// </summary>
+        /// <param name="product"><see cref="AnalysisProductType"/></param>
+        /// <returns><see cref="AnalysisResponseModelBase"/> <see cref="Type"/></returns>
+        public static Type MapAnalysisResponseModelType(AnalysisProductType product)
+        {
+            return product switch
+            {
+                AnalysisProductType.BuildingData => typeof(AnalysisBuildingDataResponseModel),
+                AnalysisProductType.Foundation => typeof(AnalysisFoundationResponseModel),
+                AnalysisProductType.FoundationPlus => typeof(AnalysisFoundationPlusResponseModel),
+                AnalysisProductType.Costs => typeof(AnalysisCostsResponseModel),
+                AnalysisProductType.Complete => typeof(AnalysisCompleteResponseModel),
+                AnalysisProductType.BuildingDescription => typeof(AnalysisBuildingDescriptionResponseModel),
+                AnalysisProductType.Risk => typeof(AnalysisRiskResponseModel),
+                _ => throw new InvalidOperationException(nameof(product)),
             };
         }
     }
