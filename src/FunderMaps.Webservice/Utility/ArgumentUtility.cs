ce61718c1fe5820316af581a3bfac3876766e7d3
Thomas Beckers <tabeckers95@gmail.com>
2020-08-05 13:47:01 +0200

    Create ArgumentUtility.cs

---
+++ src/FunderMaps.Webservice/Utility/ArgumentUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace FunderMaps.Webservice.Utility
+{
+    /// <summary>
+    /// Contains utility functions regarding collections of arguments.
+    /// </summary>
+    internal static class ArgumentUtility
+    {
+        /// <summary>
+        /// Returns the amount of items that are not null or emty in a list.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        internal static int NotNullCount(params string[] s) => s.Where(s => !string.IsNullOrEmpty(s)).Count();
+    }
+}
