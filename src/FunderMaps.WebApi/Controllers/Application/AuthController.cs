653af41c28431f8eae1f31664d282f1a5ab96b9d
Yorick de Wid <yorick17@outlook.com>
2020-08-08 21:06:56 +0200

    Signin via GET is overly complex

--- src/FunderMaps.WebApi/Controllers/Application/AuthController.cs
+++ src/FunderMaps.WebApi/Controllers/Application/AuthController.cs
@@ -27,26 +27,8 @@ namespace FunderMaps.WebApi.Controllers.Application
         }
 
         [AllowAnonymous]
-        [HttpGet("signin")]
-        public async Task<IActionResult> SignInViaGetAsync([FromQuery] SignInDto input)
-        {
-            // Act.
-            string token = await _authenticationHelper.SignInAsync(input.Email, input.Password);
-
-            // Map.
-            var output = new SignInSecurityTokenDto
-            {
-                Token = token,
-                TokenValidity = 2400,
-            };
-
-            // Return.
-            return Ok(output);
-        }
-
-        [AllowAnonymous]
         [HttpPost("signin")]
-        public async Task<IActionResult> SignInViaPostAsync([FromBody] SignInDto input)
+        public async Task<IActionResult> SignInAsync([FromBody] SignInDto input)
         {
             // Act.
             string token = await _authenticationHelper.SignInAsync(input.Email, input.Password);
