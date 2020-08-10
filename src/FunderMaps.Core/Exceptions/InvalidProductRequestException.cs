b8654a8744a6800dab0d311e780386f1a6151570
Thomas Beckers <tabeckers95@gmail.com>
2020-08-10 16:48:31 +0200

    Updated core exceptions

---
+++ src/FunderMaps.Core/Exceptions/InvalidProductRequestException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FunderMaps.Core.Exceptions
+{
+    /// <summary>
+    ///     Exception indicating a product request was invalid.
+    /// </summary>
+    public sealed class InvalidProductRequestException : FunderMapsCoreException
+    {
+        /// <summary>
+        ///     Create new instance.
+        /// </summary>
+        /// <param name="message"><see cref="Exception.Message"/></param>
+        public InvalidProductRequestException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        ///     Create new instance.
+        /// </summary>
+        /// <param name="message"><see cref="Exception.Message"/></param>
+        /// <param name="innerException"><see cref="Exception.InnerException"/></param>
+        public InvalidProductRequestException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        ///     Create new instance.
+        /// </summary>
+        public InvalidProductRequestException()
+        {
+        }
+    }
+}
--- src/FunderMaps.Core/Exceptions/ProductNotFoundException.cs
+++ src/FunderMaps.Core/Exceptions/ProductNotFoundException.cs
@@ -5,7 +5,7 @@ namespace FunderMaps.Core.Exceptions
     /// <summary>
     /// Exception indicating we can't find or parse a FunderMaps product.
     /// </summary>
-    public sealed class ProductNotFoundException : Exception
+    public sealed class ProductNotFoundException : FunderMapsCoreException
     {
         /// <summary>
         /// Indicates we can't find this product.
