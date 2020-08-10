eb25693ffb72cfdee44a3430e464f85aeeb890b2
Thomas Beckers <tabeckers95@gmail.com>
2020-08-10 16:49:45 +0200

    Created DTO's for controller input
    Including proper validation.

---
+++ src/FunderMaps.Webservice/InputModels/StatisticsInputModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FunderMaps.Webservice.InputModels
+{
+    /// <summary>
+    ///     DTO for statistics request.
+    ///     TODO This should check for invalid combinations.
+    /// </summary>
+    public sealed class StatisticsInputModel : PaginationInputModel
+    {
+        /// <summary>
+        ///     Product type.
+        /// </summary>
+        [Required]
+        public string Product { get; set; }
+
+        /// <summary>
+        ///     Incidates the area code for the calculations.
+        /// </summary>
+        public string AreaCode { get; set; }
+
+        /// <summary>
+        ///     Indicates whether or not we want the statistics for our entire fence.
+        /// </summary>
+        public bool FullFence { get; set; } = false;
+    }
+}
