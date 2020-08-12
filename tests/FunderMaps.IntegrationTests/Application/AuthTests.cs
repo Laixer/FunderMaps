bc03b9f945770644088f08b04be9a86942150061
Yorick de Wid <yorick17@outlook.com>
2020-08-12 16:20:06 +0200

    Test refresh token

--- tests/FunderMaps.IntegrationTests/Application/AuthTests.cs
+++ tests/FunderMaps.IntegrationTests/Application/AuthTests.cs
@@ -1,12 +1,10 @@
 ﻿using Bogus;
-using FunderMaps.Core.Entities;
 using FunderMaps.Core.Services;
 using FunderMaps.IntegrationTests.Extensions;
 using FunderMaps.IntegrationTests.Faker;
 using FunderMaps.IntegrationTests.Repositories;
 using FunderMaps.WebApi.DataTransferObjects;
 using System.Net;
-using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Xunit;
@@ -16,36 +14,52 @@ namespace FunderMaps.IntegrationTests.Application
     public class AuthTests : IClassFixture<AuthWebApplicationFactory<Startup>>
     {
         private readonly AuthWebApplicationFactory<Startup> _factory;
-        private readonly HttpClient _client;
-
-        private readonly User sessionUser = new UserFaker().Generate();
-        private readonly Organization sessionOrganization = new OrganizationFaker().Generate();
-        private readonly string password = new Randomizer().Password(128);
 
         public AuthTests(AuthWebApplicationFactory<Startup> factory)
         {
-            var passwordHash = new PasswordHasher().HashPassword(password);
             _factory = factory;
-            _client = _factory
-                .WithAuthentication(options => options.User = sessionUser)
-                .WithDataStoreList(new UserRecord { User = sessionUser, Password = passwordHash })
-                .WithDataStoreList(sessionOrganization)
-                .WithDataStoreList(new OrganizationUserRecord { UserId = sessionUser.Id, OrganizationId = sessionOrganization.Id })
-                .CreateClient();
         }
 
         [Fact]
         public async Task SignInReturnSuccessAndToken()
         {
             // Arrange
+            var sessionUser = new UserFaker().Generate();
+            var sessionOrganization = new OrganizationFaker().Generate();
+            var password = new Randomizer().Password(128);
             var signIn = new SignInDto
             {
                 Email = sessionUser.Email,
                 Password = password,
             };
+            var client = _factory
+                .WithAuthentication(options => options.User = sessionUser)
+                .WithDataStoreList(new UserRecord { User = sessionUser, Password = new PasswordHasher().HashPassword(password) })
+                .WithDataStoreList(sessionOrganization)
+                .WithDataStoreList(new OrganizationUserRecord { UserId = sessionUser.Id, OrganizationId = sessionOrganization.Id })
+                .CreateClient();
 
             // Act
-            var response = await _client.PostAsJsonAsync("api/auth/signin", signIn);
+            var response = await client.PostAsJsonAsync("api/auth/signin", signIn);
+            var returnObject = await response.Content.ReadFromJsonAsync<SignInSecurityTokenDto>();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(returnObject.Token);
+            Assert.True(returnObject.TokenValidity > 0);
+        }
+
+        [Fact]
+        public async Task RefreshSignInReturnSuccessAndToken()
+        {
+            // Arrange
+            var client = _factory
+                .WithAuthentication()
+                .WithAuthenticationStores()
+                .CreateClient();
+
+            // Act
+            var response = await client.GetAsync("api/auth/token-refresh");
             var returnObject = await response.Content.ReadFromJsonAsync<SignInSecurityTokenDto>();
 
             // Assert
