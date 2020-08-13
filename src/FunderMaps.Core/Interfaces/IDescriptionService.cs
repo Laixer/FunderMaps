ca36bafcc68305d76dba42d6670b71e886f5a6dc
Thomas Beckers <tabeckers95@gmail.com>
2020-08-13 09:57:17 +0200

    Created and updated abstraction for webservice

---
+++ src/FunderMaps.Core/Interfaces/IDescriptionService.cs
@@ -0,0 +1,14 @@
+﻿using FunderMaps.Core.Types.Products;
+
+namespace FunderMaps.Core.Interfaces
+{
+    /// <summary>
+    ///     Contract for generating descriptions.
+    /// </summary>
+    public interface IDescriptionService
+    {
+        string GenerateFullDescription(AnalysisProduct product);
+
+        string GenerateTerrainDescription(AnalysisProduct product);
+    }
+}
