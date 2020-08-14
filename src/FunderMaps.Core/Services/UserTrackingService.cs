7ab2813583324951cfaf28f2c8b5eff0bc8168dc
Thomas Beckers <tabeckers95@gmail.com>
2020-08-14 14:36:29 +0200

    Create UserTrackingService.cs

---
+++ src/FunderMaps.Core/Services/UserTrackingService.cs
@@ -0,0 +1,18 @@
+﻿using FunderMaps.Core.Interfaces;
+using FunderMaps.Core.Types.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunderMaps.Core.Services
+{
+    /// <summary>
+    ///     Service for tracking user behaviour with regards to products.
+    /// </summary>
+    public sealed class UserTrackingService : IUserTrackingService
+    {
+        public Task ProcessAnalysisRequest(Guid userId, AnalysisProductType productType) => throw new NotImplementedException();
+        public Task ProcessStatisticsRequestAsync(Guid userId, StatisticsProductType productType) => throw new NotImplementedException();
+    }
+}
