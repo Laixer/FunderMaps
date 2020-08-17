cf73398da22e81bdabeb856b4e6f09d8cd5125c1
Thomas Beckers <tabeckers95@gmail.com>
2020-08-17 16:33:13 +0200

    Updated analysis and statistics repository abstraction

--- src/FunderMaps.Data/Repositories/StatisticsRepository.cs
+++ src/FunderMaps.Data/Repositories/StatisticsRepository.cs
@@ -13,13 +13,20 @@ namespace FunderMaps.Data.Repositories
     /// </summary>
     internal sealed class StatisticsRepository : IStatisticsRepository
     {
-        public Task<ConstructionYearDistribution> GetConstructionYearDistributionAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
-        public Task<double> GetDataCollectedPercentageAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
-        public Task<FoundationRiskDistribution> GetFoundationRiskDistributionAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
-        public Task<FoundationTypeDistribution> GetFoundationTypeDistributionAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
-        public Task<uint> GetTotalBuildingRestoredCountAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
-        public Task<uint> GetTotalIncidentCountAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
-        public Task<uint> GetTotalReportCountAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
+        public Task<ConstructionYearDistribution> GetConstructionYearDistributionByExternalIdAsync(string neighborhoodCode, CancellationToken token) => throw new NotImplementedException();
+        public Task<ConstructionYearDistribution> GetConstructionYearDistributionByIdAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
+        public Task<double> GetDataCollectedPercentageByExternalIdAsync(string neighborhoodCode, CancellationToken token) => throw new NotImplementedException();
+        public Task<double> GetDataCollectedPercentageByIdAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
+        public Task<FoundationRiskDistribution> GetFoundationRiskDistributionByExternalIdAsync(string neighborhoodCode, CancellationToken token) => throw new NotImplementedException();
+        public Task<FoundationRiskDistribution> GetFoundationRiskDistributionByIdAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
+        public Task<FoundationTypeDistribution> GetFoundationTypeDistributionByExternalIdAsync(string neighborhoodCode, CancellationToken token) => throw new NotImplementedException();
+        public Task<FoundationTypeDistribution> GetFoundationTypeDistributionByIdAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
+        public Task<uint> GetTotalBuildingRestoredCountByExternalIdAsync(string neighborhoodCode, CancellationToken token) => throw new NotImplementedException();
+        public Task<uint> GetTotalBuildingRestoredCountByIdAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
+        public Task<uint> GetTotalIncidentCountByExternalIdAsync(string neighborhoodCode, CancellationToken token) => throw new NotImplementedException();
+        public Task<uint> GetTotalIncidentCountByIdAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
+        public Task<uint> GetTotalReportCountByExternalIdAsync(string neighborhoodCode, CancellationToken token) => throw new NotImplementedException();
+        public Task<uint> GetTotalReportCountByIdAsync(string neighborhoodId, CancellationToken token) => throw new NotImplementedException();
     }
 }
 #pragma warning disable CA1812 // Internal class is never instantiated
