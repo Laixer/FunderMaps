b223ce3ace0baf892e0bcd79c0160a0170f223c8
Thomas Beckers <tabeckers95@gmail.com>
2020-08-17 16:33:22 +0200

    Update ProductTypeMapperTests.cs

--- FunderMaps.Webservice.Tests/Mapping/ProductTypeMapperTests.cs
+++ FunderMaps.Webservice.Tests/Mapping/ProductTypeMapperTests.cs
@@ -1,7 +1,7 @@
-﻿using FunderMaps.Core.Exceptions;
-using FunderMaps.Core.Types.Products;
+﻿using FunderMaps.Core.Types.Products;
 using FunderMaps.Webservice.Mapping;
 using FunderMaps.Webservice.ResponseModels.Analysis;
+using FunderMaps.Webservice.ResponseModels.Types;
 using System;
 using Xunit;
 
@@ -13,52 +13,26 @@ namespace FunderMaps.Webservice.Tests.Mapping
     public sealed class ProductTypeMapperTests
     {
         [Theory]
-        [InlineData("BUILDINGDATA", AnalysisProductType.BuildingData)]
-        [InlineData("COMPLETE", AnalysisProductType.Complete)]
-        [InlineData("COSTS", AnalysisProductType.Costs)]
-        [InlineData("DESCRIPTION", AnalysisProductType.BuildingDescription)]
-        [InlineData("FOUNDATION", AnalysisProductType.Foundation)]
-        [InlineData("FOUNDATIONPLUS", AnalysisProductType.FoundationPlus)]
-        [InlineData("RISK", AnalysisProductType.Risk)]
-        public void AnalysisProductTypeFromUpperString(string value, AnalysisProductType expected) =>
-            // Assert
-            Assert.Equal(expected, ProductTypeMapper.MapAnalysisFromString(value));
+        [InlineData(AnalysisProductTypeResponseModel.BuildingData, AnalysisProductType.BuildingData)]
+        [InlineData(AnalysisProductTypeResponseModel.Complete, AnalysisProductType.Complete)]
+        [InlineData(AnalysisProductTypeResponseModel.Costs, AnalysisProductType.Costs)]
+        [InlineData(AnalysisProductTypeResponseModel.BuildingDescription, AnalysisProductType.BuildingDescription)]
+        [InlineData(AnalysisProductTypeResponseModel.Foundation, AnalysisProductType.Foundation)]
+        [InlineData(AnalysisProductTypeResponseModel.FoundationPlus, AnalysisProductType.FoundationPlus)]
+        [InlineData(AnalysisProductTypeResponseModel.Risk, AnalysisProductType.Risk)]
+        public void AnalysisProductTypeMaps(AnalysisProductTypeResponseModel value, AnalysisProductType expected) =>
+            Assert.Equal(expected, ProductTypeMapper.MapAnalysis(value));
 
         [Theory]
-        [InlineData("buildingdata", AnalysisProductType.BuildingData)]
-        [InlineData("complete", AnalysisProductType.Complete)]
-        [InlineData("costs", AnalysisProductType.Costs)]
-        [InlineData("description", AnalysisProductType.BuildingDescription)]
-        [InlineData("foundation", AnalysisProductType.Foundation)]
-        [InlineData("foundationplus", AnalysisProductType.FoundationPlus)]
-        [InlineData("risk", AnalysisProductType.Risk)]
-        public void AnalysisProductTypeFromLowerString(string value, AnalysisProductType expected) =>
-            // Assert
-            Assert.Equal(expected, ProductTypeMapper.MapAnalysisFromString(value));
-
-        [Theory]
-        [InlineData("RESTORATION", StatisticsProductType.BuildingsRestored)]
-        [InlineData("CONSTRUCTIONYEARS", StatisticsProductType.ConstructionYears)]
-        [InlineData("DATACOLLECTED", StatisticsProductType.DataCollected)]
-        [InlineData("FOUNDATIONRATIO", StatisticsProductType.FoundationRatio)]
-        [InlineData("FOUNDATIONRISK", StatisticsProductType.FoundationRisk)]
-        [InlineData("INCIDENTS", StatisticsProductType.Incidents)]
-        [InlineData("REPORTS", StatisticsProductType.Reports)]
-        public void StatisticsProductTypeFromUpperString(string value, StatisticsProductType expected) =>
-            // Assert
-            Assert.Equal(expected, ProductTypeMapper.MapStatisticsFromString(value));
-
-        [Theory]
-        [InlineData("restoration", StatisticsProductType.BuildingsRestored)]
-        [InlineData("constructionyears", StatisticsProductType.ConstructionYears)]
-        [InlineData("datacollected", StatisticsProductType.DataCollected)]
-        [InlineData("foundationratio", StatisticsProductType.FoundationRatio)]
-        [InlineData("foundationrisk", StatisticsProductType.FoundationRisk)]
-        [InlineData("incidents", StatisticsProductType.Incidents)]
-        [InlineData("reports", StatisticsProductType.Reports)]
-        public void StatisticsProductTypeFromLowerString(string value, StatisticsProductType expected) =>
-            // Assert
-            Assert.Equal(expected, ProductTypeMapper.MapStatisticsFromString(value));
+        [InlineData(StatisticsProductTypeResponseModel.BuildingsRestored, StatisticsProductType.BuildingsRestored)]
+        [InlineData(StatisticsProductTypeResponseModel.ConstructionYears, StatisticsProductType.ConstructionYears)]
+        [InlineData(StatisticsProductTypeResponseModel.DataCollected, StatisticsProductType.DataCollected)]
+        [InlineData(StatisticsProductTypeResponseModel.FoundationRatio, StatisticsProductType.FoundationRatio)]
+        [InlineData(StatisticsProductTypeResponseModel.FoundationRisk, StatisticsProductType.FoundationRisk)]
+        [InlineData(StatisticsProductTypeResponseModel.Incidents, StatisticsProductType.Incidents)]
+        [InlineData(StatisticsProductTypeResponseModel.Reports, StatisticsProductType.Reports)]
+        public void StatisticsProductTypeMaps(StatisticsProductTypeResponseModel value, StatisticsProductType expected) =>
+            Assert.Equal(expected, ProductTypeMapper.MapStatistics(value));
 
         [Theory]
         [InlineData(AnalysisProductType.BuildingData, typeof(AnalysisBuildingDataResponseModel))]
@@ -68,30 +42,15 @@ namespace FunderMaps.Webservice.Tests.Mapping
         [InlineData(AnalysisProductType.Foundation, typeof(AnalysisFoundationResponseModel))]
         [InlineData(AnalysisProductType.FoundationPlus, typeof(AnalysisFoundationPlusResponseModel))]
         [InlineData(AnalysisProductType.Risk, typeof(AnalysisRiskResponseModel))]
-        public void AnalysisResponseModelTypeFromProduct(AnalysisProductType value, Type expected)
-        {
-            // Assert
+        public void AnalysisResponseModelTypeFromProduct(AnalysisProductType value, Type expected) =>
             Assert.Equal(expected, ProductTypeMapper.MapAnalysisResponseModelType(value));
-        }
 
         [Fact]
-        public void AnalysisProductTypeFromStringThrows()
-        {
-            // Arrange
-            var str = "asdflkjsdhfa"; // TODO Make randomly generate
-
-            // Assert
-            Assert.Throws<ProductNotFoundException>(() => ProductTypeMapper.MapAnalysisFromString(str));
-        }
+        public void InvalidAnalysisProductTypeThrows()
+            => Assert.Throws<InvalidOperationException>(() => ProductTypeMapper.MapAnalysis((AnalysisProductTypeResponseModel)453));
 
         [Fact]
-        public void StatisticProductTypeFromStringThrows()
-        {
-            // Arrange
-            var str = "lkkjyrtur"; // TODO Make randomly generate
-
-            // Assert
-            Assert.Throws<ProductNotFoundException>(() => ProductTypeMapper.MapStatisticsFromString(str));
-        }
+        public void InvalidStatisticProductTypeThrows()
+            => Assert.Throws<InvalidOperationException>(() => ProductTypeMapper.MapStatistics((StatisticsProductTypeResponseModel)485));
     }
 }
