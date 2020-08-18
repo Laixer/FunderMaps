c2ac93cb3bb29e2c4533cf141cd58a716b0d6fe6
Yorick de Wid <yorick17@outlook.com>
2020-08-18 21:16:39 +0200

    Register services in certain environment

--- src/FunderMaps.Infrastructure/Extensions/FunderMapsInfrastructureServiceCollectionExtensions.cs
+++ src/FunderMaps.Infrastructure/Extensions/FunderMapsInfrastructureServiceCollectionExtensions.cs
@@ -1,19 +1,59 @@
 ﻿using FunderMaps.Core.Interfaces;
 using FunderMaps.Infrastructure.Email;
 using FunderMaps.Infrastructure.Notification;
-using FunderMaps.Infrastructure.Storage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    // FUTURE: Introduce a Startup class much like FunderMaps.WebApi.Startup
+
     /// <summary>
     ///     Provides extension methods for services from this assembly.
     /// </summary>
     public static class FunderMapsInfrastructureServiceCollectionExtensions
     {
+        /// <summary>
+        ///     Configuration.
+        /// </summary>
+        public static IConfiguration Configuration { get; set; }
+
+        /// <summary>
+        ///     Host environment.
+        /// </summary>
+        public static IHostEnvironment HostEnvironment { get; set; }
+
+        /// <summary>
+        ///     Use this method to add production services to the container.
+        /// </summary>
+        /// <remarks>
+        ///     Order is undetermined when configuring services.
+        /// </remarks>
+        /// <param name="services">See <see cref="IServiceCollection"/>.</param>
+        public static void ConfigureProductionServices(IServiceCollection services)
+        {
+            // Remove all existing email services and inject local email service.
+            services.RemoveAll<IEmailService>();
+            services.Configure<EmailOptions>(Configuration.GetSection(EmailOptions.Section));
+            services.AddTransient<IEmailService, EmailService>();
+        }
+
+        /// <summary>
+        ///     Use this method to add services to the container.
+        /// </summary>
+        /// <remarks>
+        ///     Order is undetermined when configuring services.
+        /// </remarks>
+        /// <param name="services">See <see cref="IServiceCollection"/>.</param>
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // Remove all existing notification services and inject local email service.
+            services.RemoveAll<INotificationService>();
+            services.AddTransient<INotificationService, NotificationHubService>();
+        }
+
         /// <summary>
         ///     Adds the infrastructure services to the container.
         /// </summary>
@@ -27,20 +67,19 @@ namespace Microsoft.Extensions.DependencyInjection
             }
 
             using var serviceProviderScope = services.BuildServiceProvider().CreateScope();
-            var configuration = serviceProviderScope.ServiceProvider.GetRequiredService<IConfiguration>();
+            Configuration = serviceProviderScope.ServiceProvider.GetRequiredService<IConfiguration>();
+            HostEnvironment = serviceProviderScope.ServiceProvider.GetRequiredService<IHostEnvironment>();
 
             // Remove all existing file storage services and inject local file stoage service.
             //services.RemoveAll<IFileStorageService>();
             //services.AddScoped<IFileStorageService, AzureBlobStorageService>();
 
-            // Remove all existing email services and inject local email service.
-            services.RemoveAll<IEmailService>();
-            services.Configure<EmailOptions>(configuration.GetSection(EmailOptions.Section));
-            services.AddTransient<IEmailService, EmailService>();
+            if (HostEnvironment.IsProduction())
+            {
+                ConfigureProductionServices(services);
+            }
 
-            // Remove all existing notification services and inject local email service.
-            services.RemoveAll<INotificationService>();
-            services.AddTransient<INotificationService, NotificationHubService>();
+            ConfigureServices(services);
 
             // FUTURE: Bind
             //services.Configure<FileStorageOptions>(options =>
