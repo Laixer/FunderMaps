47a78bd098709ff19a671cce7adc8e45fed39601
Yorick de Wid <yorick17@outlook.com>
2020-08-18 18:47:28 +0200

    Disable file storage service for the time being

--- src/FunderMaps.Infrastructure/Extensions/FunderMapsInfrastructureServiceCollectionExtensions.cs
+++ src/FunderMaps.Infrastructure/Extensions/FunderMapsInfrastructureServiceCollectionExtensions.cs
@@ -28,33 +28,35 @@ namespace Microsoft.Extensions.DependencyInjection
             using var serviceProviderScope = services.BuildServiceProvider().CreateScope();
             var configuration = serviceProviderScope.ServiceProvider.GetRequiredService<IConfiguration>();
 
-            services.RemoveAll<IFileStorageService>();
-            services.AddScoped<IFileStorageService, AzureBlobStorageService>();
+            // Remove all existing file storage services and inject local file stoage service.
+            //services.RemoveAll<IFileStorageService>();
+            //services.AddScoped<IFileStorageService, AzureBlobStorageService>();
 
+            // Remove all existing email services and inject local email service.
             services.RemoveAll<IEmailService>();
             services.Configure<EmailOptions>(configuration.GetSection(EmailOptions.Section));
             services.AddTransient<IEmailService, EmailService>();
 
             // FUTURE: Bind
-            services.Configure<FileStorageOptions>(options =>
-            {
-                if (options.StorageContainers == null)
-                {
-                    options.StorageContainers = new Dictionary<string, string>();
-                }
+            //services.Configure<FileStorageOptions>(options =>
+            //{
+            //    if (options.StorageContainers == null)
+            //    {
+            //        options.StorageContainers = new Dictionary<string, string>();
+            //    }
 
-                var rootKey = configuration.GetSection("FileStorageContainers");
-                if (rootKey == null)
-                {
-                    return; // TODO: This can never be oke.
-                }
+            //    var rootKey = configuration.GetSection("FileStorageContainers");
+            //    if (rootKey == null)
+            //    {
+            //        return; // TODO: This can never be oke.
+            //    }
 
-                // FUTURE: This can drastically be improved.
-                foreach (var item in rootKey.GetChildren())
-                {
-                    options.StorageContainers.Add(item.Key, item.Value);
-                }
-            });
+            //    // FUTURE: This can drastically be improved.
+            //    foreach (var item in rootKey.GetChildren())
+            //    {
+            //        options.StorageContainers.Add(item.Key, item.Value);
+            //    }
+            //});
 
             return services;
         }
