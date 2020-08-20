4445e9645d89ac600592f5c8d831843c742abca0
Thomas Beckers <tabeckers95@gmail.com>
2020-08-20 13:11:50 +0200

    Test user tracking service is now a nullservice

--- tests/FunderMaps.IntegrationTests/Services/TestUserTrackingService.cs
+++ tests/FunderMaps.IntegrationTests/Services/TestUserTrackingService.cs
@@ -1,13 +1,15 @@
 ﻿using FunderMaps.Core.Interfaces;
 using FunderMaps.Core.Types.Products;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FunderMaps.IntegrationTests.Services
 {
     public class TestUserTrackingService : IUserTrackingService
     {
-        public Task ProcessAnalysisRequest(Guid userId, AnalysisProductType productType) => Task.CompletedTask;
-        public Task ProcessStatisticsRequestAsync(Guid userId, StatisticsProductType productType) => Task.CompletedTask;
+        public Task ProcessMultipleAnalysisRequest(Guid userId, AnalysisProductType productType, uint itemCount, CancellationToken token) => Task.CompletedTask;
+        public Task ProcessSingleAnalysisRequest(Guid userId, AnalysisProductType productType, CancellationToken token) => Task.CompletedTask;
+        public Task ProcessStatisticsRequestAsync(Guid userId, StatisticsProductType productType, CancellationToken token) => Task.CompletedTask;
     }
 }
