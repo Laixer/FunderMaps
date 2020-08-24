7cbe36d361e4ec7d4ebeba8eb4a503139323c5bf
Yorick de Wid <yorick17@outlook.com>
2020-08-24 10:11:09 +0200

    Rewrite integration test for recovery

--- tests/FunderMaps.IntegrationTests/Backend/Report/RecoveryTests.cs
+++ tests/FunderMaps.IntegrationTests/Backend/Report/RecoveryTests.cs
@@ -1,8 +1,5 @@
-﻿using FunderMaps.Core.Entities;
-using FunderMaps.IntegrationTests.Extensions;
-using FunderMaps.IntegrationTests.Faker;
+﻿using FunderMaps.IntegrationTests.Faker;
 using FunderMaps.WebApi.DataTransferObjects;
-using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http.Json;
@@ -20,58 +17,34 @@ namespace FunderMaps.IntegrationTests.Backend.Report
             _factory = factory;
         }
 
-        internal class FakeRecoveryDtoData : EnumerableHelper<RecoveryDto>
-        {
-            protected override IEnumerable<RecoveryDto> GetEnumerableEntity()
-            {
-                return new RecoveryDtoFaker().Generate(10, 100);
-            }
-        }
-
-        internal class FakeRecoveryData : EnumerableHelper<Recovery>
-        {
-            protected override IEnumerable<Recovery> GetEnumerableEntity()
-            {
-                return new RecoveryFaker().Generate(10, 100);
-            }
-        }
-
-        [Theory]
-        [ClassData(typeof(FakeRecoveryDtoData))]
-        public async Task CreateRecoveryReturnRecovery(RecoveryDto recovery)
+        [Fact]
+        public async Task CreateRecoveryReturnRecovery()
         {
             // Arrange
             var client = _factory
                 .WithAuthentication()
                 .WithAuthenticationStores()
                 .CreateClient();
-            var recoveryDataStore = _factory.Services.GetService<EntityDataStore<Recovery>>();
 
             // Act
-            var response = await client.PostAsJsonAsync("api/recovery", recovery);
+            var response = await client.PostAsJsonAsync("api/recovery", new RecoveryDtoFaker().Generate());
             var returnObject = await response.Content.ReadFromJsonAsync<RecoveryDto>();
 
             // Assert
-            //Assert.Equal(AuditStatus.Todo, actualRecovery.AuditStatus);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.True(recoveryDataStore.IsSet);
-            Assert.Equal(recovery.Note, returnObject.Note);
-            Assert.Equal(recovery.Type, returnObject.Type);
-            Assert.Equal(recovery.DocumentFile, returnObject.DocumentFile);
-            Assert.Equal(recovery.DocumentDate, returnObject.DocumentDate);
-            Assert.Equal(recovery.AccessPolicy, returnObject.AccessPolicy);
+            //Assert.Equal(AuditStatus.Todo, returnObject.AuditStatus);
+            Assert.Null(returnObject.UpdateDate);
         }
 
-        [Theory]
-        [ClassData(typeof(FakeRecoveryData))]
-        public async Task GetRecoveryByIdReturnSingleRecovery(Recovery recovery)
+        [Fact]
+        public async Task GetRecoveryByIdReturnSingleRecovery()
         {
             // Arrange
             var client = _factory
                 .WithAuthentication()
                 .WithAuthenticationStores()
-                .WithDataStoreList(recovery)
                 .CreateClient();
+            var recovery = await client.PostAsJsonGetFromJsonAsync<RecoveryDto, RecoveryDto>("api/recovery", new RecoveryDtoFaker().Generate());
 
             // Act
             var response = await client.GetAsync($"api/recovery/{recovery.Id}");
@@ -79,97 +52,65 @@ namespace FunderMaps.IntegrationTests.Backend.Report
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal(recovery.Note, returnObject.Note);
-            Assert.Equal(recovery.Type, returnObject.Type);
-            Assert.Equal(recovery.DocumentFile, returnObject.DocumentFile);
-            Assert.Equal(recovery.DocumentDate, returnObject.DocumentDate);
-            Assert.Equal(recovery.AccessPolicy, returnObject.AccessPolicy);
+            //Assert.Equal(AuditStatus.Todo, returnObject.AuditStatus);
+            Assert.Null(returnObject.UpdateDate);
         }
 
         [Fact]
-        public async Task GetAllRecoveryReturnPageRecovery()
+        public async Task GetAllRecoveryReturnNavigationRecovery()
         {
             // Arrange
             var client = _factory
                 .WithAuthentication()
                 .WithAuthenticationStores()
-                .WithDataStoreList(new RecoveryFaker().Generate(10, 100))
                 .CreateClient();
+            for (int i = 0; i < 10; i++)
+            {
+                await client.PostAsJsonGetFromJsonAsync<RecoveryDto, RecoveryDto>("api/recovery", new RecoveryDtoFaker().Generate());
+            }
 
             // Act
-            var response = await client.GetAsync($"api/recovery");
+            var response = await client.GetAsync($"api/recovery?limit=10");
             var returnList = await response.Content.ReadFromJsonAsync<List<RecoveryDto>>();
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.True(returnList.Count > 0);
+            Assert.Equal(10, returnList.Count);
         }
 
         [Fact]
-        public async Task GetAllRecoveryReturnAllRecovery()
+        public async Task UpdateRecoveryReturnNoContent()
         {
             // Arrange
+            var newRecovery = new RecoveryDtoFaker().Generate();
             var client = _factory
                 .WithAuthentication()
                 .WithAuthenticationStores()
-                .WithDataStoreList(new RecoveryFaker().Generate(100))
                 .CreateClient();
-
-            // Act
-            var response = await client.GetAsync($"api/recovery?limit=100");
-            var returnList = await response.Content.ReadFromJsonAsync<List<RecoveryDto>>();
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal(100, returnList.Count);
-        }
-
-        [Theory]
-        [ClassData(typeof(FakeRecoveryData))]
-        public async Task UpdateRecoveryReturnNoContent(Recovery recovery)
-        {
-            // Arrange
-            var newRecovery = new RecoveryDtoFaker().Generate(); // TODO Move outside of test
-            var client = _factory
-                .WithAuthentication()
-                .WithAuthenticationStores()
-                .WithDataStoreList(recovery)
-                .CreateClient();
-            var recoveryDataStore = _factory.Services.GetService<EntityDataStore<Recovery>>();
+            var recovery = await client.PostAsJsonGetFromJsonAsync<RecoveryDto, RecoveryDto>("api/recovery", new RecoveryDtoFaker().Generate());
 
             // Act
             var response = await client.PutAsJsonAsync($"api/recovery/{recovery.Id}", newRecovery);
 
             // Assert
-            var actualRecovery = recoveryDataStore.Entities[0];
-            Assert.Equal(recovery.Id, actualRecovery.Id);
             Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-            Assert.Equal(1, recoveryDataStore.Entities.Count);
-            Assert.Equal(newRecovery.Note, actualRecovery.Note);
-            Assert.Equal(newRecovery.Type, actualRecovery.Type);
-            Assert.Equal(newRecovery.DocumentFile, actualRecovery.DocumentFile);
-            Assert.Equal(newRecovery.DocumentDate, actualRecovery.DocumentDate);
-            Assert.Equal(newRecovery.AccessPolicy, actualRecovery.AccessPolicy);
         }
 
-        [Theory]
-        [ClassData(typeof(FakeRecoveryData))]
-        public async Task DeleteRecoveryReturnNoContent(Recovery recovery)
+        [Fact]
+        public async Task DeleteRecoveryReturnNoContent()
         {
             // Arrange
             var client = _factory
                 .WithAuthentication()
                 .WithAuthenticationStores()
-                .WithDataStoreList(recovery)
                 .CreateClient();
-            var recoveryDataStore = _factory.Services.GetService<EntityDataStore<Recovery>>();
+            var recovery = await client.PostAsJsonGetFromJsonAsync<RecoveryDto, RecoveryDto>("api/recovery", new RecoveryDtoFaker().Generate());
 
             // Act
             var response = await client.DeleteAsync($"api/recovery/{recovery.Id}");
 
             // Assert
             Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-            Assert.False(recoveryDataStore.IsSet);
         }
     }
 }
