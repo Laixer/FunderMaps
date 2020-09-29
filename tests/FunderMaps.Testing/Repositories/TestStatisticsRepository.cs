0cda9270d28dd515a5801308eb03e3735ddc5e51
Yorick de Wid <yorick17@outlook.com>
2020-09-29 14:34:00 +0200

    Update testcases

--- tests/FunderMaps.Testing/Repositories/TestStatisticsRepository.cs
+++ tests/FunderMaps.Testing/Repositories/TestStatisticsRepository.cs
@@ -4,7 +4,6 @@ using FunderMaps.Core.Types.Distributions;
 using FunderMaps.Core.Types.Products;
 using System;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace FunderMaps.Testing.Repositories
@@ -27,85 +26,85 @@ namespace FunderMaps.Testing.Repositories
         private StatisticsProduct FromNeighborhoodId(string neighboorhoodId)
             => DataStore.ItemList.Where(x => x.NeighborhoodId == neighboorhoodId).FirstOrDefault();
 
-        public async Task<ConstructionYearDistribution> GetConstructionYearDistributionByIdAsync(Guid userId, string neighboorhoodId, CancellationToken token)
+        public async Task<ConstructionYearDistribution> GetConstructionYearDistributionByIdAsync(Guid userId, string neighboorhoodId)
         {
             await Task.CompletedTask;
             return FromNeighborhoodId(neighboorhoodId)?.ConstructionYearDistribution ?? throw new EntityNotFoundException();
         }
 
-        public async Task<ConstructionYearDistribution> GetConstructionYearDistributionByExternalIdAsync(Guid userId, string neighborhoodCode, CancellationToken token)
+        public async Task<ConstructionYearDistribution> GetConstructionYearDistributionByExternalIdAsync(Guid userId, string neighborhoodCode)
         {
             await Task.CompletedTask;
             return DataStore.ItemList.Where(x => x.NeighborhoodCode == neighborhoodCode).FirstOrDefault()?.ConstructionYearDistribution ?? throw new EntityNotFoundException();
         }
 
-        public async Task<double> GetDataCollectedPercentageByIdAsync(Guid userId, string neighboorhoodId, CancellationToken token)
+        public async Task<double> GetDataCollectedPercentageByIdAsync(Guid userId, string neighboorhoodId)
         {
             await Task.CompletedTask;
             return FromNeighborhoodId(neighboorhoodId)?.DataCollectedPercentage ?? throw new EntityNotFoundException();
         }
 
-        public async Task<double> GetDataCollectedPercentageByExternalIdAsync(Guid userId, string neighborhoodCode, CancellationToken token)
+        public async Task<double> GetDataCollectedPercentageByExternalIdAsync(Guid userId, string neighborhoodCode)
         {
             await Task.CompletedTask;
             return DataStore.ItemList.Where(x => x.NeighborhoodCode == neighborhoodCode).FirstOrDefault()?.DataCollectedPercentage ?? throw new EntityNotFoundException();
         }
 
-        public async Task<FoundationRiskDistribution> GetFoundationRiskDistributionByIdAsync(Guid userId, string neighboorhoodId, CancellationToken token)
+        public async Task<FoundationRiskDistribution> GetFoundationRiskDistributionByIdAsync(Guid userId, string neighboorhoodId)
         {
             await Task.CompletedTask;
             return FromNeighborhoodId(neighboorhoodId)?.FoundationRiskDistribution ?? throw new EntityNotFoundException();
         }
 
-        public async Task<FoundationRiskDistribution> GetFoundationRiskDistributionByExternalIdAsync(Guid userId, string neighborhoodCode, CancellationToken token)
+        public async Task<FoundationRiskDistribution> GetFoundationRiskDistributionByExternalIdAsync(Guid userId, string neighborhoodCode)
         {
             await Task.CompletedTask;
             return DataStore.ItemList.Where(x => x.NeighborhoodCode == neighborhoodCode).FirstOrDefault()?.FoundationRiskDistribution ?? throw new EntityNotFoundException();
         }
 
-        public async Task<FoundationTypeDistribution> GetFoundationTypeDistributionByExternalIdAsync(Guid userId, string neighborhoodCode, CancellationToken token)
+        public async Task<FoundationTypeDistribution> GetFoundationTypeDistributionByExternalIdAsync(Guid userId, string neighborhoodCode)
         {
             await Task.CompletedTask;
             return DataStore.ItemList.Where(x => x.NeighborhoodCode == neighborhoodCode).FirstOrDefault()?.FoundationTypeDistribution ?? throw new EntityNotFoundException();
         }
 
-        public async Task<FoundationTypeDistribution> GetFoundationTypeDistributionByIdAsync(Guid userId, string neighboorhoodId, CancellationToken token)
+        public async Task<FoundationTypeDistribution> GetFoundationTypeDistributionByIdAsync(Guid userId, string neighboorhoodId)
         {
             await Task.CompletedTask;
             return FromNeighborhoodId(neighboorhoodId)?.FoundationTypeDistribution ?? throw new EntityNotFoundException();
         }
 
-        public async Task<uint> GetTotalBuildingRestoredCountByExternalIdAsync(Guid userId, string neighborhoodCode, CancellationToken token)
+        public async Task<uint> GetTotalBuildingRestoredCountByExternalIdAsync(Guid userId, string neighborhoodCode)
         {
             await Task.CompletedTask;
             return DataStore.ItemList.Where(x => x.NeighborhoodCode == neighborhoodCode).FirstOrDefault()?.TotalBuildingRestoredCount ?? throw new EntityNotFoundException();
         }
 
-        public async Task<uint> GetTotalBuildingRestoredCountByIdAsync(Guid userId, string neighboorhoodId, CancellationToken token)
+        public async Task<uint> GetTotalBuildingRestoredCountByIdAsync(Guid userId, string neighboorhoodId)
         {
             await Task.CompletedTask;
             return FromNeighborhoodId(neighboorhoodId)?.TotalBuildingRestoredCount ?? throw new EntityNotFoundException();
         }
 
-        public async Task<uint> GetTotalIncidentCountByExternalIdAsync(Guid userId, string neighborhoodCode, CancellationToken token)
+        public async Task<uint> GetTotalIncidentCountByExternalIdAsync(Guid userId, string neighborhoodCode)
         {
             await Task.CompletedTask;
             return DataStore.ItemList.Where(x => x.NeighborhoodCode == neighborhoodCode).FirstOrDefault()?.TotalIncidentCount ?? throw new EntityNotFoundException();
         }
 
-        public async Task<uint> GetTotalIncidentCountByIdAsync(Guid userId, string neighboorhoodId, CancellationToken token)
+        public async Task<uint> GetTotalIncidentCountByIdAsync(Guid userId, string neighboorhoodId)
         {
             await Task.CompletedTask;
             return FromNeighborhoodId(neighboorhoodId)?.TotalIncidentCount ?? throw new EntityNotFoundException();
         }
 
-        public async Task<uint> GetTotalReportCountByExternalIdAsync(Guid userId, string neighborhoodCode, CancellationToken token)
+        public async Task<uint> GetTotalReportCountByExternalIdAsync(Guid userId, string neighborhoodCode)
         {
             await Task.CompletedTask;
             return DataStore.ItemList.Where(x => x.NeighborhoodCode == neighborhoodCode).FirstOrDefault()?.TotalReportCount ?? throw new EntityNotFoundException();
         }
 
-        public async Task<uint> GetTotalReportCountByIdAsync(Guid userId, string neighboorhoodId, CancellationToken token)
+        public async Task<uint> GetTotalReportCountByIdAsync(Guid userId, string neighboorhoodId)
         {
             await Task.CompletedTask;
             return FromNeighborhoodId(neighboorhoodId)?.TotalReportCount ?? throw new EntityNotFoundException();
--- tests/FunderMaps.Testing/Repositories/TestTrackingRepository.cs
+++ tests/FunderMaps.Testing/Repositories/TestTrackingRepository.cs
@@ -8,7 +8,7 @@ namespace FunderMaps.Testing.Repositories
 {
     public class TestTrackingRepository : ITrackingRepository
     {
-        public Task ProcessAnalysisUsageAsync(Guid userId, AnalysisProductType product, uint itemCount = 1, CancellationToken token = default) => Task.CompletedTask;
-        public Task ProcessStatisticsUsageAsync(Guid userId, StatisticsProductType product, uint itemCount = 1, CancellationToken token = default) => Task.CompletedTask;
+        public Task ProcessAnalysisUsageAsync(Guid userId, AnalysisProductType product, uint itemCount = 1) => Task.CompletedTask;
+        public Task ProcessStatisticsUsageAsync(Guid userId, StatisticsProductType product, uint itemCount = 1) => Task.CompletedTask;
     }
 }
