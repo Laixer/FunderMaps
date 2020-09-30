24a8d9e81856879a6b85812ce8319489a5af3945
Yorick de Wid <yorick17@outlook.com>
2020-09-30 14:17:20 +0200

    Create database context to be used in the repositories

---
+++ src/FunderMaps.Data/DbContext.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Threading.Tasks;
+using FunderMaps.Core;
+using FunderMaps.Core.Exceptions;
+using FunderMaps.Data.Providers;
+
+namespace FunderMaps.Data
+{
+    /// <summary>
+    ///     Database context.
+    /// </summary>
+    internal class DbContext : IAsyncDisposable
+    {
+        /// <summary>
+        ///     Data provider interface.
+        /// </summary>
+        public DbProvider DbProvider { get; set; }
+
+        /// <summary>
+        ///     Application context.
+        /// </summary>
+        public Core.AppContext AppContext { get; set; }
+
+        /// <summary>
+        ///     Database connection.
+        /// </summary>
+        public DbConnection Connection { get; private set; }
+
+        /// <summary>
+        ///     Database command.
+        /// </summary>
+        public DbCommand Command { get; private set; }
+
+        private DbDataReader reader;
+        public DbDataReader Reader
+        {
+            get => reader;
+            set
+            {
+                if (reader != null)
+                {
+                    reader.Dispose();
+                }
+                reader = value;
+            }
+        }
+
+        /// <summary>
+        ///     Initialize database context.
+        /// </summary>
+        /// <param name="cmdText">The text of the query.</param>
+        public async ValueTask InitializeAsync(string cmdText)
+        {
+            Connection = await DbProvider.OpenConnectionScopeAsync(AppContext.CancellationToken);
+            Command = DbProvider.CreateCommand(cmdText, Connection);
+        }
+
+        /// <summary>
+        ///     Add parameter with key and value to command.
+        /// </summary>
+        /// <remarks>
+        ///     Sets a database null value if the object value is null.
+        /// </remarks>
+        /// <param name="parameterName">Parameter name.</param>
+        /// <param name="value">Parameter value.</param>
+        public void AddParameterWithValue(string parameterName, object value)
+        {
+            var parameter = Command.CreateParameter();
+
+            parameter.ParameterName = parameterName;
+            parameter.Value = value ?? DBNull.Value;
+
+            if (value is string && string.IsNullOrEmpty(value as string))
+            {
+                parameter.Value = DBNull.Value;
+            }
+
+            Command.Parameters.Add(parameter);
+        }
+
+        // FUTURE: Do not depend on Npgsql. Too npgsql specific.
+        /// <summary>
+        ///     Add parameter with key and json value to command.
+        /// </summary>
+        /// <remarks>
+        ///     Sets a database null value if the object value is null.
+        /// </remarks>
+        /// <param name="parameterName">Parameter name.</param>
+        /// <param name="value">Parameter value.</param>
+        public void AddJsonParameterWithValue(string parameterName, object value)
+        {
+            var parameter = new Npgsql.NpgsqlParameter(parameterName, NpgsqlTypes.NpgsqlDbType.Jsonb)
+            {
+                Value = value ?? DBNull.Value
+            };
+
+            Command.Parameters.Add(parameter);
+        }
+
+        /// <summary>
+        ///     Execute command and return a reader.
+        /// </summary>
+        /// <param name="readAhead">Read to first row.</param>
+        /// <param name="hasRowsGuard">Throw if no rows returned.</param>
+        public async ValueTask<DbDataReader> ReaderAsync(bool readAhead = true, bool hasRowsGuard = true)
+        {
+            Reader = await Command.ExecuteReaderAsync(AppContext.CancellationToken);
+            if (!Reader.HasRows && hasRowsGuard)
+            {
+                throw new EntityNotFoundException();
+            }
+
+            if (readAhead)
+            {
+                await Reader.ReadAsync(AppContext.CancellationToken);
+            }
+
+            return Reader;
+        }
+
+        /// <summary>
+        ///     Execute command and return a reader per row.
+        /// </summary>
+        /// <param name="hasRowsGuard">Throw if no rows returned.</param>
+        public async IAsyncEnumerable<DbDataReader> EnumerableReaderAsync(bool hasRowsGuard = false)
+        {
+            Reader = await Command.ExecuteReaderAsync(AppContext.CancellationToken);
+            if (!Reader.HasRows && hasRowsGuard)
+            {
+                throw new EntityNotFoundException();
+            }
+
+            while (await Reader.ReadAsync(AppContext.CancellationToken))
+            {
+                yield return Reader;
+            }
+        }
+
+        /// <summary>
+        ///     Execute command.
+        /// </summary>
+        /// <param name="affectedGuard">Throw if no rows were affected.</param>
+        public async ValueTask NonQueryAsync(bool affectedGuard = true)
+        {
+            var affected = await Command.ExecuteNonQueryAsync(AppContext.CancellationToken);
+            if (affected <= 0 && affectedGuard)
+            {
+                throw new EntityNotFoundException();
+            }
+        }
+
+        /// <summary>
+        ///     Execute command and return scalar result.
+        /// </summary>
+        /// <param name="resultGuard">Throw if no result was returned.</param>
+        public async ValueTask<TResult> ScalarAsync<TResult>(bool resultGuard = true)
+        {
+            var result = await Command.ExecuteScalarAsync(AppContext.CancellationToken);
+            if (result == null && resultGuard)
+            {
+                throw new EntityNotFoundException();
+            }
+
+            return (TResult)result;
+        }
+
+        /// <summary>
+        ///     Dispose unmanaged resources.
+        /// </summary>
+        public async ValueTask DisposeAsync()
+        {
+            if (Reader != null)
+            {
+                await Reader.DisposeAsync();
+            }
+
+            await Command.DisposeAsync();
+            await Connection.DisposeAsync();
+        }
+    }
+}
