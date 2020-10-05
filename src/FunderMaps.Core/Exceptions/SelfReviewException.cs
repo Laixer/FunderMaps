8a298d4fc89024c656831952eb69d00f53975048
Thomas Beckers <tabeckers95@gmail.com>
2020-10-05 13:21:56 +0200

    Create SelfReviewException.cs

---
+++ src/FunderMaps.Core/Exceptions/SelfReviewException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FunderMaps.Core.Exceptions
+{
+    /// <summary>
+    ///     Indicates the situation where a user tries to assign himself
+    ///     as reviewer on an inquiry that he or she has created.
+    /// </summary>
+    public sealed class SelfReviewException : FunderMapsCoreException
+    {
+        /// <summary>
+        ///     Create new instance.
+        /// </summary>
+        public SelfReviewException()
+        {
+        }
+
+        /// <summary>
+        ///     Create new instance.
+        /// </summary>
+        /// <param name="message">The message to display.</param>
+        public SelfReviewException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        ///     Create new instance.
+        /// </summary>
+        /// <param name="message">The message to display.</param>
+        /// <param name="innerException">The inner <see cref="Exception"/>.</param>
+        public SelfReviewException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
