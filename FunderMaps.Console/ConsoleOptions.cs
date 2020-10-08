b53286c08e5aeabbf262b0bb4b67f9095ba627e3
Thomas Beckers <tabeckers95@gmail.com>
2020-10-08 15:26:32 +0200

    Created console options and constants

---
+++ FunderMaps.Console/ConsoleOptions.cs
@@ -0,0 +1,40 @@
+﻿namespace FunderMaps.Console
+{
+    /// <summary>
+    ///     Wrapper for all our console options.
+    /// </summary>
+    public sealed class ConsoleOptions
+    {
+        // TODO We already have these properties in the connection string, might be a bit too double?
+
+        /// <summary>
+        ///     The database host address.
+        /// </summary>
+        public string DatabaseHost { get; set; }
+
+        /// <summary>
+        ///     The database username.
+        /// </summary>
+        public string DatabaseUser { get; set; }
+
+        /// <summary>
+        ///     The database user password.
+        /// </summary>
+        public string DatabasePassword { get; set; }
+
+        /// <summary>
+        ///     The name of the database to extract from.
+        /// </summary>
+        public string DatabaseName { get; set; }
+
+        /// <summary>
+        ///     Minimum mvt tile zoom.
+        /// </summary>
+        public uint MvtMinZoom { get; set; }
+
+        /// <summary>
+        ///     Maximum mvt tile zoom.
+        /// </summary>
+        public uint MvtMaxZoom { get; set; }
+    }
+}
