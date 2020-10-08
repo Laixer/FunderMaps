3ecaca99505c4160cad9f792aab9d977588cc37f
Thomas Beckers <tabeckers95@gmail.com>
2020-10-08 15:30:16 +0200

    Created access type for files

---
+++ src/FunderMaps.Core/Types/AcessType.cs
@@ -0,0 +1,18 @@
+﻿namespace FunderMaps.Core.Types
+{
+    /// <summary>
+    ///     Enum representing a file access type.
+    /// </summary>
+    public enum AccessType
+    {
+        /// <summary>
+        ///     Indicates we want to upload a file.
+        /// </summary>
+        Write,
+
+        /// <summary>
+        ///     Indicates we want to download a file.
+        /// </summary>
+        Read,
+    }
+}
