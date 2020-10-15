4a96d97a2705de8aa161f7c4b0118d426d93287f
Yorick de Wid <yorick17@outlook.com>
2020-10-15 14:46:16 +0200

    Move common logic and DTOs into FunderMaps.AspNetCore
    - Move allowed file mimes to core

--- src/FunderMaps.WebApi/Constants.cs
+++ src/FunderMaps.WebApi/Constants.cs
@@ -6,31 +6,6 @@
     internal static class Constants
     {
         /// <summary>
-        ///     Allowed file mime types in the application.
-        /// </summary>
-        /// <remarks>
-        ///     These mime types are whitelisted. Every other
-        ///     file type is not allowed in the application.
-        ///     <para>
-        ///         Controllers can take a subset of this list
-        ///         for specific methods.
-        ///     </para>
-        /// </remarks>
-        internal static readonly string[] AllowedFileMimes =
-        {
-            "application/pdf",
-
-            "image/png",
-            "image/jpeg",
-            "image/gif",
-            "image/bmp",
-            "image/tiff",
-            "image/webp",
-
-            "text/plain",
-        };
-
-        /// <summary>
         ///     Incident gateway name.
         /// </summary>
         internal const string IncidentGateway = "FunderMaps.WebApi";
