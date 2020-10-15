f4b3c97f62701f1bf486a40d138fd3c39bd5018e
Yorick de Wid <yorick17@outlook.com>
2020-10-15 14:29:59 +0200

    Move portal logic into FunderMaps.Portal

---
+++ src/FunderMaps.Portal/Constants.cs
@@ -0,0 +1,13 @@
+namespace FunderMaps
+{
+    /// <summary>
+    ///     Application constants.
+    /// </summary>
+    internal static class Constants
+    {
+        /// <summary>
+        ///     Incident gateway name.
+        /// </summary>
+        internal const string IncidentGateway = "FunderMaps.Portal";
+    }
+}
