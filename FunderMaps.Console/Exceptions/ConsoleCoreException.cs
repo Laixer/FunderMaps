66aaffa34b40980325b91842de7c38bbce3af6a3
Thomas Beckers <tabeckers95@gmail.com>
2020-10-27 15:25:39 +0100

    Created console exceptions

---
+++ FunderMaps.Console/Exceptions/ConsoleCoreException.cs
@@ -0,0 +1,28 @@
+﻿using FunderMaps.Core.Exceptions;
+using System;
+using System.Runtime.Serialization;
+
+namespace FunderMaps.Console.Exceptions
+{
+    /// <summary>
+    ///     Base exception for all console related errors.
+    /// </summary>
+    public abstract class ConsoleCoreException : FunderMapsCoreException
+    {
+        public ConsoleCoreException()
+        {
+        }
+
+        public ConsoleCoreException(string message) : base(message)
+        {
+        }
+
+        public ConsoleCoreException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ConsoleCoreException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ FunderMaps.Console/Exceptions/TaskTypeNotSupportedException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FunderMaps.Console.Exceptions
+{
+    /// <summary>
+    ///     Indicates a type of task is not supported.
+    /// </summary>
+    public sealed class TaskTypeNotSupportedException : ConsoleCoreException
+    {
+        public TaskTypeNotSupportedException()
+        {
+        }
+
+        public TaskTypeNotSupportedException(string message) : base(message)
+        {
+        }
+
+        public TaskTypeNotSupportedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public TaskTypeNotSupportedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
