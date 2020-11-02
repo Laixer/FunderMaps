5860f29f1058db4f85296ca04ed91e098d865da2
Yorick de Wid <yorickdewid@users.noreply.github.com>
2020-11-02 14:43:21 +0100

    Move context status into class

--- src/FunderMaps.Core/Notification/NotifyContext.cs
+++ src/FunderMaps.Core/Notification/NotifyContext.cs
@@ -1,32 +1,6 @@
 namespace FunderMaps.Core.Notification
 {
     /// <summary>
-    ///     Notification handler status.
-    /// </summary>
-    public enum NotifyContextStatus
-    {
-        /// <summary>
-        ///     Notify was submitted to handler.
-        /// </summary>
-        Submitted,
-
-        /// <summary>
-        ///     Notification was handled with success.
-        /// </summary>
-        Success,
-
-        /// <summary>
-        ///     Notification was handled with fail.
-        /// </summary>
-        Failed,
-
-        /// <summary>
-        ///     Notification was not handled.
-        /// </summary>
-        NotHandled
-    }
-
-    /// <summary>
     ///     Notification context.
     /// </summary>
     public class NotifyContext
