5fec211eef632c7acc463162ff847cd251ee014d
Yorick de Wid <yorickdewid@users.noreply.github.com>
2020-11-03 14:02:33 +0100

    Enqueue tasks directly

--- src/FunderMaps.Core/Threading/DispatchManager.cs
+++ src/FunderMaps.Core/Threading/DispatchManager.cs
@@ -1,5 +1,6 @@
 ﻿using FunderMaps.Core.Exceptions;
 using FunderMaps.Core.Types.BackgroundTasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
@@ -17,13 +18,14 @@ namespace FunderMaps.Core.Threading
     /// </remarks>
     public class DispatchManager : IDisposable
     {
-        private readonly CancellationTokenSource cts = new CancellationTokenSource();
+        private CancellationTokenSource cts = new CancellationTokenSource();
         private readonly int defaultWorkerThreads;
         private readonly int defaultCompletionPortThreads;
 
         private readonly IEnumerable<BackgroundTask> _backgroundTasks;
         private readonly BackgroundWorkOptions _options;
         private readonly ILogger<DispatchManager> _logger;
+        private readonly IServiceProvider _serviceProvider;
 
         /// <summary>
         ///     Create new instance.
@@ -31,14 +33,24 @@ namespace FunderMaps.Core.Threading
         public DispatchManager(
             IEnumerable<BackgroundTask> backgroundTasks,
             IOptions<BackgroundWorkOptions> options,
-            ILogger<DispatchManager> logger)
+            ILogger<DispatchManager> logger,
+            IServiceProvider serviceProvider)
         {
             _backgroundTasks = backgroundTasks ?? throw new ArgumentNullException(nameof(backgroundTasks));
             _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
 
             ThreadPool.GetMaxThreads(out defaultWorkerThreads, out defaultCompletionPortThreads);
             ThreadPool.SetMaxThreads(_options.MaxWorkers, _options.MaxWorkers);
+        }
+
+        /// <summary>
+        ///     Destroy instance.
+        /// </summary>
+        ~DispatchManager()
+        {
+            ThreadPool.SetMaxThreads(defaultWorkerThreads, defaultCompletionPortThreads);
         }
 
         /// <summary>
@@ -57,19 +69,54 @@ namespace FunderMaps.Core.Threading
                 throw new ArgumentNullException(nameof(value));
             }
 
+            foreach (var backgroundTask in _backgroundTasks)
+            {
+                if (backgroundTask.CanHandle(value))
+                {
+                    QueueTaskItem(backgroundTask, value);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Enqueues an object to process onto the queue with provided task.
+        /// </summary>
+        /// <param name="value">The object to process.</param>
+        public void EnqueueTask<TTask>(object value)
+            where TTask : BackgroundTask
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            QueueTaskItem(ActivatorUtilities.CreateInstance<TTask>(_serviceProvider), value);
+        }
+
+        /// <summary>
+        ///     Dispatch task item to the background, then return.
+        /// </summary>
+        protected virtual void QueueTaskItem(BackgroundTask backgroundTask, object value)
+        {
             if (ThreadPool.PendingWorkItemCount > _options.MaxQueueSize)
             {
                 throw new QueueFullException();
             }
 
-            // TODO This can exceed the max queue count but I don't think that's a problem.
-            foreach (var backgroundTask in _backgroundTasks)
+            // TODO: Rewrite {
+            var appContext = _serviceProvider.GetRequiredService<AppContext>();
+            cts = CancellationTokenSource.CreateLinkedTokenSource(cts.Token, appContext.CancellationToken);
+
+            var context = new BackgroundTaskContext
             {
-                if (backgroundTask.CanHandle(value))
-                {
-                    ThreadPool.QueueUserWorkItem(BuildWaitCallback(backgroundTask, value));
-                }
-            }
+                CancellationToken = cts.Token,
+                Value = value,
+            };
+            // }
+
+            _logger.LogInformation($"Queue background task {context.Id}");
+
+            ThreadPool.QueueUserWorkItem(BuildWaitCallback(backgroundTask, context));
         }
 
         /// <summary>
@@ -79,21 +126,15 @@ namespace FunderMaps.Core.Threading
         /// <remarks>
         ///     This creates a new <see cref="BackgroundTaskContext"/>.
         /// </remarks>
-        /// <param name="backgroundTask">The background task to execute.</param>
-        /// <param name="value">The enqueued value object.</param>
-        private WaitCallback BuildWaitCallback(BackgroundTask backgroundTask, object value)
+        /// <param name="backgroundTask">Background task to execute.</param>
+        /// <param name="context">Background task execution context.</param>
+        private WaitCallback BuildWaitCallback(BackgroundTask backgroundTask, BackgroundTaskContext context)
         {
-            var context = new BackgroundTaskContext
-            {
-                CancellationToken = cts.Token,
-                Value = value,
-            };
-
             return async (object o) =>
             {
                 try
                 {
-                    _logger.LogDebug($"Starting background task {context.Id}");
+                    _logger.LogInformation($"Starting background task {context.Id}");
 
                     await backgroundTask.ProcessAsync(context);
                 }
@@ -116,8 +157,6 @@ namespace FunderMaps.Core.Threading
         /// </remarks>
         public void Dispose()
         {
-            ThreadPool.SetMaxThreads(defaultWorkerThreads, defaultCompletionPortThreads);
-
             cts.Dispose();
         }
     }
