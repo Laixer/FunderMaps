aec513ee6c5e647b3574d1bbe13c9fef47e0a204
Yorick de Wid <yorickdewid@users.noreply.github.com>
2020-11-10 15:53:51 +0100

    Rewrite the notification hub, now using batch service as backend

---
+++ src/FunderMaps.Core/Interfaces/IBatchService.cs
@@ -0,0 +1,24 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunderMaps.Core.Interfaces
+{
+    /// <summary>
+    ///     Batch service.
+    /// </summary>
+    public interface IBatchService
+    {
+        /// <summary>
+        ///     Add task to batch queue.
+        /// </summary>
+        /// <param name="name">Name of task to run.</param>
+        /// <param name="value">Task payload.</param>
+        /// <param name="token">Canellation token.</param>
+        Task EnqueueAsync(string name, object value, CancellationToken token = default);
+
+        /// <summary>
+        ///     Test the batch service backend.
+        /// </summary>
+        Task TestService();
+    }
+}
