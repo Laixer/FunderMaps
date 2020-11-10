6fc3ebd78e885806751b658b7ad142a8c23b3d06
Yorick de Wid <yorickdewid@users.noreply.github.com>
2020-11-10 15:11:53 +0100

    FIX: Forgotten include

--- src/FunderMaps.BatchNode/Command/CommandTask.cs
+++ src/FunderMaps.BatchNode/Command/CommandTask.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using FunderMaps.Core.Exceptions;
 using FunderMaps.Core.Threading;
 using Microsoft.Extensions.Logging;
 
