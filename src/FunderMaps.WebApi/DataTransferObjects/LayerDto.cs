ca1c480cf61c775623a4ad55c8be84c36134a522
Patrick Nobbe <patrick.nobbe@hotmail.com>
2020-11-11 16:35:09 +0100

    Add LayerDto

---
+++ src/FunderMaps.WebApi/DataTransferObjects/LayerDto.cs
@@ -0,0 +1,31 @@
+﻿using FunderMaps.Core.Types.MapLayer;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FunderMaps.WebApi.DataTransferObjects
+{
+    /// <summary>
+    ///     Layer DTO.
+    /// </summary>
+    public class LayerDto
+    {
+        /// <summary>
+        ///     Layer identifier.
+        /// </summary>
+        [Required]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        ///     Layer full name
+        /// </summary>
+        [Required]
+        public string FullName { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the display name for the bundle.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
