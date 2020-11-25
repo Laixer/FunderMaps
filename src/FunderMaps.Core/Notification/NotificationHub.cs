76d10b9e17aa26fb51fc2d001bfa2140b32eee2f
Yorick de Wid <yorickdewid@users.noreply.github.com>
2020-11-25 16:24:54 +0100

    Notify specialization with task name

--- src/FunderMaps.Core/Notification/NotificationHub.cs
+++ src/FunderMaps.Core/Notification/NotificationHub.cs
@@ -29,6 +29,14 @@ namespace FunderMaps.Core.Notification
         /// <param name="envelope">Envelope containing the notification.</param>
         public Task DispatchNotifyAsync(Envelope envelope)
             => _batchService.EnqueueAsync(TaskName, envelope, AppContext.CancellationToken);
+
+        /// <summary>
+        ///     Notify by means of contacting.
+        /// </summary>
+        /// <param name="taskName">Name of the task to handle the job.</param>
+        /// <param name="envelope">Envelope containing the notification.</param>
+        public Task DispatchNotifyAsync(string taskName, Envelope envelope)
+            => _batchService.EnqueueAsync(taskName, envelope, AppContext.CancellationToken);            
     }
 }
 #pragma warning restore CA1812 // Internal class is never instantiated
