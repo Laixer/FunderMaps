f0ce72bee28367d9eca2856956e8adba413a8926
Yorick de Wid <yorickdewid@users.noreply.github.com>
2020-12-14 11:28:38 +0100

    If a legacy bag id is found, then try to convert to BAG 2.0
    - Always check legth first, this could be faster

--- src/FunderMaps.Core/Interfaces/IGeocoderParser.cs
+++ src/FunderMaps.Core/Interfaces/IGeocoderParser.cs
@@ -13,5 +13,13 @@ namespace FunderMaps.Core.Interfaces
         /// <param name="input">Input identifier.</param>
         /// <returns>Geocoder datasource via <see cref="GeocoderDatasource"/>.</returns>
         GeocoderDatasource FromIdentifier(string input);
+
+        /// <summary>
+        ///     Identify the geocoder datasource from the input and return repaired identifier.
+        /// </summary>
+        /// <param name="input">Input identifier.</param>
+        /// <param name="output">Output identifier.</param>
+        /// <returns>Geocoder datasource via <see cref="GeocoderDatasource"/>.</returns>
+        GeocoderDatasource FromIdentifier(string input, out string output);
     }
 }
--- src/FunderMaps.Core/Types/GeocoderDatasource.cs
+++ src/FunderMaps.Core/Types/GeocoderDatasource.cs
@@ -21,28 +21,38 @@ namespace FunderMaps.Core.Types
         NlBagAddress = 2,
 
         /// <summary>
+        ///     Basis Registratie Gebouwen (BAG) legacy building identifier.
+        /// </summary>
+        NlBagLegacyBuilding = 8,
+
+        /// <summary>
+        ///     Basis Registratie Gebouwen (BAG) legacy address identifier.
+        /// </summary>
+        NlBagLegacyAddress = 9,
+
+        /// <summary>
         ///     Centraal Bureau Statistiek (CBS) neighborhoor identifier.
         /// </summary>
-        NlCbsNeighborhood = 5,
+        NlCbsNeighborhood = 15,
 
         /// <summary>
         ///     Centraal Bureau Statistiek (CBS) district identifier.
         /// </summary>
-        NlCbsDistrict = 6,
+        NlCbsDistrict = 16,
 
         /// <summary>
         ///     Centraal Bureau Statistiek (CBS) municipality identifier.
         /// </summary>
-        NlCbsMunicipality = 7,
+        NlCbsMunicipality = 17,
 
         /// <summary>
         ///     Centraal Bureau Statistiek (CBS) state identifier.
         /// </summary>
-        NlCbsState = 8,
+        NlCbsState = 18,
 
         /// <summary>
         ///     Unknown identifier.
         /// </summary>
-        Unknown = 10,
+        Unknown = 50,
     }
 }
