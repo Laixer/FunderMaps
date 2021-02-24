56e23bfe7ceef296317eec8db67996e00db62032
Patrick Nobbe <patrick.nobbe@hotmail.com>
2021-02-24 16:46:47 +0100

    Implement IRecoverySampleRepository interface in tests

--- tests/FunderMaps.Testing/Repositories/TestRecoverySampleRepository.cs
+++ tests/FunderMaps.Testing/Repositories/TestRecoverySampleRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Bogus;
+using FunderMaps.Core;
 using FunderMaps.Core.Entities;
 using FunderMaps.Core.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FunderMaps.Testing.Repositories
@@ -19,5 +22,17 @@ namespace FunderMaps.Testing.Repositories
             entity.Id = randomizer.Int(0, int.MaxValue);
             return base.AddAsync(entity);
         }
+        IAsyncEnumerable<RecoverySample> IRecoverySampleRepository.ListAllAsync(int report, Navigation navigation)
+        {
+            var result = DataStore.ItemList.Where(e => e.Recovery == report);
+            return Helper.AsAsyncEnumerable(Helper.ApplyNavigation(result, navigation));
+        }
+
+        Task<long> IRecoverySampleRepository.CountAsync(int report)
+        {
+            var result = DataStore.ItemList.Where(e => e.Recovery == report);
+            return Task.FromResult((long)result.Count());
+        }
+
     }
 }
