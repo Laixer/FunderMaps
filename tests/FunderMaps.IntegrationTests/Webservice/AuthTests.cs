8c0a47a839615bdd7ac2da546afe142125edafd5
Yorick de Wid <ydewid@gmail.com>
2021-03-06 15:52:49 +0100

    Rewrite auth test

--- tests/FunderMaps.IntegrationTests/Webservice/AuthTests.cs
+++ tests/FunderMaps.IntegrationTests/Webservice/AuthTests.cs
@@ -1,11 +1,7 @@
 ﻿using Bogus;
 using FunderMaps.AspNetCore.DataTransferObjects;
 using FunderMaps.AspNetCore.InputModels;
-using FunderMaps.Core.Types;
-using FunderMaps.IntegrationTests.Backend;
 using FunderMaps.Testing.Extensions;
-using FunderMaps.Testing.Faker;
-using FunderMaps.WebApi.DataTransferObjects;
 using System;
 using System.Net;
 using System.Net.Http.Json;
@@ -19,36 +15,26 @@ namespace FunderMaps.IntegrationTests.Webservice
     /// </summary>
     public class AuthTests : IClassFixture<AuthWebserviceWebApplicationFactory>
     {
-        private readonly AuthWebserviceWebApplicationFactory _factory;
+        private AuthWebserviceWebApplicationFactory Factory { get; }
 
         /// <summary>
         ///     Create new instance.
         /// </summary>
-        public AuthTests(AuthWebserviceWebApplicationFactory factory) => _factory = factory;
+        public AuthTests(AuthWebserviceWebApplicationFactory factory)
+            => Factory = factory;
 
         [Fact]
         public async Task SignInReturnSuccessAndToken()
         {
             // Arrange
-            var client1 = new AuthBackendWebApplicationFactory()
-                .ConfigureAuthentication(options => options.User.Role = ApplicationRole.Administrator)
-                .WithAuthenticationStores()
-                .CreateClient();
-            var organizationProp = await client1.PostAsJsonGetFromJsonAsync<OrganizationProposalDto, OrganizationProposalDto>("api/organization/proposal", new OrganizationProposalDtoFaker().Generate());
-            var client2 = new BackendWebApplicationFactory()
-                .CreateClient();
-            var organizationSetup = new OrganizationSetupDtoFaker().Generate();
-            var organization = await client2.PostAsJsonAsync($"api/organization/{organizationProp.Id}/setup", organizationSetup);
-            var client3 = _factory
-                .CreateClient();
-            var signIn = new SignInInputModel
-            {
-                Email = organizationSetup.Email,
-                Password = organizationSetup.Password,
-            };
+            var client = Factory.CreateClient();
 
             // Act
-            var response = await client3.PostAsJsonAsync("api/auth/signin", signIn);
+            var response = await client.PostAsJsonAsync("api/auth/signin", new SignInInputModel()
+            {
+                Email = Factory.OrganizationSetup.Email,
+                Password = Factory.OrganizationSetup.Password,
+            });
             var returnObject = await response.Content.ReadFromJsonAsync<SignInSecurityTokenDto>();
 
             // Assert
@@ -62,9 +48,7 @@ namespace FunderMaps.IntegrationTests.Webservice
         public async Task RefreshSignInReturnSuccessAndToken()
         {
             // Arrange
-            var client = _factory
-                .WithAuthenticationStores()
-                .CreateClient();
+            var client = Factory.CreateClient();
 
             // Act
             var response = await client.GetAsync("api/auth/token-refresh");
@@ -81,48 +65,20 @@ namespace FunderMaps.IntegrationTests.Webservice
         public async Task SignInInvalidCredentialsReturnError()
         {
             // Arrange
-            var client1 = new AuthBackendWebApplicationFactory()
-                .ConfigureAuthentication(options => options.User.Role = ApplicationRole.Administrator)
-                .WithAuthenticationStores()
-                .CreateClient();
-            var organizationProp = await client1.PostAsJsonGetFromJsonAsync<OrganizationProposalDto, OrganizationProposalDto>("api/organization/proposal", new OrganizationProposalDtoFaker().Generate());
-            var client2 = new BackendWebApplicationFactory()
-                .CreateClient();
-            var organizationSetup = new OrganizationSetupDtoFaker().Generate();
-            var organization = await client2.PostAsJsonAsync($"api/organization/{organizationProp.Id}/setup", organizationSetup);
-            var client3 = _factory
-                .CreateClient();
-            var signIn = new SignInInputModel
-            {
-                Email = organizationSetup.Email,
-                Password = new Randomizer().Password(64),
-            };
+            var client = Factory.CreateClient();
 
             // Act
-            var response = await client3.PostAsJsonAsync("api/auth/signin", signIn);
+            var response = await client.PostAsJsonAsync("api/auth/signin", new SignInInputModel()
+            {
+                Email = Factory.OrganizationSetup.Email,
+                Password = new Randomizer().Password(64),
+            });
             var returnObject = await response.Content.ReadFromJsonAsync<ProblemModel>();
 
             // Assert
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
             Assert.Equal((short)HttpStatusCode.Unauthorized, returnObject.Status);
             Assert.Contains("Login", returnObject.Title, StringComparison.InvariantCultureIgnoreCase);
-        }
-
-        [Theory]
-        [InlineData("/")]
-        [InlineData("api/user")]
-        [InlineData("api/auth/token-refresh")]
-        public async Task RefreshSignInReturnUnauthorized(string uri)
-        {
-            // Arrange
-            using var factory = new BackendWebApplicationFactory();
-            var client = factory.CreateClient();
-
-            // Act
-            var response = await client.GetAsync(uri);
-
-            // Assert
-            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
     }
 }
--- tests/FunderMaps.IntegrationTests/Webservice/WebserviceWebApplicationFactory.cs
+++ tests/FunderMaps.IntegrationTests/Webservice/WebserviceWebApplicationFactory.cs
@@ -1,4 +1,15 @@
-﻿using FunderMaps.Webservice;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FunderMaps.AspNetCore.DataTransferObjects;
+using FunderMaps.AspNetCore.InputModels;
+using FunderMaps.Core.Types;
+using FunderMaps.IntegrationTests.Backend;
+using FunderMaps.Testing.Faker;
+using FunderMaps.WebApi.DataTransferObjects;
+using FunderMaps.Webservice;
 
 namespace FunderMaps.IntegrationTests.Webservice
 {
@@ -6,7 +17,75 @@ namespace FunderMaps.IntegrationTests.Webservice
     {
     }
 
-    public class AuthWebserviceWebApplicationFactory : AuthWebApplicationFactory<Startup>
+    public class AuthWebserviceWebApplicationFactory : CustomWebApplicationFactory<Startup>
     {
+        private readonly HttpClient administratorBackendAppClient;
+        private readonly HttpClient publicBackendAppClient;
+
+        public SignInSecurityTokenDto AuthToken { get; set; }
+        public OrganizationProposalDto OrganizationProposal { get; private set; }
+        public OrganizationSetupDto OrganizationSetup { get; private set; }
+        public OrganizationDto Organization { get; private set; }
+
+        public AuthWebserviceWebApplicationFactory()
+        {
+            administratorBackendAppClient = new AuthBackendWebApplicationFactory()
+                .ConfigureAuthentication(options => options.User.Role = ApplicationRole.Administrator)
+                .WithAuthenticationStores()
+                .CreateClient();
+
+            publicBackendAppClient = new BackendWebApplicationFactory()
+                .CreateClient();
+
+            OrganizationProposal = new OrganizationProposalDtoFaker().Generate();
+            OrganizationSetup = new OrganizationSetupDtoFaker().Generate();
+
+            ConfigureOrganizationAsync().Wait();
+            SignInAsync().Wait();
+        }
+
+        protected async virtual Task ConfigureOrganizationAsync()
+        {
+            OrganizationProposal = await administratorBackendAppClient.PostAsJsonGetFromJsonAsync<OrganizationProposalDto, OrganizationProposalDto>("api/organization/proposal", OrganizationProposal);
+
+            await publicBackendAppClient.PostAsJsonAsync($"api/organization/{OrganizationProposal.Id}/setup", OrganizationSetup);
+
+            Organization = await administratorBackendAppClient.GetFromJsonAsync<OrganizationDto>($"api/admin/organization/{OrganizationProposal.Id}");
+        }
+
+        protected async virtual Task SignInAsync()
+        {
+            AuthToken = await publicBackendAppClient.PostAsJsonGetFromJsonAsync<SignInSecurityTokenDto, SignInInputModel>("api/auth/signin", new()
+            {
+                Email = OrganizationSetup.Email,
+                Password = OrganizationSetup.Password,
+            });
+        }
+
+        protected async virtual Task TeardownOrganization()
+        {
+            foreach (var user in await administratorBackendAppClient.GetFromJsonAsync<IList<OrganizationUserDto>>($"api/admin/organization/{Organization.Id}/user"))
+            {
+                await administratorBackendAppClient.DeleteAsync($"api/admin/organization/{Organization.Id}/user/{user.Id}");
+            }
+            await administratorBackendAppClient.DeleteAsync($"api/admin/organization/{Organization.Id}");
+        }
+
+        protected override void ConfigureClient(HttpClient client)
+        {
+            base.ConfigureClient(client);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AuthToken.Token);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            TeardownOrganization().Wait();
+
+            publicBackendAppClient.Dispose();
+            administratorBackendAppClient.Dispose();
+
+            base.Dispose(disposing);
+        }
     }
 }
