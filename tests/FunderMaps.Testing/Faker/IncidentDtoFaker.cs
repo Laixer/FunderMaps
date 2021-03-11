4b392de7457e729caee055ca4661d313575cf2e1
Yorick de Wid <ydewid@gmail.com>
2021-03-11 21:20:50 +0100

    Randomize null properties

--- tests/FunderMaps.Testing/Faker/IncidentDtoFaker.cs
+++ tests/FunderMaps.Testing/Faker/IncidentDtoFaker.cs
@@ -18,15 +18,15 @@ namespace FunderMaps.Testing.Faker
         public IncidentDtoFaker()
         {
             RuleFor(f => f.Id, f => f.Random.Replace("FIR######-#####"));
-            RuleFor(f => f.FoundationType, f => f.PickRandom<FoundationType>());
+            RuleFor(f => f.FoundationType, f => f.PickRandom<FoundationType>().OrNull(f, .3f));;
             RuleFor(f => f.ChainedBuilding, f => f.Random.Bool());
             RuleFor(f => f.Owner, f => f.Random.Bool());
             RuleFor(f => f.FoundationRecovery, f => f.Random.Bool());
             RuleFor(f => f.NeighborRecovery, f => f.Random.Bool());
-            RuleFor(f => f.FoundationDamageCause, f => f.PickRandom<FoundationDamageCause>());
+            RuleFor(f => f.FoundationDamageCause, f => f.PickRandom<FoundationDamageCause>().OrNull(f, .2f));
             RuleFor(f => f.DocumentFile, f => Enumerable.Range(0, f.Random.Int(0, 3)).Select(x => f.System.FileName()).ToArray());
-            RuleFor(f => f.Note, f => f.Lorem.Text());
-            RuleFor(f => f.InternalNote, f => f.Lorem.Text());
+            RuleFor(f => f.Note, f => f.Lorem.Text().OrNull(f, .5f));
+            RuleFor(f => f.InternalNote, f => f.Lorem.Text().OrNull(f, .8f));
             RuleFor(f => f.AuditStatus, f => f.PickRandom<AuditStatus>());
             RuleFor(f => f.QuestionType, f => f.PickRandom<IncidentQuestionType>());
             RuleFor(f => f.FoundationDamageCharacteristics, f => f.Random.ArrayElements((FoundationDamageCharacteristics[])Enum.GetValues(typeof(FoundationDamageCharacteristics))));
--- tests/FunderMaps.Testing/Faker/InquiryDtoFaker.cs
+++ tests/FunderMaps.Testing/Faker/InquiryDtoFaker.cs
@@ -22,7 +22,7 @@ namespace FunderMaps.Testing.Faker
             RuleFor(f => f.Inspection, f => f.Random.Bool());
             RuleFor(f => f.JointMeasurement, f => f.Random.Bool());
             RuleFor(f => f.FloorMeasurement, f => f.Random.Bool());
-            RuleFor(f => f.Note, f => f.Lorem.Text());
+            RuleFor(f => f.Note, f => f.Lorem.Text().OrNull(f, .7f));
             RuleFor(f => f.DocumentDate, f => f.Date.Between(DateTime.Parse("1000-01-01"), DateTime.Now));
             RuleFor(f => f.DocumentFile, f => f.Internet.RemoteFileWithSecureUrl());
             RuleFor(f => f.Type, f => f.PickRandom<InquiryType>());
