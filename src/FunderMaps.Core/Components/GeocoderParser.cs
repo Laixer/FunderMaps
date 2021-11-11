700f1fffa5300338a26f442fa6eda88378036861
Yorick de Wid <yorickdewid@users.noreply.github.com>
2021-11-11 13:41:12 +0100

    Minor improvements

--- src/FunderMaps.Core/Components/GeocoderParser.cs
+++ src/FunderMaps.Core/Components/GeocoderParser.cs
@@ -1,104 +1,103 @@
 using FunderMaps.Core.Interfaces;
 using FunderMaps.Core.Types;
 
-namespace FunderMaps.Core.Components
+namespace FunderMaps.Core.Components;
+
+/// <summary>
+///     Identify the geocoder datasource from the input.
+/// </summary>
+public class GeocoderParser : IGeocoderParser
 {
     /// <summary>
     ///     Identify the geocoder datasource from the input.
     /// </summary>
-    public class GeocoderParser : IGeocoderParser
-    {
-        /// <summary>
-        ///     Identify the geocoder datasource from the input.
-        /// </summary>
-        /// <param name="input">Input identifier.</param>
-        /// <returns>Geocoder datasource via <see cref="GeocoderDatasource"/>.</returns>
-        public virtual GeocoderDatasource FromIdentifier(string input)
-            => input switch
-            {
-                string when input.StartsWith("gfm-", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.FunderMaps,
-                string when input.StartsWith("NL.IMBAG.PAND", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlBagBuilding,
-                string when input.StartsWith("NL.IMBAG.LIGPLAATS", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlBagBerth,
-                string when input.StartsWith("NL.IMBAG.STANDPLAATS", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlBagPosting,
-                string when input.StartsWith("NL.IMBAG.NUMMERAANDUIDING", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlBagAddress,
-                string when input.Length == 10 && input.StartsWith("BU", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlCbsNeighborhood,
-                string when input.Length == 8 && input.StartsWith("WK", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlCbsDistrict,
-                string when input.Length == 6 && input.StartsWith("GM", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlCbsMunicipality,
-                string when input.Length == 4 && input.StartsWith("PV", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlCbsState,
-                string when input.Length == 16 && input.Substring(4, 2) == "10" => GeocoderDatasource.NlBagLegacyBuilding,
-                string when input.Length == 16 && input.Substring(4, 2) == "20" => GeocoderDatasource.NlBagLegacyAddress,
-                string when input.Length == 16 && input.Substring(4, 2) == "02" => GeocoderDatasource.NlBagLegacyBerth,
-                string when input.Length == 16 && input.Substring(4, 2) == "03" => GeocoderDatasource.NlBagLegacyPosting,
-                string when input.Length == 15 && input.Substring(3, 2) == "10" => GeocoderDatasource.NlBagLegacyBuilding,
-                string when input.Length == 15 && input.Substring(3, 2) == "20" => GeocoderDatasource.NlBagLegacyAddress,
-                string when input.Length == 15 && input.Substring(3, 2) == "02" => GeocoderDatasource.NlBagLegacyBerth,
-                string when input.Length == 15 && input.Substring(3, 2) == "03" => GeocoderDatasource.NlBagLegacyPosting,
-                _ => GeocoderDatasource.Unknown,
-            };
-
-        /// <summary>
-        ///     Identify the geocoder datasource from the input and return repaired identifier.
-        /// </summary>
-        /// <remarks>
-        ///     Try and fix invalid BAG identifiers. This should not be necessary however
-        ///     malformed BAG identifiers are common.
-        /// </remarks>
-        /// <param name="input">Input identifier.</param>
-        /// <param name="output">Output identifier.</param>
-        /// <returns>Geocoder datasource via <see cref="GeocoderDatasource"/>.</returns>
-        public virtual GeocoderDatasource FromIdentifier(string input, out string output)
+    /// <param name="input">Input identifier.</param>
+    /// <returns>Geocoder datasource via <see cref="GeocoderDatasource"/>.</returns>
+    public virtual GeocoderDatasource FromIdentifier(string input)
+        => input switch
         {
-            output = input;
+            string when input.StartsWith("gfm-", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.FunderMaps,
+            string when input.StartsWith("NL.IMBAG.PAND", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlBagBuilding,
+            string when input.StartsWith("NL.IMBAG.LIGPLAATS", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlBagBerth,
+            string when input.StartsWith("NL.IMBAG.STANDPLAATS", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlBagPosting,
+            string when input.StartsWith("NL.IMBAG.NUMMERAANDUIDING", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlBagAddress,
+            string when input.Length == 10 && input.StartsWith("BU", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlCbsNeighborhood,
+            string when input.Length == 8 && input.StartsWith("WK", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlCbsDistrict,
+            string when input.Length == 6 && input.StartsWith("GM", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlCbsMunicipality,
+            string when input.Length == 4 && input.StartsWith("PV", StringComparison.InvariantCultureIgnoreCase) => GeocoderDatasource.NlCbsState,
+            string when input.Length == 16 && input.Substring(4, 2) == "10" => GeocoderDatasource.NlBagLegacyBuilding,
+            string when input.Length == 16 && input.Substring(4, 2) == "20" => GeocoderDatasource.NlBagLegacyAddress,
+            string when input.Length == 16 && input.Substring(4, 2) == "02" => GeocoderDatasource.NlBagLegacyBerth,
+            string when input.Length == 16 && input.Substring(4, 2) == "03" => GeocoderDatasource.NlBagLegacyPosting,
+            string when input.Length == 15 && input.Substring(3, 2) == "10" => GeocoderDatasource.NlBagLegacyBuilding,
+            string when input.Length == 15 && input.Substring(3, 2) == "20" => GeocoderDatasource.NlBagLegacyAddress,
+            string when input.Length == 15 && input.Substring(3, 2) == "02" => GeocoderDatasource.NlBagLegacyBerth,
+            string when input.Length == 15 && input.Substring(3, 2) == "03" => GeocoderDatasource.NlBagLegacyPosting,
+            _ => GeocoderDatasource.Unknown,
+        };
 
-            GeocoderDatasource source = FromIdentifier(input);
-            switch (source)
-            {
-                case GeocoderDatasource.NlBagLegacyBuilding:
-                    if (input.Length == 16)
-                    {
-                        output = $"NL.IMBAG.PAND.{output}";
-                    }
-                    else if (input.Length == 15)
-                    {
-                        output = $"NL.IMBAG.PAND.0{output}";
-                    }
-                    return GeocoderDatasource.NlBagBuilding;
+    /// <summary>
+    ///     Identify the geocoder datasource from the input and return repaired identifier.
+    /// </summary>
+    /// <remarks>
+    ///     Try and fix invalid BAG identifiers. This should not be necessary however
+    ///     malformed BAG identifiers are common.
+    /// </remarks>
+    /// <param name="input">Input identifier.</param>
+    /// <param name="output">Output identifier.</param>
+    /// <returns>Geocoder datasource via <see cref="GeocoderDatasource"/>.</returns>
+    public virtual GeocoderDatasource FromIdentifier(string input, out string output)
+    {
+        output = input;
 
-                case GeocoderDatasource.NlBagLegacyAddress:
-                    if (input.Length == 16)
-                    {
-                        output = $"NL.IMBAG.NUMMERAANDUIDING.{output}";
-                    }
-                    else if (input.Length == 15)
-                    {
-                        output = $"NL.IMBAG.NUMMERAANDUIDING.0{output}";
-                    }
-                    return GeocoderDatasource.NlBagAddress;
+        GeocoderDatasource source = FromIdentifier(input);
+        switch (source)
+        {
+            case GeocoderDatasource.NlBagLegacyBuilding:
+                if (input.Length == 16)
+                {
+                    output = $"NL.IMBAG.PAND.{output}";
+                }
+                else if (input.Length == 15)
+                {
+                    output = $"NL.IMBAG.PAND.0{output}";
+                }
+                return GeocoderDatasource.NlBagBuilding;
 
-                case GeocoderDatasource.NlBagLegacyBerth:
-                    if (input.Length == 16)
-                    {
-                        output = $"NL.IMBAG.LIGPLAATS.{output}";
-                    }
-                    else if (input.Length == 15)
-                    {
-                        output = $"NL.IMBAG.LIGPLAATS.0{output}";
-                    }
-                    return GeocoderDatasource.NlBagBerth;
+            case GeocoderDatasource.NlBagLegacyAddress:
+                if (input.Length == 16)
+                {
+                    output = $"NL.IMBAG.NUMMERAANDUIDING.{output}";
+                }
+                else if (input.Length == 15)
+                {
+                    output = $"NL.IMBAG.NUMMERAANDUIDING.0{output}";
+                }
+                return GeocoderDatasource.NlBagAddress;
 
-                case GeocoderDatasource.NlBagLegacyPosting:
-                    if (input.Length == 16)
-                    {
-                        output = $"NL.IMBAG.STANDPLAATS.{output}";
-                    }
-                    else if (input.Length == 15)
-                    {
-                        output = $"NL.IMBAG.STANDPLAATS.0{output}";
-                    }
-                    return GeocoderDatasource.NlBagPosting;
-            }
+            case GeocoderDatasource.NlBagLegacyBerth:
+                if (input.Length == 16)
+                {
+                    output = $"NL.IMBAG.LIGPLAATS.{output}";
+                }
+                else if (input.Length == 15)
+                {
+                    output = $"NL.IMBAG.LIGPLAATS.0{output}";
+                }
+                return GeocoderDatasource.NlBagBerth;
 
-            return source;
+            case GeocoderDatasource.NlBagLegacyPosting:
+                if (input.Length == 16)
+                {
+                    output = $"NL.IMBAG.STANDPLAATS.{output}";
+                }
+                else if (input.Length == 15)
+                {
+                    output = $"NL.IMBAG.STANDPLAATS.0{output}";
+                }
+                return GeocoderDatasource.NlBagPosting;
         }
+
+        return source;
     }
 }
--- src/FunderMaps.Core/Components/PasswordHasher.cs
+++ src/FunderMaps.Core/Components/PasswordHasher.cs
@@ -3,138 +3,137 @@ using Microsoft.Extensions.Logging;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
-namespace FunderMaps.Core.Components
+namespace FunderMaps.Core.Components;
+
+/// <summary>
+///     Password hasher.
+/// </summary>
+public class PasswordHasher : IPasswordHasher
 {
+    private static readonly HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA256;
+    private const int iterRounds = 10000;
+    private const int subkeyLength = 256 / 8; // 256 bits
+    private const int saltSize = 128 / 8; // 128 bits
+    private const byte formatMarker = 0x01;
+
     /// <summary>
-    ///     Password hasher.
+    ///     Gets the <see cref="IRandom"/> used.
     /// </summary>
-    public class PasswordHasher : IPasswordHasher
+    public IRandom Random { get; }
+
+    /// <summary>
+    ///     Gets the <see cref="ILogger"/> used.
+    /// </summary>
+    public ILogger Logger { get; }
+
+    /// <summary>
+    ///     Create new instance.
+    /// </summary>
+    public PasswordHasher(IRandom random, ILogger<PasswordHasher> logger)
+        => (Random, Logger) = (random, logger);
+
+    // Compares two byte arrays for equality. The method is specifically written so that the loop is not optimized.
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    private static bool ByteArraysEqual(byte[] a, byte[] b)
     {
-        private static readonly HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA256;
-        private const int iterRounds = 10000;
-        private const int subkeyLength = 256 / 8; // 256 bits
-        private const int saltSize = 128 / 8; // 128 bits
-        private const byte formatMarker = 0x01;
-
-        /// <summary>
-        ///     Gets the <see cref="IRandom"/> used.
-        /// </summary>
-        public IRandom Random { get; }
-
-        /// <summary>
-        ///     Gets the <see cref="ILogger"/> used.
-        /// </summary>
-        public ILogger Logger { get; }
-
-        /// <summary>
-        ///     Create new instance.
-        /// </summary>
-        public PasswordHasher(IRandom random, ILogger<PasswordHasher> logger)
-            => (Random, Logger) = (random, logger);
-
-        // Compares two byte arrays for equality. The method is specifically written so that the loop is not optimized.
-        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-        private static bool ByteArraysEqual(byte[] a, byte[] b)
+        if (a is null && b is null)
         {
-            if (a is null && b is null)
-            {
-                return true;
-            }
-            if (a is null || b is null || a.Length != b.Length)
+            return true;
+        }
+        if (a is null || b is null || a.Length != b.Length)
+        {
+            return false;
+        }
+        var areSame = true;
+        for (var i = 0; i < a.Length; i++)
+        {
+            areSame &= (a[i] == b[i]);
+        }
+        return areSame;
+    }
+
+    private byte[] GeneratePasswordHash(string password)
+    {
+        byte[] salt = new byte[saltSize];
+        Random.WriteBytes(salt);
+        //_rng.GetBytes(salt);
+
+        using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterRounds, hashAlgorithm);
+        byte[] subkey = pbkdf2.GetBytes(subkeyLength);
+
+        byte[] outputBytes = new byte[1 + saltSize + subkeyLength];
+        outputBytes[0] = formatMarker;
+        Buffer.BlockCopy(salt, 0, outputBytes, 1, saltSize);
+        Buffer.BlockCopy(subkey, 0, outputBytes, 1 + saltSize, subkeyLength);
+        return outputBytes;
+    }
+
+    private static bool VerifyHashedPassword(byte[] inputBytes, string password)
+    {
+        byte[] salt = new byte[saltSize];
+        Buffer.BlockCopy(inputBytes, 1, salt, 0, saltSize);
+
+        byte[] expectedSubkey = new byte[subkeyLength];
+        Buffer.BlockCopy(inputBytes, 1 + saltSize, expectedSubkey, 0, subkeyLength);
+
+        using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterRounds, hashAlgorithm);
+        byte[] subkey = pbkdf2.GetBytes(subkeyLength);
+
+        return ByteArraysEqual(subkey, expectedSubkey);
+    }
+
+    /// <summary>
+    ///     Hash plaintext paassword and return the password hash.
+    /// </summary>
+    /// <param name="password">Plaintext password.</param>
+    /// <returns>Returns hashed password.</returns>
+    public string HashPassword(string password)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            throw new ArgumentNullException(nameof(password));
+        }
+
+        return Convert.ToBase64String(GeneratePasswordHash(password));
+    }
+
+    /// <summary>
+    ///     Check if password is valid.
+    /// </summary>
+    /// <remarks>
+    ///     If anything fails in the process this method will return as if
+    ///     the password validation failed. Exception details are logged.
+    /// </remarks>
+    /// <param name="hashedPassword">Password hash.</param>
+    /// <param name="providedPassword">Plaintext password to test.</param>
+    /// <returns>Returns <c>true</c> if passwords match, false otherwise.</returns>
+    public bool IsPasswordValid(string hashedPassword, string providedPassword)
+    {
+        if (string.IsNullOrEmpty(hashedPassword))
+        {
+            throw new ArgumentNullException(nameof(hashedPassword));
+        }
+
+        if (string.IsNullOrEmpty(providedPassword))
+        {
+            throw new ArgumentNullException(nameof(providedPassword));
+        }
+
+        try
+        {
+            byte[] decodedHashedPassword = Convert.FromBase64String(hashedPassword);
+            if (decodedHashedPassword[0] != formatMarker)
             {
                 return false;
             }
-            var areSame = true;
-            for (var i = 0; i < a.Length; i++)
-            {
-                areSame &= (a[i] == b[i]);
-            }
-            return areSame;
+
+            return VerifyHashedPassword(decodedHashedPassword, providedPassword);
         }
-
-        private byte[] GeneratePasswordHash(string password)
+        catch (SystemException exception)
         {
-            byte[] salt = new byte[saltSize];
-            Random.WriteBytes(salt);
-            //_rng.GetBytes(salt);
+            Logger.LogError(exception, "Error occurred during password validation");
 
-            using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterRounds, hashAlgorithm);
-            byte[] subkey = pbkdf2.GetBytes(subkeyLength);
-
-            byte[] outputBytes = new byte[1 + saltSize + subkeyLength];
-            outputBytes[0] = formatMarker;
-            Buffer.BlockCopy(salt, 0, outputBytes, 1, saltSize);
-            Buffer.BlockCopy(subkey, 0, outputBytes, 1 + saltSize, subkeyLength);
-            return outputBytes;
-        }
-
-        private static bool VerifyHashedPassword(byte[] inputBytes, string password)
-        {
-            byte[] salt = new byte[saltSize];
-            Buffer.BlockCopy(inputBytes, 1, salt, 0, saltSize);
-
-            byte[] expectedSubkey = new byte[subkeyLength];
-            Buffer.BlockCopy(inputBytes, 1 + saltSize, expectedSubkey, 0, subkeyLength);
-
-            using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterRounds, hashAlgorithm);
-            byte[] subkey = pbkdf2.GetBytes(subkeyLength);
-
-            return ByteArraysEqual(subkey, expectedSubkey);
-        }
-
-        /// <summary>
-        ///     Hash plaintext paassword and return the password hash.
-        /// </summary>
-        /// <param name="password">Plaintext password.</param>
-        /// <returns>Returns hashed password.</returns>
-        public string HashPassword(string password)
-        {
-            if (string.IsNullOrEmpty(password))
-            {
-                throw new ArgumentNullException(nameof(password));
-            }
-
-            return Convert.ToBase64String(GeneratePasswordHash(password));
-        }
-
-        /// <summary>
-        ///     Check if password is valid.
-        /// </summary>
-        /// <remarks>
-        ///     If anything fails in the process this method will return as if
-        ///     the password validation failed. Exception details are logged.
-        /// </remarks>
-        /// <param name="hashedPassword">Password hash.</param>
-        /// <param name="providedPassword">Plaintext password to test.</param>
-        /// <returns>Returns <c>true</c> if passwords match, false otherwise.</returns>
-        public bool IsPasswordValid(string hashedPassword, string providedPassword)
-        {
-            if (string.IsNullOrEmpty(hashedPassword))
-            {
-                throw new ArgumentNullException(nameof(hashedPassword));
-            }
-
-            if (string.IsNullOrEmpty(providedPassword))
-            {
-                throw new ArgumentNullException(nameof(providedPassword));
-            }
-
-            try
-            {
-                byte[] decodedHashedPassword = Convert.FromBase64String(hashedPassword);
-                if (decodedHashedPassword[0] != formatMarker)
-                {
-                    return false;
-                }
-
-                return VerifyHashedPassword(decodedHashedPassword, providedPassword);
-            }
-            catch (SystemException exception)
-            {
-                Logger.LogError(exception, "Error occurred during password validation");
-
-                return false;
-            }
+            return false;
         }
     }
 }
