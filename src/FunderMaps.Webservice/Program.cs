3583a9983b0bec657a28eb1dc06d5b61a2b06060
Yorick de Wid <yorickdewid@users.noreply.github.com>
2021-11-11 09:22:38 +0100

    Code improvements

--- src/FunderMaps.Webservice/Program.cs
+++ src/FunderMaps.Webservice/Program.cs
@@ -1,32 +1,31 @@
-namespace FunderMaps.Webservice
+namespace FunderMaps.Webservice;
+
+/// <summary>
+///     Application entry.
+/// </summary>
+public static class Program
 {
     /// <summary>
-    ///     Application entry.
+    ///     Application entry point.
     /// </summary>
-    public static class Program
-    {
-        /// <summary>
-        ///     Application entry point.
-        /// </summary>
-        /// <param name="args">Commandline arguments.</param>
-        public static Task Main(string[] args)
-            => CreateHostBuilder(args).Build().RunAsync();
+    /// <param name="args">Commandline arguments.</param>
+    public static Task Main(string[] args)
+        => CreateHostBuilder(args).Build().RunAsync();
 
-        /// <summary>
-        ///     Build a host and run the application.
-        /// </summary>
-        /// <remarks>
-        ///     The signature of this method should not be changed.
-        ///     External tooling expects this function be present.
-        /// </remarks>
-        /// <param name="args">Commandline arguments.</param>
-        /// <returns>See <see cref="IHostBuilder"/>.</returns>
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>()
-                        .UseSetting(WebHostDefaults.HostingStartupAssembliesKey, "FunderMaps.AspNetCore");
-                });
-    }
+    /// <summary>
+    ///     Build a host and run the application.
+    /// </summary>
+    /// <remarks>
+    ///     The signature of this method should not be changed.
+    ///     External tooling expects this function be present.
+    /// </remarks>
+    /// <param name="args">Commandline arguments.</param>
+    /// <returns>See <see cref="IHostBuilder"/>.</returns>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>()
+                    .UseSetting(WebHostDefaults.HostingStartupAssembliesKey, "FunderMaps.AspNetCore");
+            });
 }
--- src/FunderMaps.Webservice/Startup.cs
+++ src/FunderMaps.Webservice/Startup.cs
@@ -5,198 +5,197 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 
 [assembly: ApiController]
-namespace FunderMaps.Webservice
+namespace FunderMaps.Webservice;
+
+/// <summary>
+///     Application configuration.
+/// </summary>
+public class Startup
 {
     /// <summary>
-    ///     Application configuration.
+    ///     Configuration.
     /// </summary>
-    public class Startup
+    public IConfiguration Configuration { get; }
+
+    /// <summary>
+    ///     Create a new instance.
+    /// </summary>
+    /// <param name="configuration">See <see cref="IConfiguration"/>.</param>
+    public Startup(IConfiguration configuration) => Configuration = configuration;
+
+    /// <summary>
+    ///     Use this method to add services to the container regardless of the environment.
+    /// </summary>
+    /// <remarks>
+    ///     Order is undetermined when configuring services.
+    /// </remarks>
+    /// <param name="services">See <see cref="IServiceCollection"/>.</param>
+    private static void StartupConfigureServices(IServiceCollection services)
     {
-        /// <summary>
-        ///     Configuration.
-        /// </summary>
-        public IConfiguration Configuration { get; }
+        services.AddAutoMapper(typeof(Startup));
 
-        /// <summary>
-        ///     Create a new instance.
-        /// </summary>
-        /// <param name="configuration">See <see cref="IConfiguration"/>.</param>
-        public Startup(IConfiguration configuration) => Configuration = configuration;
-
-        /// <summary>
-        ///     Use this method to add services to the container regardless of the environment.
-        /// </summary>
-        /// <remarks>
-        ///     Order is undetermined when configuring services.
-        /// </remarks>
-        /// <param name="services">See <see cref="IServiceCollection"/>.</param>
-        private void StartupConfigureServices(IServiceCollection services)
+        services.AddSwaggerGen(options =>
         {
-            services.AddAutoMapper(typeof(Startup));
-
-            services.AddSwaggerGen(options =>
+            options.SwaggerDoc("v1", new()
             {
-                options.SwaggerDoc("v1", new()
+                Title = "FunderMaps Webservice",
+                Version = "v1",
+                Description = "FunderMaps Webservice REST API",
+                Contact = new()
                 {
-                    Title = "FunderMaps Webservice",
-                    Version = "v1",
-                    Description = "FunderMaps Webservice REST API",
-                    Contact = new()
-                    {
-                        Name = "Laixer B.V.",
-                        Email = "info@laixer.com",
-                    },
-                }
-                );
-                options.DocumentFilter<BasePathFilter>();
+                    Name = "Laixer B.V.",
+                    Email = "info@laixer.com",
+                },
+            }
+            );
+            options.DocumentFilter<BasePathFilter>();
 
                 // FUTURE: The full enum description support for swagger with System.Text.Json is a WIP. This is a custom tempfix.
                 options.SchemaFilter<EnumSchemaFilter>();
-                options.UseOneOfForPolymorphism();
+            options.UseOneOfForPolymorphism();
 
-                string DocumentationFile = $"{AppContext.BaseDirectory}DocumentationFunderMapsWebservice.xml";
-                if (File.Exists(DocumentationFile))
-                {
-                    options.IncludeXmlComments(DocumentationFile);
-                }
-            });
+            string DocumentationFile = $"{AppContext.BaseDirectory}DocumentationFunderMapsWebservice.xml";
+            if (File.Exists(DocumentationFile))
+            {
+                options.IncludeXmlComments(DocumentationFile);
+            }
+        });
 
-            // Register components from reference assemblies.
-            services.AddFunderMapsDataServices("FunderMapsConnection");
-        }
+        // Register components from reference assemblies.
+        services.AddFunderMapsDataServices("FunderMapsConnection");
+    }
 
-        /// <summary>
-        ///     This method gets called by the runtime if no environment is set.
-        /// </summary>
-        /// <param name="services">See <see cref="IServiceCollection"/>.</param>
-        public void ConfigureServices(IServiceCollection services)
+    /// <summary>
+    ///     This method gets called by the runtime if no environment is set.
+    /// </summary>
+    /// <param name="services">See <see cref="IServiceCollection"/>.</param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        StartupConfigureServices(services);
+    }
+
+    /// <summary>
+    ///     This method gets called by the runtime if environment is set to development.
+    /// </summary>
+    /// <param name="services">See <see cref="IServiceCollection"/>.</param>
+    public void ConfigureDevelopmentServices(IServiceCollection services)
+    {
+        StartupConfigureServices(services);
+
+        services.AddCors(options =>
         {
-            StartupConfigureServices(services);
-        }
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            });
+        });
+    }
 
-        /// <summary>
-        ///     This method gets called by the runtime if environment is set to development.
-        /// </summary>
-        /// <param name="services">See <see cref="IServiceCollection"/>.</param>
-        public void ConfigureDevelopmentServices(IServiceCollection services)
+    /// <summary>
+    ///     This method gets called by the runtime. Use this method to configure the HTTP
+    ///     request pipeline if environment is set to development.
+    /// </summary>
+    /// <remarks>
+    ///     The order in which the pipeline handles request is of importance.
+    /// </remarks>
+    public static void ConfigureDevelopment(IApplicationBuilder app)
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseCors();
+
+        app.UseExceptionHandler("/oops");
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
         {
-            StartupConfigureServices(services);
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "FunderMaps Webservice");
+            options.RoutePrefix = string.Empty;
+        });
 
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(policy =>
-                {
-                    policy.AllowAnyHeader();
-                    policy.AllowAnyMethod();
-                    policy.AllowAnyOrigin();
-                });
-            });
-        }
+        app.UsePathBase(new("/api"));
+        app.UseRouting();
 
-        /// <summary>
-        ///     This method gets called by the runtime. Use this method to configure the HTTP
-        ///     request pipeline if environment is set to development.
-        /// </summary>
-        /// <remarks>
-        ///     The order in which the pipeline handles request is of importance.
-        /// </remarks>
-        public static void ConfigureDevelopment(IApplicationBuilder app)
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseAspAppContext();
+
+        app.UseEndpoints(endpoints =>
         {
-            app.UseDeveloperExceptionPage();
-            app.UseCors();
+            endpoints.MapControllers();
+        });
+    }
 
-            app.UseExceptionHandler("/oops");
-
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "FunderMaps Webservice");
-                options.RoutePrefix = string.Empty;
-            });
-
-            app.UsePathBase(new("/api"));
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseAspAppContext();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-
-        /// <summary>
-        ///     This method gets called by the runtime. Use this method to configure the HTTP
-        ///     request pipeline if environment is set to staging.
-        /// </summary>
-        /// <remarks>
-        ///     The order in which the pipeline handles request is of importance.
-        /// </remarks>
-        public static void ConfigureStaging(IApplicationBuilder app)
+    /// <summary>
+    ///     This method gets called by the runtime. Use this method to configure the HTTP
+    ///     request pipeline if environment is set to staging.
+    /// </summary>
+    /// <remarks>
+    ///     The order in which the pipeline handles request is of importance.
+    /// </remarks>
+    public static void ConfigureStaging(IApplicationBuilder app)
+    {
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
         {
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
-            });
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+        });
 
-            app.UseExceptionHandler("/oops");
+        app.UseExceptionHandler("/oops");
 
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "FunderMaps Webservice");
-                options.RoutePrefix = string.Empty;
-            });
-
-            app.UsePathBase(new("/api"));
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseAspAppContext();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapHealthChecks("/health").WithMetadata(new AllowAnonymousAttribute());
-            });
-        }
-
-        /// <summary>
-        ///     This method gets called by the runtime. Use this method to configure the HTTP
-        ///     request pipeline if no environment is set.
-        /// </summary>
-        /// <remarks>
-        ///     The order in which the pipeline handles request is of importance.
-        /// </remarks>
-        public static void Configure(IApplicationBuilder app)
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
         {
-            app.UseForwardedHeaders(new()
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
-            });
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "FunderMaps Webservice");
+            options.RoutePrefix = string.Empty;
+        });
 
-            app.UseExceptionHandler("/oops");
+        app.UsePathBase(new("/api"));
+        app.UseRouting();
 
-            app.UseSwagger();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-            app.UsePathBase(new("/api"));
-            app.UseRouting();
+        app.UseAspAppContext();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHealthChecks("/health").WithMetadata(new AllowAnonymousAttribute());
+        });
+    }
 
-            app.UseAspAppContext();
+    /// <summary>
+    ///     This method gets called by the runtime. Use this method to configure the HTTP
+    ///     request pipeline if no environment is set.
+    /// </summary>
+    /// <remarks>
+    ///     The order in which the pipeline handles request is of importance.
+    /// </remarks>
+    public static void Configure(IApplicationBuilder app)
+    {
+        app.UseForwardedHeaders(new()
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+        });
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapHealthChecks("/health").WithMetadata(new AllowAnonymousAttribute());
-            });
-        }
+        app.UseExceptionHandler("/oops");
+
+        app.UseSwagger();
+
+        app.UsePathBase(new("/api"));
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseAspAppContext();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHealthChecks("/health").WithMetadata(new AllowAnonymousAttribute());
+        });
     }
 }
