5141f015ef3e931053f09fef1df98288ae4a54e8
Yorick de Wid <yorickdewid@users.noreply.github.com>
2021-11-27 14:10:58 +0100

    Remove namespaces

--- src/FunderMaps.Core/Types/Products/AnalysisProduct2.cs
+++ src/FunderMaps.Core/Types/Products/AnalysisProduct2.cs
@@ -1,98 +1,97 @@
 ﻿using FunderMaps.Core.DataAnnotations;
 
-namespace FunderMaps.Core.Types.Products
+namespace FunderMaps.Core.Types.Products;
+
+/// <summary>
+///     Represents a model for the complete endpoint.
+/// </summary>
+public sealed record AnalysisProduct2
 {
     /// <summary>
-    ///     Represents a model for the complete endpoint.
+    ///     Building identifier.
     /// </summary>
-    public sealed record AnalysisProduct2
-    {
-        /// <summary>
-        ///     Building identifier.
-        /// </summary>
-        [Geocoder]
-        public string BuildingId { get; init; }
+    [Geocoder]
+    public string BuildingId { get; init; }
 
-        /// <summary>
-        ///     Building external identifier.
-        /// </summary>
-        public string ExternalBuildingId { get; init; }
+    /// <summary>
+    ///     Building external identifier.
+    /// </summary>
+    public string ExternalBuildingId { get; init; }
 
-        /// <summary>
-        ///     Address identifier.
-        /// </summary>
-        [Geocoder]
-        public string AddressId { get; init; }
+    /// <summary>
+    ///     Address identifier.
+    /// </summary>
+    [Geocoder]
+    public string AddressId { get; init; }
 
-        /// <summary>
-        ///     Address external identifier.
-        /// </summary>
-        public string ExternalAddressId { get; init; }
+    /// <summary>
+    ///     Address external identifier.
+    /// </summary>
+    public string ExternalAddressId { get; init; }
 
-        /// <summary>
-        ///     Neighborhood identifier.
-        /// </summary>
-        [Geocoder]
-        public string NeighborhoodId { get; init; }
+    /// <summary>
+    ///     Neighborhood identifier.
+    /// </summary>
+    [Geocoder]
+    public string NeighborhoodId { get; init; }
 
-        /// <summary>
-        ///     Built year.
-        /// </summary>
-        public int? ConstructionYear { get; init; }
+    /// <summary>
+    ///     Built year.
+    /// </summary>
+    public int? ConstructionYear { get; init; }
 
-        /// <summary>
-        ///     Foundation recovery type.
-        /// </summary>
-        public RecoveryType? RecoveryType { get; init; }
+    /// <summary>
+    ///     Foundation recovery type.
+    /// </summary>
+    public RecoveryType? RecoveryType { get; init; }
 
-        /// <summary>
-        ///     Represents the estimated restoration costs for this building.
-        /// </summary>
-        public int? RestorationCosts { get; init; }
+    /// <summary>
+    ///     Represents the estimated restoration costs for this building.
+    /// </summary>
+    public int? RestorationCosts { get; init; }
 
-        /// <summary>
-        ///     Foundation type.
-        /// </summary>
-        public FoundationType? FoundationType { get; init; }
+    /// <summary>
+    ///     Foundation type.
+    /// </summary>
+    public FoundationType? FoundationType { get; init; }
 
-        /// <summary>
-        ///     Foundation type reliability.
-        /// </summary>
-        public Reliability FoundationTypeReliability { get; init; }
+    /// <summary>
+    ///     Foundation type reliability.
+    /// </summary>
+    public Reliability FoundationTypeReliability { get; init; }
 
-        /// <summary>
-        ///     Foundation type reliability.
-        /// </summary>
-        public Reliability DrystandReliability { get; init; }
+    /// <summary>
+    ///     Foundation type reliability.
+    /// </summary>
+    public Reliability DrystandReliability { get; init; }
 
-        /// <summary>
-        ///     Represents the foundation risk for this building.
-        /// </summary>
-        public FoundationRisk? DrystandRisk { get; init; }
+    /// <summary>
+    ///     Represents the foundation risk for this building.
+    /// </summary>
+    public FoundationRisk? DrystandRisk { get; init; }
 
-        /// <summary>
-        ///     Dewatering depth reliability.
-        /// </summary>
-        public Reliability DewateringDepthReliability { get; init; }
+    /// <summary>
+    ///     Dewatering depth reliability.
+    /// </summary>
+    public Reliability DewateringDepthReliability { get; init; }
 
-        /// <summary>
-        ///     Dewatering depth risk.
-        /// </summary>
-        public FoundationRisk? DewateringDepthRisk { get; init; }
+    /// <summary>
+    ///     Dewatering depth risk.
+    /// </summary>
+    public FoundationRisk? DewateringDepthRisk { get; init; }
 
-        /// <summary>
-        ///     Biological infection reliability.
-        /// </summary>
-        public Reliability BioInfectionReliability { get; init; }
+    /// <summary>
+    ///     Biological infection reliability.
+    /// </summary>
+    public Reliability BioInfectionReliability { get; init; }
 
-        /// <summary>
-        ///     Biological infection risk.
-        /// </summary>
-        public FoundationRisk? BioInfectionRisk { get; init; }
+    /// <summary>
+    ///     Biological infection risk.
+    /// </summary>
+    public FoundationRisk? BioInfectionRisk { get; init; }
 
-        /// <summary>
-        ///     Unclassified risk.
-        /// </summary>
-        public FoundationRisk? UnclassifiedRisk { get; init; }
-    }
+    /// <summary>
+    ///     Unclassified risk.
+    /// </summary>
+    public FoundationRisk? UnclassifiedRisk { get; init; }
 }
--- src/FunderMaps.Core/Types/Products/StatisticsProduct.cs
+++ src/FunderMaps.Core/Types/Products/StatisticsProduct.cs
@@ -1,56 +1,55 @@
 ﻿using FunderMaps.Core.Types.Distributions;
 
-namespace FunderMaps.Core.Types.Products
+namespace FunderMaps.Core.Types.Products;
+
+/// <summary>
+///     Model representing statistics about some region.
+/// </summary>
+public sealed record StatisticsProduct
 {
     /// <summary>
-    ///     Model representing statistics about some region.
+    ///     Represents the distribution of foundation types.
     /// </summary>
-    public sealed record StatisticsProduct
-    {
-        /// <summary>
-        ///     Represents the distribution of foundation types.
-        /// </summary>
-        public FoundationTypeDistribution FoundationTypeDistribution { get; set; }
+    public FoundationTypeDistribution FoundationTypeDistribution { get; set; }
 
-        /// <summary>
-        ///     Represents the distribution of building construction years in the
-        ///     given region.
-        /// </summary>
-        public ConstructionYearDistribution ConstructionYearDistribution { get; set; }
+    /// <summary>
+    ///     Represents the distribution of building construction years in the
+    ///     given region.
+    /// </summary>
+    public ConstructionYearDistribution ConstructionYearDistribution { get; set; }
 
-        /// <summary>
-        ///     Represents the distribution of foundation risks in the given region.
-        /// </summary>
-        public FoundationRiskDistribution FoundationRiskDistribution { get; set; }
+    /// <summary>
+    ///     Represents the distribution of foundation risks in the given region.
+    /// </summary>
+    public FoundationRiskDistribution FoundationRiskDistribution { get; set; }
 
-        /// <summary>
-        ///     Represents the percentage of collected data in the given region.
-        /// </summary>
-        public decimal DataCollectedPercentage { get; set; }
+    /// <summary>
+    ///     Represents the percentage of collected data in the given region.
+    /// </summary>
+    public decimal DataCollectedPercentage { get; set; }
 
-        /// <summary>
-        ///     Total amount of restored buildings in the given area.
-        /// </summary>
-        public long TotalBuildingRestoredCount { get; set; }
+    /// <summary>
+    ///     Total amount of restored buildings in the given area.
+    /// </summary>
+    public long TotalBuildingRestoredCount { get; set; }
 
-        /// <summary>
-        ///     Total amount of incidents in the given region.
-        /// </summary>
-        public IEnumerable<IncidentYearPair> TotalIncidentCount { get; set; }
+    /// <summary>
+    ///     Total amount of incidents in the given region.
+    /// </summary>
+    public IEnumerable<IncidentYearPair> TotalIncidentCount { get; set; }
 
-        /// <summary>
-        ///     Total amount of incidents in the given region.
-        /// </summary>
-        public IEnumerable<IncidentYearPair> MunicipalityIncidentCount { get; set; }
+    /// <summary>
+    ///     Total amount of incidents in the given region.
+    /// </summary>
+    public IEnumerable<IncidentYearPair> MunicipalityIncidentCount { get; set; }
 
-        /// <summary>
-        ///     Total amount of reports in the given region.
-        /// </summary>
-        public IEnumerable<InquiryYearPair> TotalReportCount { get; set; }
+    /// <summary>
+    ///     Total amount of reports in the given region.
+    /// </summary>
+    public IEnumerable<InquiryYearPair> TotalReportCount { get; set; }
 
-        /// <summary>
-        ///     Total amount of reports in the given region.
-        /// </summary>
-        public IEnumerable<InquiryYearPair> MunicipalityReportCount { get; set; }
-    }
+    /// <summary>
+    ///     Total amount of reports in the given region.
+    /// </summary>
+    public IEnumerable<InquiryYearPair> MunicipalityReportCount { get; set; }
 }
