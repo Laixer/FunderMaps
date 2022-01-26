6f4afbcd922200663959827a90e829c44f0211a8
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-01-26 15:02:03 +0100

    Set the default to JWT tokens

--- src/FunderMaps.AspNetCore/FunderMapsStartup.cs
+++ src/FunderMaps.AspNetCore/FunderMapsStartup.cs
@@ -114,7 +114,7 @@ public class FunderMapsStartup : IHostingStartup
                 // Add the authentication layer.
                 services.AddAuthentication(options =>
                 {
-                    options.DefaultAuthenticateScheme = Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme;
                     options.DefaultSignInScheme = Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme;
                     options.DefaultChallengeScheme = Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme;
                 })
