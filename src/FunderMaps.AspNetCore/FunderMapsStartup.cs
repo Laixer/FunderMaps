0b3219c359ac3f09d9ca2d854068cf0e5743556e
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-01-26 14:54:27 +0100

    Cookie authentication

--- src/FunderMaps.AspNetCore/FunderMapsStartup.cs
+++ src/FunderMaps.AspNetCore/FunderMapsStartup.cs
@@ -112,7 +112,17 @@ public class FunderMapsStartup : IHostingStartup
 
             {
                 // Add the authentication layer.
-                services.AddAuthentication(Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/account/login";
+                    options.LogoutPath = "/account/logout";
+                })
                 .AddJwtBearer(options =>
                 {
                     options.SaveToken = false;
