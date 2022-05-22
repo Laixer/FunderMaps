26597cb8a8d50364bb71608f3b9b146d8fe3f06e
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-05-22 16:02:30 +0200

    Remove data queries

--- src/FunderMaps.Core/Interfaces/Repositories/IAnalysisRepository.cs
+++ src/FunderMaps.Core/Interfaces/Repositories/IAnalysisRepository.cs
@@ -11,43 +11,7 @@ public interface IAnalysisRepository
     ///     Gets an analysis product by its internal building id.
     /// </summary>
     /// <param name="id">Internal building id.</param>
-    Task<AnalysisProduct> GetByIdAsync(string id);
-
-    /// <summary>
-    ///     Gets an analysis product by its internal building id.
-    /// </summary>
-    /// <param name="id">Internal building id.</param>
-    Task<AnalysisProduct2> GetById2Async(string id);
-
-    /// <summary>
-    ///     Gets an analysis product by its internal building id.
-    /// </summary>
-    /// <param name="id">Internal building id.</param>
     Task<AnalysisProduct3> Get3Async(string id);
-
-    /// <summary>
-    ///     Gets an analysis product by its external building id.
-    /// </summary>
-    /// <param name="id">External building id.</param>
-    Task<AnalysisProduct> GetByExternalIdAsync(string id);
-
-    /// <summary>
-    ///     Gets an analysis product by its external building id.
-    /// </summary>
-    /// <param name="id">External building id.</param>
-    Task<AnalysisProduct2> GetByExternalId2Async(string id);
-
-    /// <summary>
-    ///     Gets an analysis product by its external address id.
-    /// </summary>
-    /// <param name="id">External address id.</param>
-    Task<AnalysisProduct> GetByAddressExternalIdAsync(string id);
-
-    /// <summary>
-    ///     Gets an analysis product by its external address id.
-    /// </summary>
-    /// <param name="id">External address id.</param>
-    Task<AnalysisProduct2> GetByAddressExternalId2Async(string id);
 
     /// <summary>
     ///     Gets the risk index by its internal building id.
