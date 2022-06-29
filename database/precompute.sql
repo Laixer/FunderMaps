5df989d6943cd79a34879ed656f6cf3cde097b2c
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-06-29 10:29:27 +0200

    Refresh stats views

--- database/precompute.sql
+++ database/precompute.sql
@@ -21,13 +21,6 @@ REFRESH MATERIALIZED VIEW "data".building_type WITH DATA;
 
 
 --
--- Data for Name: analysis_foundation_indicative; Type: TABLE DATA; Schema: data; Owner: fundermaps
---
-
-REFRESH MATERIALIZED VIEW "data".analysis_foundation_indicative WITH DATA;
-
-
---
 -- Data for Name: analysis_foundation_risk; Type: TABLE DATA; Schema: data; Owner: fundermaps
 --
 
@@ -39,3 +32,52 @@ REFRESH MATERIALIZED VIEW "data".analysis_foundation_risk WITH DATA;
 --
 
 REFRESH MATERIALIZED VIEW "data".analysis_complete WITH DATA;
+
+
+--
+-- Data for Name: statistics_product_buildings_restored; Type: TABLE DATA; Schema: data; Owner: fundermaps
+--
+
+REFRESH MATERIALIZED VIEW "data".statistics_product_buildings_restored WITH DATA;
+
+
+--
+-- Data for Name: statistics_product_construction_years; Type: TABLE DATA; Schema: data; Owner: fundermaps
+--
+
+REFRESH MATERIALIZED VIEW "data".statistics_product_construction_years WITH DATA;
+
+
+--
+-- Data for Name: statistics_product_data_collected; Type: TABLE DATA; Schema: data; Owner: fundermaps
+--
+
+REFRESH MATERIALIZED VIEW "data".statistics_product_data_collected WITH DATA;
+
+
+--
+-- Data for Name: statistics_product_foundation_risk; Type: TABLE DATA; Schema: data; Owner: fundermaps
+--
+
+REFRESH MATERIALIZED VIEW "data".statistics_product_foundation_risk WITH DATA;
+
+
+--
+-- Data for Name: statistics_product_foundation_type; Type: TABLE DATA; Schema: data; Owner: fundermaps
+--
+
+REFRESH MATERIALIZED VIEW "data".statistics_product_foundation_type WITH DATA;
+
+
+--
+-- Data for Name: statistics_product_incidents; Type: TABLE DATA; Schema: data; Owner: fundermaps
+--
+
+REFRESH MATERIALIZED VIEW "data".statistics_product_incidents WITH DATA;
+
+
+--
+-- Data for Name: statistics_product_inquiries; Type: TABLE DATA; Schema: data; Owner: fundermaps
+--
+
+REFRESH MATERIALIZED VIEW "data".statistics_product_inquiries WITH DATA;
