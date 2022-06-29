24e6e0b9d4b912701c62bbccb0398b29cfaecd0c
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-06-29 16:20:15 +0200

    Batchnode commandline

--- src/FunderMaps.BatchNode/Program.cs
+++ src/FunderMaps.BatchNode/Program.cs
@@ -1,3 +1,5 @@
+using System.CommandLine;
+
 namespace FunderMaps.BatchNode;
 
 /// <summary>
@@ -18,9 +20,65 @@ public static class Program
     /// <summary>
     ///     Application entry point.
     /// </summary>
-    /// <param name="args">Commandline arguments.</param>
-    public static Task Main(string[] args)
-        => CreateHostBuilder(args).Build().RunAsync();
+    static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        var serviceProvider = host.Services;
+
+        RootCommand rootCommand = new();
+
+        Command verbCommand = new("map-bundle");
+        verbCommand.SetHandler(async () =>
+        {
+            Console.WriteLine($"Running map-bundle job");
+
+            var exportJob = serviceProvider.GetRequiredService<Core.MapBundle.Jobs.ExportJob>();
+            await exportJob.ExecuteCommandAsync(new Core.Threading.Command.CommandTaskContext(Guid.NewGuid())
+            {
+                Workspace = System.Environment.CurrentDirectory + "/workspace",
+                KeepWorkspace = true,
+                Failed = false,
+            });
+        });
+        rootCommand.Add(verbCommand);
+
+        // TODO: Should be updated. This job should export the entire analysis_complete.
+        Command exportCommand = new("export");
+        exportCommand.SetHandler(async () =>
+        {
+            Console.WriteLine($"Running export job");
+
+            var exportJob = serviceProvider.GetRequiredService<Core.MapBundle.Jobs.ExportGpkg>();
+            await exportJob.ExecuteCommandAsync(new Core.Threading.Command.CommandTaskContext(Guid.NewGuid())
+            {
+                Workspace = System.Environment.CurrentDirectory + "/workspace",
+                KeepWorkspace = true,
+                Failed = false,
+            });
+        });
+        rootCommand.Add(exportCommand);
+
+        Command refreshCommand = new("refresh");
+        refreshCommand.SetHandler(async () =>
+        {
+            Console.WriteLine($"Running refresh job");
+
+            var scope = serviceProvider.CreateAsyncScope();
+
+            var exportJob = scope.ServiceProvider.GetRequiredService<Core.Model.Jobs.RefreshJob>();
+            await exportJob.ExecuteCommandAsync(new Core.Threading.Command.CommandTaskContext(Guid.NewGuid())
+            {
+                Workspace = System.Environment.CurrentDirectory + "/workspace",
+                KeepWorkspace = true,
+                Failed = false,
+            });
+        });
+        rootCommand.Add(refreshCommand);
+
+        rootCommand.Description = "FunderMaps batch job runner";
+
+        await rootCommand.InvokeAsync(args);
+    }
 
     /// <summary>
     ///     Build a host and run the application.
@@ -35,20 +93,16 @@ public static class Program
         Host.CreateDefaultBuilder(args)
             .ConfigureServices(services =>
             {
-                    // Configure FunderMaps services.
-                    services.AddFunderMapsCoreServices();
+                // Configure FunderMaps services.
+                services.AddFunderMapsCoreServices();
                 services.AddFunderMapsInfrastructureServices();
                 services.AddFunderMapsDataServices("FunderMapsConnection");
 
-                    // The startup essential properties can be used to setup components.
-                    (Configuration, HostEnvironment) = services.BuildStartupProperties();
+                // The startup essential properties can be used to setup components.
+                (Configuration, HostEnvironment) = services.BuildStartupProperties();
 
-                    // Add the task scheduler.
-                    services.Configure<MapBundleOptions>(Configuration.GetSection(MapBundleOptions.Section));
+                // Add the task scheduler.
+                services.Configure<MapBundleOptions>(Configuration.GetSection(MapBundleOptions.Section));
                 services.AddHostedService<TimedMapBundleService>();
-
-                    // Add the task scheduler.
-                    // services.Configure<ModelOptions>(Configuration.GetSection(ModelOptions.Section));
-                    // services.AddHostedService<TimedModelService>();
-                });
+            });
 }
