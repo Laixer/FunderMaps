e99d44481999103aa2495ceafea6e4552ed2dbfc
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-06-29 16:19:39 +0200

    Refresh models

--- src/FunderMaps.Core/Model/Jobs/RefreshJob.cs
+++ src/FunderMaps.Core/Model/Jobs/RefreshJob.cs
@@ -1,20 +1,30 @@
 using FunderMaps.Core.Threading.Command;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace FunderMaps.Core.Model.Jobs;
 
 /// <summary>
 ///     Refresh job entry.
 /// </summary>
-internal class RefreshJob : CommandTask
+public class RefreshJob : CommandTask
 {
     private const string TaskName = "MODEL_REFRESH";
 
-    private static readonly string[] models = new string[]
+    private static readonly string[] concurrent_models = new string[]
     {
-            "data.analysis_foundation_risk",
-            "data.analysis_foundation_indicative",
-            "data.analysis_complete",
+        "data.analysis_complete",
+        "data.statistics_product_buildings_restored",
+        "data.statistics_product_data_collected",
+    };
+
+    private static readonly string[] sequential_models = new string[]
+    {
+        "data.statistics_product_construction_years",
+        "data.statistics_product_foundation_risk",
+        "data.statistics_product_foundation_type",
+        "data.statistics_product_incidents",
+        "data.statistics_product_inquiries",
     };
 
     private readonly string connectionString;
@@ -22,9 +32,11 @@ internal class RefreshJob : CommandTask
     /// <summary>
     ///     Create new instance.
     /// </summary>
-    public RefreshJob(IConfiguration configuration)
+    public RefreshJob(IConfiguration configuration, ILogger<RefreshJob> logger)
     {
-        connectionString = configuration.GetConnectionString("FunderMapsConnection");
+        // TODO: HACK
+        Logger = logger;
+        connectionString = configuration.GetConnectionString("FunderMapsAdminConnection");
     }
 
     /// <summary>
@@ -33,14 +45,32 @@ internal class RefreshJob : CommandTask
     /// <param name="context">Command task execution context.</param>
     public override async Task ExecuteCommandAsync(CommandTaskContext context)
     {
-        foreach (var model in models)
+        // TODO: HACK
+        Context = context;
+
+        // TODO: HACK.
+        CreateDirectory("out");
+
+        foreach (var model in concurrent_models)
         {
             CommandInfo command = new("psql");
 
             new PostgreSQLSink(connectionString).ParseCommandString(command);
 
             command.ArgumentList.Add("-c");
-            command.ArgumentList.Add($"refresh materialized view {model} with data");
+            command.ArgumentList.Add($"REFRESH MATERIALIZED VIEW CONCURRENTLY {model} WITH DATA");
+
+            await RunCommandAsync(command);
+        }
+
+        foreach (var model in sequential_models)
+        {
+            CommandInfo command = new("psql");
+
+            new PostgreSQLSink(connectionString).ParseCommandString(command);
+
+            command.ArgumentList.Add("-c");
+            command.ArgumentList.Add($"REFRESH MATERIALIZED VIEW {model} WITH DATA");
 
             await RunCommandAsync(command);
         }
