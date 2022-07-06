578462a92cab8cd342e6667986f3c654a7064359
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-07-06 17:16:27 +0200

    Update tests for the new dataset

--- tests/FunderMaps.IntegrationTests/Backend/Geocoder/AddressTests.cs
+++ tests/FunderMaps.IntegrationTests/Backend/Geocoder/AddressTests.cs
@@ -3,73 +3,73 @@ using FunderMaps.Core.Types;
 using System.Net;
 using Xunit;
 
-namespace FunderMaps.IntegrationTests.Backend.Geocoder
+namespace FunderMaps.IntegrationTests.Backend.Geocoder;
+
+// TODO: Include test for inactive address
+public class AddressTests : IClassFixture<BackendFixtureFactory>
 {
-    public class AddressTests : IClassFixture<BackendFixtureFactory>
+    private BackendFixtureFactory Factory { get; }
+
+    /// <summary>
+    ///     Create new instance.
+    /// </summary>
+    public AddressTests(BackendFixtureFactory factory)
+        => Factory = factory;
+
+    [Fact]
+    public async Task GetAddressByIdReturnSingleAddress()
     {
-        private BackendFixtureFactory Factory { get; }
+        // Arrange
+        using var client = Factory.CreateClient();
 
-        /// <summary>
-        ///     Create new instance.
-        /// </summary>
-        public AddressTests(BackendFixtureFactory factory)
-            => Factory = factory;
+        // Act
+        var response = await client.GetAsync($"api/address/gfm-059f268aad9d43339afe56a32cf641cc");
+        var returnObject = await response.Content.ReadFromJsonAsync<AddressDto>();
 
-        [Fact]
-        public async Task GetAddressByIdReturnSingleAddress()
-        {
-            // Arrange
-            using var client = Factory.CreateClient();
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal("gfm-059f268aad9d43339afe56a32cf641cc", returnObject.Id);
+        Assert.Equal("gfm-629e12e409bb4d0893640ae9ef3a53b7", returnObject.BuildingId);
+        Assert.Equal("319b", returnObject.BuildingNumber);
+        Assert.Equal("3023DG", returnObject.PostalCode);
+        Assert.Equal("Rochussenstraat", returnObject.Street);
+        Assert.Equal("Rotterdam", returnObject.City);
+        Assert.True(response.Headers.CacheControl.Public);
+    }
 
-            // Act
-            var response = await client.GetAsync($"api/address/gfm-35952a0487304368b0069b4485a69b4b");
-            var returnObject = await response.Content.ReadFromJsonAsync<AddressDto>();
+    [Theory]
+    [InlineData("gfm-08e199f14ecc41d8927476e0d28c6ed4", "gfm-08e199f14ecc41d8927476e0d28c6ed4")]
+    [InlineData("NL.IMBAG.NUMMERAANDUIDING.0599200000187924", "gfm-2a3472fa13b94cd189d5917bdf42491f")]
+    [InlineData("0599200100024846", "gfm-ba0deefb78844d45b5f9e55f89cc384c")]
+    public async Task GetAddressByGeoIdReturnSingleAddress(string address, string expected)
+    {
+        // Arrange
+        using var client = Factory.CreateClient();
 
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal("gfm-35952a0487304368b0069b4485a69b4b", returnObject.Id);
-            Assert.Equal("gfm-0b50e18111754ed3b5c1408f9f64bf22", returnObject.BuildingId);
-            Assert.Equal("4d", returnObject.BuildingNumber);
-            Assert.Equal("2612PA", returnObject.PostalCode);
-            Assert.Equal("Poortweg", returnObject.Street);
-            Assert.Equal("Delft", returnObject.City);
-            Assert.True(response.Headers.CacheControl.Public);
-        }
+        // Act
+        var response = await client.GetAsync($"api/address/{address}");
+        var returnObject = await response.Content.ReadFromJsonAsync<AddressDto>();
 
-        [Theory]
-        [InlineData("gfm-6d70df27db5347f88d932faa3a72d3b3", "gfm-6d70df27db5347f88d932faa3a72d3b3")]
-        [InlineData("NL.IMBAG.NUMMERAANDUIDING.0503200000018943", "gfm-09e3b90972de425ea140ae27e49d60b5")]
-        [InlineData("0503200000019289", "gfm-9ecb0f685cb84355ae464e2a358ac158")]
-        public async Task GetAddressByGeoIdReturnSingleAddress(string address, string expected)
-        {
-            // Arrange
-            using var client = Factory.CreateClient();
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(expected, returnObject.Id);
+        Assert.True(response.Headers.CacheControl.Public);
+    }
 
-            // Act
-            var response = await client.GetAsync($"api/address/{address}");
-            var returnObject = await response.Content.ReadFromJsonAsync<AddressDto>();
+    [Theory]
+    [InlineData(OrganizationRole.Superuser)]
+    [InlineData(OrganizationRole.Verifier)]
+    [InlineData(OrganizationRole.Writer)]
+    public async Task GetAddressByIdReturnOk(OrganizationRole role)
+    {
+        // Arrange
+        using var client = Factory.CreateClient(role);
 
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal(expected, returnObject.Id);
-            Assert.True(response.Headers.CacheControl.Public);
-        }
+        // Act
+        var response = await client.GetAsync($"api/address/gfm-b2a766e060444d3487c30d8de51fa408");
 
-        [Theory]
-        [InlineData(OrganizationRole.Superuser)]
-        [InlineData(OrganizationRole.Verifier)]
-        [InlineData(OrganizationRole.Writer)]
-        public async Task GetAddressByIdReturnOk(OrganizationRole role)
-        {
-            // Arrange
-            using var client = Factory.CreateClient(role);
-
-            // Act
-            var response = await client.GetAsync($"api/address/gfm-6d70df27db5347f88d932faa3a72d3b3");
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.True(response.Headers.CacheControl.Public);
-        }
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.True(response.Headers.CacheControl.Public);
     }
 }
