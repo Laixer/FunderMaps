e343bbe4806148d2bb11971ab16cffd020d7f443
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-01-19 12:18:40 +0100

    Code cleanup

--- tests/FunderMaps.IntegrationTests/Webservice/StatisticsTests.cs
+++ tests/FunderMaps.IntegrationTests/Webservice/StatisticsTests.cs
@@ -1,67 +1,66 @@
-﻿using FunderMaps.AspNetCore.DataTransferObjects;
+﻿using FunderMaps.Core.Types.Products;
 using System.Net;
 using Xunit;
 
-namespace FunderMaps.IntegrationTests.Webservice
+namespace FunderMaps.IntegrationTests.Webservice;
+
+/// <summary>
+///     Integration test for the statistics controller.
+/// </summary>
+public class StatisticsTests : IClassFixture<WebserviceFixtureFactory>
 {
+    private WebserviceFixtureFactory Factory { get; }
+
     /// <summary>
-    ///     Integration test for the statistics controller.
+    ///     Create new instance and setup the test data.
     /// </summary>
-    public class StatisticsTests : IClassFixture<WebserviceFixtureFactory>
+    public StatisticsTests(WebserviceFixtureFactory factory)
+        => Factory = factory;
+
+    [Fact(Skip = "Needs FIX")]
+    public async Task GetProductByIdReturnProduct()
     {
-        private WebserviceFixtureFactory Factory { get; }
+        // Arrange
+        using var client = Factory.CreateClient();
 
-        /// <summary>
-        ///     Create new instance and setup the test data.
-        /// </summary>
-        public StatisticsTests(WebserviceFixtureFactory factory)
-            => Factory = factory;
+        // Act.
+        var response = await client.GetAsync($"api/v3/product/statistics/gfm-6aae47cb5aa4416abdf19d98ba8218ac");
+        var returnObject = await response.Content.ReadFromJsonAsync<StatisticsProduct>();
 
-        [Fact(Skip = "Needs FIX")]
-        public async Task GetProductByIdReturnProduct()
-        {
-            // Arrange
-            using var client = Factory.CreateClient();
+        // Assert.
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        // Assert.True(returnObject.ItemCount >= 1);
+    }
 
-            // Act.
-            var response = await client.GetAsync($"api/v3/product/statistics?id=gfm-6aae47cb5aa4416abdf19d98ba8218ac");
-            var returnObject = await response.Content.ReadFromJsonAsync<ResponseWrapper<StatisticsDto>>();
+    [Fact(Skip = "Needs FIX")]
+    public async Task GetProductByExternalIdReturnProduct()
+    {
+        // Arrange
+        using var client = Factory.CreateClient();
 
-            // Assert.
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.True(returnObject.ItemCount >= 1);
-        }
+        // Act.
+        var response = await client.GetAsync($"api/v3/product/statistics/BU05031403");
+        var returnObject = await response.Content.ReadFromJsonAsync<StatisticsProduct>();
 
-        [Fact(Skip = "Needs FIX")]
-        public async Task GetProductByExternalIdReturnProduct()
-        {
-            // Arrange
-            using var client = Factory.CreateClient();
+        // Assert.
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        // Assert.True(returnObject.ItemCount >= 1);
+    }
 
-            // Act.
-            var response = await client.GetAsync($"api/v3/product/statistics?id=BU05031403");
-            var returnObject = await response.Content.ReadFromJsonAsync<ResponseWrapper<StatisticsDto>>();
+    [Theory(Skip = "Needs FIX")]
+    [InlineData("id=3kjhr834dhfjdeh")]
+    [InlineData("bagid=4928374hfdkjsfh")]
+    [InlineData("query=thisismyquerystringyes")]
+    [InlineData("fdshjbf438gi")]
+    public async Task GetByIdInvalidAddressThrows(string address)
+    {
+        // Arrange
+        using var client = Factory.CreateClient();
 
-            // Assert.
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.True(returnObject.ItemCount >= 1);
-        }
+        // Act.
+        var response = await client.GetAsync($"api/v3/product/statistics/{address}");
 
-        [Theory(Skip = "Needs FIX")]
-        [InlineData("id=3kjhr834dhfjdeh")]
-        [InlineData("bagid=4928374hfdkjsfh")]
-        [InlineData("query=thisismyquerystringyes")]
-        [InlineData("fdshjbf438gi")]
-        public async Task GetByIdInvalidAddressThrows(string address)
-        {
-            // Arrange
-            using var client = Factory.CreateClient();
-
-            // Act.
-            var response = await client.GetAsync($"api/v3/product/statistics?id={address}");
-
-            // Assert.
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-        }
+        // Assert.
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 }
--- tests/FunderMaps.IntegrationTests/Webservice/VersionsTests.cs
+++ tests/FunderMaps.IntegrationTests/Webservice/VersionsTests.cs
@@ -1,35 +1,34 @@
 using System.Net;
 using Xunit;
 
-namespace FunderMaps.IntegrationTests.Webservice
+namespace FunderMaps.IntegrationTests.Webservice;
+
+public class VersionsTests : IClassFixture<WebserviceFixtureFactory>
 {
-    public class VersionsTests : IClassFixture<WebserviceFixtureFactory>
+    private WebserviceFixtureFactory Factory { get; }
+
+    /// <summary>
+    ///     Create new instance.
+    /// </summary>
+    public VersionsTests(WebserviceFixtureFactory factory)
+        => Factory = factory;
+
+    [Fact]
+    public async Task GetVersionUnauthorizedReturnSuccessAndCorrectContentType()
     {
-        private WebserviceFixtureFactory Factory { get; }
+        await TestStub.VersionAsync(Factory);
+    }
 
-        /// <summary>
-        ///     Create new instance.
-        /// </summary>
-        public VersionsTests(WebserviceFixtureFactory factory)
-            => Factory = factory;
+    [Fact]
+    public async Task GetVersionAuthorizedReturnSuccessAndCorrectContentType()
+    {
+        // Arrange
+        using var client = Factory.CreateClient();
 
-        [Fact]
-        public async Task GetVersionUnauthorizedReturnSuccessAndCorrectContentType()
-        {
-            await TestStub.VersionAsync(Factory);
-        }
+        // Act
+        var response = await client.GetAsync("api/version");
 
-        [Fact]
-        public async Task GetVersionAuthorizedReturnSuccessAndCorrectContentType()
-        {
-            // Arrange
-            using var client = Factory.CreateClient();
-
-            // Act
-            var response = await client.GetAsync("api/version");
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        }
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
