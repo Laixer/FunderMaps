9fb9c73e7f00426bc194e4633a8a67d1a1450b54
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-02-27 16:16:10 +0100

    Redirect with vendor parameter

--- src/FunderMaps.Portal/Controllers/IndexController.cs
+++ src/FunderMaps.Portal/Controllers/IndexController.cs
@@ -6,6 +6,23 @@ public class IndexController : Controller
 {
     public ActionResult Index()
     {
+        if (Request.Host.Host.ToLower().Contains("schiedam"))
+        {
+            return RedirectPermanent("https://incident.fundermaps.com/?vendor=schiedam");
+        }
+        else if (Request.Host.Host.ToLower().Contains("lansingerland"))
+        {
+            return RedirectPermanent("https://incident.fundermaps.com/?vendor=lansingerland");
+        }
+        else if (Request.Host.Host.ToLower().Contains("regiodeal"))
+        {
+            return RedirectPermanent("https://incident.fundermaps.com/?vendor=regiodeal");
+        }
+        else if (Request.Host.Host.ToLower().Contains("veenweidefryslan"))
+        {
+            return RedirectPermanent("https://incident.fundermaps.com/?vendor=veenweidefryslan");
+        }
+
         return RedirectPermanent("https://incident.fundermaps.com/");
     }
 }
