69f79b7e5b8c0ad66a73c826706b869a466cdcf5
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-06-23 17:16:07 +0200

    Support legacy versions of the API calls

--- src/FunderMaps.Webservice/Controllers/ProductController.cs
+++ src/FunderMaps.Webservice/Controllers/ProductController.cs
@@ -1,4 +1,4 @@
-﻿using FunderMaps.Core.Interfaces.Repositories;
+using FunderMaps.Core.Interfaces.Repositories;
 using FunderMaps.Core.Types.Products;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -39,6 +39,20 @@ public class ProductController : ControllerBase
             MunicipalityReportCount = await _statisticsRepository.GetMunicipalityReportCountByIdAsync(id),
         };
 
+    // TODO: LEGACY
+    // GET: api/v3/product/analysis
+    [HttpGet("analysis")]
+    public async Task<IActionResult> GetAnalysisLegacyAsync([FromQuery] string id)
+    {
+        var product = await _analysisRepository.GetAsync(id);
+        if (product is not null)
+        {
+            return Ok(product);
+        }
+
+        return NotFound();
+    }
+
     // GET: api/v3/product/analysis
     /// <summary>
     ///     Request the analysis product.
@@ -56,20 +70,36 @@ public class ProductController : ControllerBase
         return NotFound();
     }
 
+    // TODO: LEGACY
     // GET: api/v3/product/at_risk
     /// <summary>
     ///     Request the risk index per id.
     /// </summary>
-    [Authorize(Roles = "webservice-atrisk")]
+    [HttpGet("at_risk")]
+    public Task<bool> GetRiskIndexLegacyAsync([FromQuery] string id)
+        => _analysisRepository.GetRiskIndexAsync(id);
+
+    // GET: api/v3/product/at_risk
+    /// <summary>
+    ///     Request the risk index per id.
+    /// </summary>
     [HttpGet("at_risk/{id}")]
     public Task<bool> GetRiskIndexAsync(string id)
         => _analysisRepository.GetRiskIndexAsync(id);
+
+    // TODO: LEGACY
+    // GET: api/v3/product/statistics
+    /// <summary>
+    ///     Request the statistics product.
+    /// </summary>
+    [HttpGet("statistics")]
+    public Task<StatisticsProduct> GetStatisticsLegacyAsync([FromQuery] string id)
+        => GetStatisticsByIdAsync(id);
 
     // GET: api/v3/product/statistics
     /// <summary>
     ///     Request the statistics product.
     /// </summary>
-    [Authorize(Roles = "webservice-statistics")]
     [HttpGet("statistics/{id}")]
     public Task<StatisticsProduct> GetStatisticsAsync(string id)
         => GetStatisticsByIdAsync(id);
