29a1e1a61555ef31dce4cffe2dd271cc9c598274
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-07-04 10:25:00 +0200

    Use the new geocoder

--- src/FunderMaps.Incident/Pages/Address.razor.cs
+++ src/FunderMaps.Incident/Pages/Address.razor.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components;
 using FunderMaps.Incident.Data;
 using FunderMaps.Incident.Shared;
 using FunderMaps.Core.Interfaces;
+using FunderMaps.Core.Services;
 
 namespace FunderMaps.Incident.Pages;
 
@@ -11,7 +12,6 @@ public partial class Address : ComponentBase, IAsyncDisposable
     private ElementReference mapElement;
     private IJSObjectReference? module;
     private IJSObjectReference? mapInstance;
-    private HttpClient http = new();
 
     [Inject]
     private IJSRuntime IJRuntime { get; set; } = default!;
@@ -22,97 +22,51 @@ public partial class Address : ComponentBase, IAsyncDisposable
     [Inject]
     private IGeocoderTranslation GeocoderTranslation { get; set; } = default!;
 
+    [Inject]
+    private PDOKLocationService LocationService { get; set; } = default!;
+
     [CascadingParameter]
     State State { get; set; } = default!;
 
     [CascadingParameter]
     MainLayout Parent { get; set; } = default!;
 
-    private List<PDOKAddressSuggestion> autoComplete = new();
+    private List<FunderMaps.Core.Services.PDOKSuggestion> autoComplete = new();
 
     string? inputKaasAutoCompleteDing;
-
-    public Address()
-    {
-        http.BaseAddress = new("https://geodata.nationaalgeoregister.nl/locatieserver/v3/");
-    }
-
-    class PDOKAddressSuggestion
-    {
-        public string id { get; set; }
-        public string type { get; set; }
-        public float score { get; set; }
-        public string weergavenaam { get; set; }
-    }
-
-    class PDOKResponse
-    {
-        public int numFound { get; set; }
-        public int start { get; set; }
-        public float maxScore { get; set; }
-        public List<PDOKAddressSuggestion> docs { get; set; }
-    }
-
-    class PDOKResult
-    {
-        public PDOKResponse response { get; set; }
-        // object highlighting;
-        // object spellcheck;
-    }
-
-    class PDOKLookup
-    {
-        public string centroide_ll { get; set; }
-        public string nummeraanduiding_id { get; set; }
-    }
-
-    class PDOKResponse2
-    {
-        public int numFound { get; set; }
-        public int start { get; set; }
-        public float maxScore { get; set; }
-        public List<PDOKLookup> docs { get; set; }
-    }
-
-    class PDOKResult2
-    {
-        public PDOKResponse2 response { get; set; }
-        // object highlighting;
-        // object spellcheck;
-    }
 
     public async Task OnType(ChangeEventArgs e)
     {
         var filter = e.Value?.ToString();
 
-        var addressSuggestion = await http.GetFromJsonAsync<PDOKResult>($"suggest?fq=type:adres&q={filter}&rows=7");
-        if (addressSuggestion is not null)
+        if (string.IsNullOrEmpty(filter))
         {
-            autoComplete = addressSuggestion.response.docs;
+            return;
+        }
+
+        var addressSuggestion = await LocationService.SuggestAsync(filter, 7);
+        if (addressSuggestion.Any())
+        {
+            autoComplete = addressSuggestion;
+            return;
         }
     }
 
     async Task SelectCustomer(string id)
     {
-        var suggestion = autoComplete.Find(x => x.id == id);
-        if (suggestion is not null)
-        {
-            inputKaasAutoCompleteDing = suggestion.weergavenaam;
-        }
+        var doc = await LocationService.LookupAsync(id);
 
-        var customers = await http.GetFromJsonAsync<PDOKResult2>($"lookup?fl=nummeraanduiding_id,centroide_ll&id={id}");
-        if (customers is not null)
-        {
-            var address = await GeocoderTranslation.GetAddressIdAsync(customers.response.docs[0].nummeraanduiding_id);
-
-            State.Model.Address = address.Id;
-            State.Model.Building = address.BuildingId ?? throw new ArgumentNullException(nameof(address.BuildingId));
-
-            State.DisableNavNext = false;
-            Parent.Kaas();
-        }
+        inputKaasAutoCompleteDing = doc.weergavenaam;
 
         autoComplete.Clear();
+
+        var address = await GeocoderTranslation.GetAddressIdAsync(doc.nummeraanduiding_id);
+
+        State.Model.Address = address.Id;
+        State.Model.Building = address.BuildingId ?? throw new ArgumentNullException(nameof(address.BuildingId));
+
+        State.DisableNavNext = false;
+        Parent.Kaas();
     }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
