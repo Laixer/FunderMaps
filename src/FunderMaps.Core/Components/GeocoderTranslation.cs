2769ec70a60cc55053f90df1f918929dc8ad04f7
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-07-19 15:08:17 +0200

    Fetch building from input id

--- src/FunderMaps.Core/Components/GeocoderTranslation.cs
+++ src/FunderMaps.Core/Components/GeocoderTranslation.cs
@@ -13,14 +13,16 @@ internal class GeocoderTranslation : IGeocoderTranslation
 {
     private readonly IGeocoderParser _geocoderParser;
     private readonly IAddressRepository _addressRepository;
+    private readonly IBuildingRepository _buildingRepository;
 
     /// <summary>
     ///     Create new instance.
     /// </summary>
-    public GeocoderTranslation(IGeocoderParser geocoderParser, IAddressRepository addressRepository)
+    public GeocoderTranslation(IGeocoderParser geocoderParser, IAddressRepository addressRepository, IBuildingRepository buildingRepository)
     {
         _geocoderParser = geocoderParser ?? throw new ArgumentNullException(nameof(geocoderParser));
         _addressRepository = addressRepository ?? throw new ArgumentNullException(nameof(addressRepository));
+        _buildingRepository = buildingRepository ?? throw new ArgumentNullException(nameof(buildingRepository));
     }
 
     /// <summary>
@@ -34,5 +36,14 @@ internal class GeocoderTranslation : IGeocoderTranslation
             GeocoderDatasource.FunderMaps => await _addressRepository.GetByIdAsync(id),
             GeocoderDatasource.NlBagAddress => await _addressRepository.GetByExternalIdAsync(id),
             _ => throw new EntityNotFoundException("Requested address entity could not be found."),
+        };
+
+    public async Task<Building> GetBuildingIdAsync(string input)
+        => _geocoderParser.FromIdentifier(input, out string id) switch
+        {
+            GeocoderDatasource.FunderMaps => await _buildingRepository.GetByIdAsync(id),
+            GeocoderDatasource.NlBagAddress => await _buildingRepository.GetByExternalAddressIdAsync(id),
+            GeocoderDatasource.NlBagBuilding => await _buildingRepository.GetByExternalIdAsync(id),
+            _ => throw new EntityNotFoundException("Requested building entity could not be found."),
         };
 }
