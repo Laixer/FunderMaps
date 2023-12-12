974d5a2afd883617b71137ed273c4242d4e7671b
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-12-12 23:57:33 +0100

    Add MapBundleTask class and GenerateBundleAsync method

--- src/FunderMaps.Worker/Tasks/MapBundleTask.cs
+++ src/FunderMaps.Worker/Tasks/MapBundleTask.cs
@@ -1,3 +1,4 @@
+using FunderMaps.Core.Entities;
 using FunderMaps.Core.Helpers;
 using FunderMaps.Core.Interfaces;
 using FunderMaps.Core.Interfaces.Repositories;
@@ -21,63 +22,67 @@ internal sealed class MapBundleTask(
 {
     private readonly DbProviderOptions _dbProviderOptions = dbProviderOptions?.Value ?? throw new ArgumentNullException(nameof(dbProviderOptions));
 
+    private async Task GenerateBundleAsync(Bundle bundle, CancellationToken cancellationToken)
+    {
+        try
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(bundle.Precondition))
+            {
+                if (!await bundleRepository.RunPreconditionAsync(bundle.Tileset, bundle.Precondition))
+                {
+                    logger.LogInformation("Precondition for bundle '{Tileset}' failed, skipping", bundle.Tileset);
+                    return;
+                }
+            }
+
+            logger.LogInformation("Processing tileset '{Tileset}'", bundle.Tileset);
+
+            var dataSourceBuilder = new Npgsql.NpgsqlConnectionStringBuilder(_dbProviderOptions.ConnectionString);
+            var input = $"PG:dbname='{dataSourceBuilder.Database}' host='{dataSourceBuilder.Host}' port='{dataSourceBuilder.Port}' user='{dataSourceBuilder.Username}' password='{dataSourceBuilder.Password}'";
+
+            gdalService.Convert(input, $"{bundle.Tileset}.gpkg", $"maplayer.{bundle.Tileset}", cancellationToken);
+            await blobStorageService.StoreFileAsync($"tileset/{bundle.Tileset}.gpkg", $"{bundle.Tileset}.gpkg");
+
+            DateTime currentDate = DateTime.Now;
+            string dateString = currentDate.ToString("yyyy-MM-dd");
+
+            await blobStorageService.StoreFileAsync($"tileset/archive/{dateString}_{bundle.Tileset}.gpkg", $"{bundle.Tileset}.gpkg");
+
+            if (bundle.MapEnabled)
+            {
+                logger.LogInformation("Generating map for tileset '{Tileset}'", bundle.Tileset);
+
+                gdalService.Convert($"{bundle.Tileset}.gpkg", $"{bundle.Tileset}.geojson", cancellationToken: cancellationToken);
+                tilesetGeneratorService.Generate($"{bundle.Tileset}.geojson", $"{bundle.Tileset}.mbtiles", bundle.Tileset, bundle.MaxZoomLevel, bundle.MinZoomLevel, cancellationToken);
+                await mapboxService.UploadAsync(bundle.Name, bundle.Tileset, $"{bundle.Tileset}.mbtiles");
+            }
+
+            await bundleRepository.LogBuiltTimeAsync(bundle.Tileset);
+        }
+        finally
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            FileHelper.DeleteFilesWithExtension(currentDirectory, "gpkg");
+            FileHelper.DeleteFilesWithExtension(currentDirectory, "gpkg-journal");
+            FileHelper.DeleteFilesWithExtension(currentDirectory, "geojson");
+            FileHelper.DeleteFilesWithExtension(currentDirectory, "mbtiles");
+        }
+    }
+
     /// <summary>
     ///    Triggered when the application host is ready to start the service.
     /// </summary>
     public async Task RunAsync(CancellationToken cancellationToken)
     {
-        // TODO: Split the actual processing into a separate function.
         await foreach (var bundle in bundleRepository.ListAllEnabledAsync())
         {
-            try
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    break;
-                }
-
-                if (!string.IsNullOrEmpty(bundle.Precondition))
-                {
-                    if (!await bundleRepository.RunPreconditionAsync(bundle.Tileset, bundle.Precondition))
-                    {
-                        logger.LogInformation("Precondition for bundle '{Tileset}' failed, skipping", bundle.Tileset);
-                        continue;
-                    }
-                }
-
-                logger.LogInformation("Processing tileset '{Tileset}'", bundle.Tileset);
-
-                var dataSourceBuilder = new Npgsql.NpgsqlConnectionStringBuilder(_dbProviderOptions.ConnectionString);
-                var input = $"PG:dbname='{dataSourceBuilder.Database}' host='{dataSourceBuilder.Host}' port='{dataSourceBuilder.Port}' user='{dataSourceBuilder.Username}' password='{dataSourceBuilder.Password}'";
-
-                gdalService.Convert(input, $"{bundle.Tileset}.gpkg", $"maplayer.{bundle.Tileset}", cancellationToken);
-                await blobStorageService.StoreFileAsync($"tileset/{bundle.Tileset}.gpkg", $"{bundle.Tileset}.gpkg");
-
-                DateTime currentDate = DateTime.Now;
-                string dateString = currentDate.ToString("yyyy-MM-dd");
-
-                await blobStorageService.StoreFileAsync($"tileset/archive/{dateString}_{bundle.Tileset}.gpkg", $"{bundle.Tileset}.gpkg");
-
-                if (bundle.MapEnabled)
-                {
-                    logger.LogInformation("Generating map for tileset '{Tileset}'", bundle.Tileset);
-
-                    gdalService.Convert($"{bundle.Tileset}.gpkg", $"{bundle.Tileset}.geojson", cancellationToken: cancellationToken);
-                    tilesetGeneratorService.Generate($"{bundle.Tileset}.geojson", $"{bundle.Tileset}.mbtiles", bundle.Tileset, bundle.MaxZoomLevel, bundle.MinZoomLevel, cancellationToken);
-                    await mapboxService.UploadAsync(bundle.Name, bundle.Tileset, $"{bundle.Tileset}.mbtiles");
-                }
-
-                await bundleRepository.LogBuiltTimeAsync(bundle.Tileset);
-            }
-            finally
-            {
-                var currentDirectory = Directory.GetCurrentDirectory();
-
-                FileHelper.DeleteFilesWithExtension(currentDirectory, "gpkg");
-                FileHelper.DeleteFilesWithExtension(currentDirectory, "gpkg-journal");
-                FileHelper.DeleteFilesWithExtension(currentDirectory, "geojson");
-                FileHelper.DeleteFilesWithExtension(currentDirectory, "mbtiles");
-            }
+            await GenerateBundleAsync(bundle, cancellationToken);
         }
     }
 }
