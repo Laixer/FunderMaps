b9de35900d24136561d0c75f672489372f0f777b
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-12-13 14:03:33 +0100

    Update .NET version and add DataProtection application name

--- tests/FunderMaps.IntegrationTests/Webservice/WebserviceStub.cs
+++ tests/FunderMaps.IntegrationTests/Webservice/WebserviceStub.cs
@@ -11,14 +11,11 @@ public static class WebserviceStub
 {
     public static async Task<int> CheckQuotaUsageAsync(WebserviceFixtureFactory factory, string product)
     {
-        // Arrange
         using var client = factory.CreateClient();
 
-        // Act
         var response = await client.GetAsync("api/quota/usage");
         var returnObject = await response.Content.ReadFromJsonAsync<IList<ProductTelemetry>>();
 
-        // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.NotNull(returnObject);
 
