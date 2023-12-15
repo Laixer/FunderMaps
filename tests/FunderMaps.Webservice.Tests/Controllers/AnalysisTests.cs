fbaea665be124532f3b3f74be4238c0905cc7dd6
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-12-15 15:34:44 +0100

    Fix invalid address test cases in AnalysisTests.cs

--- tests/FunderMaps.Webservice.Tests/Controllers/AnalysisTests.cs
+++ tests/FunderMaps.Webservice.Tests/Controllers/AnalysisTests.cs
@@ -71,55 +71,31 @@ public class AnalysisTests(FunderMapsWebApplicationFactory<Program> factory) : I
         Assert.True(returnObject);
     }
 
-    // [Theory]
-    // [InlineData("id=3kjhr834dhfjdeh")]
-    // [InlineData("bagid=4928374hfdkjsfh")]
-    // [InlineData("query=thisismyquerystringyes")]
-    // [InlineData("fdshjbf438gi")]
-    // public async Task GetRiskIndexByExternalIdInvalidAddressThrows(string address)
-    // {
-    //     using var client = factory.CreateClient();
-
-    //     var response = await client.GetAsync($"api/v3/product/at_risk?id={address}");
-
-    //     Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-    // }
-
-    // [Theory]
-    // [InlineData("gfm-bc31bec346744745b29f8505dff8182f")]
-    // [InlineData("gfm-00096758461b4c8c8a8c145790126beb")]
-    // public async Task GetRiskIndexByExternalIdAddressNotFoundThrows(string address)
-    // {
-    //     using var client = factory.CreateClient();
-
-    //     var response = await client.GetAsync($"api/v3/product/at_risk?id={address}");
-
-    //     Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-    // }
-
-    // [Theory]
-    // [InlineData("id=3kjhr834dhfjdeh")]
-    // [InlineData("bagid=4928374hfdkjsfh")]
-    // [InlineData("query=thisismyquerystringyes")]
-    // [InlineData("fdshjbf438gi")]
-    // public async Task GetByIdInvalidAddressThrows(string address)
-    // {
-    //     using var client = factory.CreateClient();
-
-    //     var response = await client.GetAsync($"api/v3/product/analysis?id={address}");
-
-    //     Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-    // }
-
-    // [Theory]
+    [Theory]
+    [InlineData("sdf-sd3kjhr834dhfjdeh")]
+    [InlineData("343545435_4928374hfdkjsfh")]
+    [InlineData("thisismyquerystringyes")]
+    [InlineData("fdshjbf438gi")]
     // [InlineData("gfm-dfcdbbabf1de41c38597c049b0cce5d4")]
     // [InlineData("gfm-1437da5c31e944dd8d362264041d067a")]
-    // public async Task GetByIdAddressNotFoundThrows(string address)
-    // {
-    //     using var client = factory.CreateClient();
+    public async Task GetByIdInvalidAddressThrows(string address)
+    {
+        using var client = factory.CreateClient();
 
-    //     var response = await client.GetAsync($"api/v3/product/analysis?id={address}");
+        var authResponse = await client.PostAsJsonAsync("api/auth/signin", new SignInDto()
+        {
+            Email = "lester@contoso.com",
+            Password = "fundermaps",
+        });
+        var returnToken = await authResponse.Content.ReadFromJsonAsync<SignInSecurityTokenDto>();
 
-    //     Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-    // }
+        Assert.NotNull(returnToken);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"api/v3/product/analysis?id={address}");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", returnToken.Token);
+
+        var response = await client.SendAsync(request);
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
 }
