95cf027d14042eb36ef68fe8b90b49291b0485df
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-12-15 14:55:53 +0100

    Refactor ProductController to inherit from FunderMapsController

--- src/FunderMaps.Webservice/Controllers/ProductController.cs
+++ src/FunderMaps.Webservice/Controllers/ProductController.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Claims;
-using FunderMaps.Core.Authentication;
+﻿using FunderMaps.Core.Controllers;
 using FunderMaps.Core.Services;
 using FunderMaps.Core.Types.Products;
 using Microsoft.AspNetCore.Mvc;
@@ -10,7 +9,7 @@ namespace FunderMaps.Webservice.Controllers;
 ///     Controller for all product endpoints.
 /// </summary>
 [Route("api/v3/product")]
-public sealed class ProductController(ModelService modelService) : ControllerBase
+public sealed class ProductController(ModelService modelService) : FunderMapsController
 {
     // TODO: LEGACY
     // GET: api/v3/product/analysis
@@ -25,10 +24,7 @@ public sealed class ProductController(ModelService modelService) : ControllerBas
     [HttpGet("analysis/{id}")]
     public async Task<AnalysisProduct> GetAnalysisAsync(string id)
     {
-        // TODO: Get from base controller
-        var tenantId = Guid.Parse(User.FindFirstValue(FunderMapsAuthenticationClaimTypes.Tenant) ?? throw new InvalidOperationException());
-
-        return await modelService.GetAnalysisAsync(id, tenantId);
+        return await modelService.GetAnalysisAsync(id, TenantId);
     }
 
     // TODO: LEGACY
@@ -47,9 +43,7 @@ public sealed class ProductController(ModelService modelService) : ControllerBas
     [HttpGet("at_risk/{id}")]
     public async Task<bool> GetRiskIndexAsync(string id)
     {
-        var tenantId = Guid.Parse(User.FindFirstValue(FunderMapsAuthenticationClaimTypes.Tenant) ?? throw new InvalidOperationException());
-
-        return await modelService.GetRiskIndexAsync(id, tenantId);
+        return await modelService.GetRiskIndexAsync(id, TenantId);
     }
 
     // TODO: LEGACY
@@ -68,8 +62,6 @@ public sealed class ProductController(ModelService modelService) : ControllerBas
     [HttpGet("statistics/{id}")]
     public Task<StatisticsProduct> GetStatisticsAsync(string id)
     {
-        var tenantId = Guid.Parse(User.FindFirstValue(FunderMapsAuthenticationClaimTypes.Tenant) ?? throw new InvalidOperationException());
-
-        return modelService.GetStatisticsAsync(id, tenantId);
+        return modelService.GetStatisticsAsync(id, TenantId);
     }
 }
