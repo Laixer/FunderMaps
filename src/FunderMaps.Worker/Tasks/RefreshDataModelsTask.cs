a5d72b09f46d378fe9ce5a78f8e4d2f08f7c3491
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-12-19 19:35:10 +0100

    Add logging to RefreshDataModelsTask

--- src/FunderMaps.Worker/Tasks/RefreshDataModelsTask.cs
+++ src/FunderMaps.Worker/Tasks/RefreshDataModelsTask.cs
@@ -1,16 +1,24 @@
 using FunderMaps.Core.Interfaces;
 using FunderMaps.Core.Interfaces.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace FunderMaps.Worker.Tasks;
 
-internal sealed class RefreshDataModelsTask(IOperationRepository operationRepository) : ITaskService
+internal sealed class RefreshDataModelsTask(
+    IOperationRepository operationRepository,
+    ILogger<RefreshDataModelsTask> logger) : ITaskService
 {
     /// <summary>
     ///    Triggered when the application host is ready to start the service.
     /// </summary>
     public async Task RunAsync(CancellationToken cancellationToken)
     {
+        logger.LogInformation("Refreshing model");
+
         await operationRepository.RefreshModelAsync();
+
+        logger.LogInformation("Refreshing statistics");
+
         await operationRepository.RefreshStatisticsAsync();
     }
 }
