2b03c56cdec42a097d16972370551a7d18d18079
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-05-06 11:21:17 +0200

    Remove deprecated Address property in Incident class

--- src/FunderMaps.Core/Entities/Incident.cs
+++ src/FunderMaps.Core/Entities/Incident.cs
@@ -103,13 +103,6 @@ public sealed class Incident : RecordControl, IEntityIdentifier<string>
     [StringLength(16)]
     public string? PhoneNumber { get; set; }
 
-    // TODO: Remove this property, replaced by building.
-    /// <summary>
-    ///     An address identifier.
-    /// </summary>
-    [Required]
-    public string Address { get; set; } = default!;
-
     /// <summary>
     ///     Building identifier.
     /// </summary>
